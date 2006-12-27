4087c2fb735dc6078482f4ca2a5607fec01ae09d
globalqss <devnull@localhost>
2006-12-27 20:04:15 +0000

    Change version of JavaService.exe to 2.0.10 (better support for windows parameters - like depends)

---
+++ utils/windows/Adempiere_Service_Install.bat
@@ -0,0 +1,24 @@
+@echo off
+
+REM Adempiere_Service_Install.bat - globalqss - based on http://javaservice.objectweb.org
+
+if (%ADEMPIERE_HOME%) == () (CALL myEnvironment.bat Server) else (CALL %ADEMPIERE_HOME%\utils\myEnvironment.bat Server)
+
+%ADEMPIERE_HOME%\utils\windows\AdempiereService.exe -install Adempiere %JAVA_HOME%\jre\bin\server\jvm.dll -Xmx256M -Djava.class.path=%JAVA_HOME%\lib\tools.jar;%ADEMPIERE_HOME%\jboss\bin\run.jar -server %ADEMPIERE_JAVA_OPTIONS% -Djetty.port=%ADEMPIERE_WEB_PORT% -Djetty.ssl=%ADEMPIERE_SSL_PORT% -Djetty.keystore=%ADEMPIERE_KEYSTORE% -Djetty.password=%ADEMPIERE_KEYSTORE_PASSWORD% -start org.jboss.Main -params -c -stop org.jboss.Main -method systemExit -current %ADEMPIERE_HOME%\jboss\bin -out %ADEMPIERE_HOME%\jboss\bin\out.txt -manual -overwrite
+
+
+rem add this option for dependency with postgresql service (replace with proper service name
+rem -depends postgresql
+
+rem add this option for dependency with oracle service (replace with proper service name)
+rem -depends oraclexe
+
+rem replace -manual by -auto to make service run automatically on system startup
+
+rem other usefult options can be:
+rem -err err_log_file
+rem -shutdown seconds
+rem -user user_name
+rem -password password
+rem -append or -overwrite
+rem -description service_desc
--- utils/windows/Windows_Service_Install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM $Id: Windows_Service_Install.bat,v 1.2 2003/11/01 20:55:15 comdivisionys Exp $
-
-if (%ADEMPIERE_HOME%) == () (CALL myEnvironment.bat Server) else (CALL %ADEMPIERE_HOME%\utils\myEnvironment.bat Server)
-
-%ADEMPIERE_HOME%\utils\windows\JavaService.exe -install Adempiere %JAVA_HOME%\jre\bin\server\jvm.dll -Xmx256M -Djava.class.path=%JAVA_HOME%\lib\tools.jar;%ADEMPIERE_HOME%\jboss\bin\run.jar -server %ADEMPIERE_JAVA_OPTIONS% -Djetty.port=%ADEMPIERE_WEB_PORT% -Djetty.ssl=%ADEMPIERE_SSL_PORT% -Djetty.keystore=%ADEMPIERE_KEYSTORE% -Djetty.password=%ADEMPIERE_KEYSTORE_PASSWORD% -start org.jboss.Main -params -c adempiere -stop org.jboss.Main -method systemExit -out %ADEMPIERE_HOME%\jboss\bin\out.txt -current %ADEMPIERE_HOME%\jboss\bin
