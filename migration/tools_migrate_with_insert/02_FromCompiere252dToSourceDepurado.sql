317f9153fe54710c03f12fec71098c034d6af012
globalqss <devnull@localhost>
2007-02-27 08:36:31 +0000

    add proper file

---
+++ migration/tools_migrate_with_insert/02_FromCompiere252dToSourceDepurado.sql
@@ -0,0 +1,11 @@
+--------------------------------------------------------------------------
+-- Play this script in COMPIERE252D@LINUXQSS/XE to make it look like SOURCE@LINUXQSS/XE
+--                                                                      --
+-- Please review the script before using it to make sure it won't       --
+-- cause any unacceptable data loss.                                    --
+--                                                                      --
+-- COMPIERE252D@LINUXQSS/XE Schema Extracted by User COMPIERE252D 
+-- SOURCE@LINUXQSS/XE Schema Extracted by User COMPIERE252D 
+
+
+-- Here must be the output generated with TOAD or Karsten tool (just schema)
