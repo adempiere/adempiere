3643fdc5f060d6ec7f1bf31812a9c0ea1ad7c57c
globalqss <devnull@localhost>
2007-04-14 22:17:59 +0000

    little enhancement to myDBcopyTemplate.sh - add datetime to jar file generated

--- utils/myDBcopyTemplate.sh
+++ utils/myDBcopyTemplate.sh
@@ -1,5 +1,7 @@
 # Copy Database after backup
 # $Id: myDBcopyTemplate.sh,v 1.1 2002/10/07 01:23:32 jjanke Exp $
 
-echo copy $ADEMPIERE_HOME/data/ExpDat.jar to backup media
+DATE=`date +%Y%m%d_%H%M%S`
+mv $ADEMPIERE_HOME/data/ExpDat.jar $ADEMPIERE_HOME/data/ExpDat$DATE.jar
+echo copy $ADEMPIERE_HOME/data/ExpDat$DATE.jar to backup media
 
