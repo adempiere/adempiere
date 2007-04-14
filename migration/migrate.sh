0fcac06869556ad0bf1ad1a9414dc801ec7f201e
globalqss <devnull@localhost>
2007-04-14 21:34:13 +0000

    Little enhancements to migrate.sh script - convert from dos to unix - spool

--- migration/migrate.sh
+++ migration/migrate.sh
@@ -5,18 +5,20 @@
 # mode (the default mode -- this stips out all the "commit" statements)
 # and a commit mode for deployment. You need to pipe the output of this
 # script into sqlplus, for example:
-# ./migrate.sh 313-314 commit | sqlplus compiere/compiere
+# ./migrate.sh 313-314 commit | sqlplus adempiere/adempiere
 
 # Contributed by Chris Farley - northernbrewer
 
 if [ -z "$1" ]; then
-   echo "Usage: migrate DIRECTORY [commit]"
+   echo "Usage: $0 DIRECTORY [commit]"
    exit 0
 fi
-echo "SET SQLBLANKLINES ON;"
-echo "SET DEFINE OFF;"
+echo "SET SQLBLANKLINES ON"
+echo "SET DEFINE OFF"
+echo "SPOOL $1"
 for file in `ls $1`; do
    echo "SELECT '$file' AS Filename FROM dual;"
+   dos2unix $1/$file
    cat $1/$file | sed 's/commit[ ]*;//I'
    echo
 done
