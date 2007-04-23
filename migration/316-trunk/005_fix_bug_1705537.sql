89c9cd1bc52242d5209b20cfeb34c59c9fc2aa29
globalqss <devnull@localhost>
2007-04-23 04:48:02 +0000

    Bug Fix [ 1705537 ] Import Account parent key not shown

---
+++ migration/316-trunk/005_fix_bug_1705537.sql
@@ -0,0 +1,7 @@
+UPDATE AD_COLUMN
+   SET ad_reference_value_id = 182,
+       updated = TO_DATE ('2007-04-22 23:11:58', 'YYYY-MM-DD HH24:MI:SS'),
+       updatedby = 100
+ WHERE ad_column_id = 7968;
+
+COMMIT;
