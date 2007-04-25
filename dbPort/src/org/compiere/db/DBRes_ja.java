b8385565ef2056e77207bc964cbdcc6291314459
trifonnt <devnull@localhost>
2007-04-25 15:55:15 +0000

    Contribution: [ 1707503 ] Japanese messages before login Contributed by: kubotti - kubotti

--- dbPort/src/org/compiere/db/DBRes_ja.java
+++ dbPort/src/org/compiere/db/DBRes_ja.java
@@ -28,7 +28,7 @@ public class DBRes_ja extends ListResourceBundle
 {
 	/** Data        */
 	static final Object[][] contents = new String[][]{
-	{ "CConnectionDialog", 	"\u30b3\u30f3\u30d4\u30a8\u30fc\u30ec\u306e\u63a5\u7d9a" },
+	{ "CConnectionDialog", 	"\u30a2\u30c7\u30f3\u30d4\u30a8\u30fc\u30ec\u306e\u63a5\u7d9a" },
 	{ "Name", 				"\u540d\u524d" },
 	{ "AppsHost", 			"\u30a2\u30d7\u30ea\u30b1\u30fc\u30b7\u30e7\u30f3\u30fb\u30b5\u30fc\u30d0" },
 	{ "AppsPort", 			"\u30a2\u30d7\u30ea\u30b1\u30fc\u30b7\u30e7\u30f3\u30fb\u30dd\u30fc\u30c8" },
@@ -42,15 +42,15 @@ public class DBRes_ja extends ListResourceBundle
 	{ "FWPort", 			"\u30d5\u30a1\u30a4\u30a2\u30a6\u30a9\u30fc\u30eb\u30fb\u30dd\u30fc\u30c8" },
 	{ "TestConnection", 	"\u30c7\u30fc\u30bf\u30d9\u30fc\u30b9\u30fb\u30b5\u30fc\u30d0\u306e\u30c6\u30b9\u30c8" },
 	{ "Type", 				"\u30c7\u30fc\u30bf\u30d9\u30fc\u30b9" },
-	{ "BequeathConnection", "\u53e4\u63a5\u7d9a" },
-	{ "Overwrite", 			"\u30aa\u30fc\u30f4\u30a1\u30e9\u30a4\u30c8" },
+	{ "BequeathConnection", "\u7d99\u627f\u63a5\u7d9a" },
+	{ "Overwrite", 			"\u30aa\u30fc\u30d0\u30fc\u30e9\u30a4\u30c9" },
 	{ "ConnectionProfile",	"Connection" },
 	{ "LAN",		 		"LAN" },
 	{ "TerminalServer",		"Terminal Server" },
 	{ "VPN",		 		"VPN" },
 	{ "WAN", 				"WAN" },
-	{ "ConnectionError", 	"\u63a5\u7d9a\u306e\u30a8\u30e9\u30fc" },
-	{ "ServerNotActive", 	"\u30b5\u30fc\u30d0\u3092\u898b\u4ed8\u3051\u308c\u306a\u3044" }
+	{ "ConnectionError", 	"\u63a5\u7d9a\u30a8\u30e9\u30fc" },
+	{ "ServerNotActive", 	"\u30b5\u30fc\u30d0\u30fc\u304c\u898b\u3064\u304b\u308a\u307e\u305b\u3093" }
 	};
 
 	/**
