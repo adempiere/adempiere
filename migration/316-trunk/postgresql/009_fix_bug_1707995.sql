72999d7c247fa23ddb01df1a72261458e4b8bc07
deathmeat <devnull@localhost>
2007-04-26 20:42:15 +0000

    Fixed WRONG ad_val_rule_id and WRONG escaping of tickmarks (single quotes) since the script can't be executed in oracle sql developer (and i guess the same with plain sqlplus). Also fixed the escaping for the postgresql script

--- migration/316-trunk/postgresql/009_fix_bug_1707995.sql
+++ migration/316-trunk/postgresql/009_fix_bug_1707995.sql
@@ -3,10 +3,10 @@
 
 INSERT INTO AD_VAL_RULE (ad_val_rule_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, 
 		 type, code, entitytype )
-	VALUES(1000002, 0, 0, 'Y', TO_TIMESTAMP('2007-04-26','YYYY-MM-DD'), 100, TO_TIMESTAMP('2007-04-26','YYYY-MM-DD'), 100, 'M_PriceList is SO/PO', 'Limits the Sales & Purchase Order window to the correct price lsits',
-		'S', '(M_PriceList.IsSOPriceList = \'Y\' AND \'@IsSOTrx@\'=\'Y\') OR (M_PriceList.IsSOPriceList = \'N\' AND \'@IsSOTrx@\'=\'N\')', 'D');
+	VALUES(271, 0, 0, 'Y', TO_TIMESTAMP('2007-04-26','YYYY-MM-DD'), 100, TO_TIMESTAMP('2007-04-26','YYYY-MM-DD'), 100, 'M_PriceList is SO/PO', 'Limits the Sales & Purchase Order window to the correct price lsits',
+		'S', '(M_PriceList.IsSOPriceList = ''Y'' AND ''@IsSOTrx@''=''Y'') OR (M_PriceList.IsSOPriceList = ''N'' AND ''@IsSOTrx@''=''N'')', 'D');
 
-UPDATE AD_COLUMN set ad_val_rule_id = 1000002 where ad_column_id = 2204;
+UPDATE AD_COLUMN set ad_val_rule_id = 271 where ad_column_id = 2204;
 
 
 COMMIT;
