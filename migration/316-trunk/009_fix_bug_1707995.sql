78dcde6eec9f956c40c5ca7caa17f117fe5bdd43
globalqss <devnull@localhost>
2007-04-26 21:28:58 +0000

    Avoid problems with & SET DEFINE OFF;

--- migration/316-trunk/009_fix_bug_1707995.sql
+++ migration/316-trunk/009_fix_bug_1707995.sql
@@ -1,12 +1,15 @@
 -- Bug Fix
 -- http://sourceforge.net/tracker/index.php?func=detail&aid=1707995&group_id=176962&atid=879332
 
-INSERT INTO AD_VAL_RULE (ad_val_rule_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, name, description, 
-		 type, code, entitytype )
+SET DEFINE OFF;
+SET SQLBLANKLINES OFF;
+
+INSERT INTO AD_VAL_RULE (ad_val_rule_id, ad_client_id, ad_org_id, isactive, created, createdby, updated, updatedby, NAME, description, 
+		 TYPE, code, entitytype )
 	VALUES(271, 0, 0, 'Y', TO_DATE('2007-04-26','YYYY-MM-DD'), 100, TO_DATE('2007-04-26','YYYY-MM-DD'), 100, 'M_PriceList is SO/PO', 'Limits the Sales & Purchase Order window to the correct price lsits',
 		'S', '(M_PriceList.IsSOPriceList = ''Y'' AND ''@IsSOTrx@''=''Y'') OR (M_PriceList.IsSOPriceList = ''N'' AND ''@IsSOTrx@''=''N'')', 'D');
 
-UPDATE AD_COLUMN set ad_val_rule_id = 271 where ad_column_id = 2204;
+UPDATE AD_COLUMN SET ad_val_rule_id = 271 WHERE ad_column_id = 2204;
 
 
 COMMIT;
