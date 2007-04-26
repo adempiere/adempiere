9dd6f38d95fb7ad3ff85d8d0ac64776f1bb261b5
globalqss <devnull@localhost>
2007-04-26 21:10:59 +0000

    Reorder and fix quote bug

--- migration/316-trunk/007_country_changes.sql
+++ migration/316-trunk/007_country_changes.sql
@@ -1,33 +1,53 @@
 -- juddm BugFix:(Country Changes) 
--- add country Serbia
-INSERT INTO C_COUNTRY
-		(C_COUNTRY_ID, AD_CLIENT_ID, AD_ORG_ID, ISACTIVE, CREATED, CREATEDBY, UPDATED, UPDATEDBY,
-		NAME, DESCRIPTION, COUNTRYCODE, HASREGION, DISPLAYSEQUENCE, HASPOSTAL_ADD, C_CURRENCY_ID,
-		ISADDRESSLINESREVERSE, ISADDRESSLINESLOCALREVERSE)
-	VALUES (349, 0, 0, 'Y', TO_DATE('2003-03-09', 'YYYY-MM-DD'), 0, TO_DATE('2000-01-02','YYYY-MM-DD'), 0,
-		'Serbia', 'Serbia', 'RS', 'N', '@C@, @P@', 'N', 347, 'N', 'N');
-
--- add country Montenegro		
-INSERT INTO C_COUNTRY
-		(C_COUNTRY_ID, AD_CLIENT_ID, AD_ORG_ID, ISACTIVE, CREATED, CREATEDBY, UPDATED, UPDATEDBY,
-		NAME, DESCRIPTION, COUNTRYCODE, HASREGION, DISPLAYSEQUENCE, HASPOSTAL_ADD, C_CURRENCY_ID,
-		ISADDRESSLINESREVERSE, ISADDRESSLINESLOCALREVERSE)
-	VALUES (350, 0, 0, 'Y', TO_DATE('2003-03-09','YYYY-MM-DD'), 0, TO_DATE('2000-01-02','YYYY-MM-DD'), 0,
-		'Montenegro', 'Montenegro', 'ME', 'N', '@C@, @P@', 'N', 102, 'N', 'N');
-
--- deactivate Yugoslavia
-UPDATE C_COUNTRY SET ISACTIVE = 'N' WHERE C_COUNTRY_ID=346;
 
 --add Serbian currency
 INSERT INTO C_CURRENCY
-			(C_CURRENCY_ID, AD_CLIENT_ID, AD_ORG_ID, ISACTIVE, CREATED, CREATEDBY, UPDATED, UPDATEDBY, ISO_CODE,
-			CURSYMBOL, DESCRIPTION, STDPRECISION, COSTINGPRECISION, ISEURO, ISEMUMEMBER)
-		VALUES(347, 0, 0, 'Y', TO_DATE('2003-08-06','YYYY-MM-DD'),0, TO_DATE('2000-01-02','YYYY-MM-DD), 0, 'RSD',
-			'RSD', 'Serbian Dinar', 2, 4, 'N', 'N')
+            (c_currency_id, ad_client_id, ad_org_id, isactive, created,
+             createdby, updated, updatedby, iso_code, cursymbol,
+             description, stdprecision, costingprecision, iseuro, isemumember
+            )
+     VALUES (347, 0, 0, 'Y', TO_DATE ('2003-08-06', 'YYYY-MM-DD'),
+             0, TO_DATE ('2000-01-02', 'YYYY-MM-DD'), 0, 'RSD', 'RSD',
+             'Serbian Dinar', 2, 4, 'N', 'N'
+            );
+
+-- add country Serbia
+INSERT INTO C_COUNTRY
+            (c_country_id, ad_client_id, ad_org_id, isactive, created,
+             createdby, updated, updatedby, NAME, description,
+             countrycode, hasregion, displaysequence, haspostal_add,
+             c_currency_id, isaddresslinesreverse, isaddresslineslocalreverse
+            )
+     VALUES (349, 0, 0, 'Y', TO_DATE ('2003-03-09', 'YYYY-MM-DD'),
+             0, TO_DATE ('2000-01-02', 'YYYY-MM-DD'), 0, 'Serbia', 'Serbia',
+             'RS', 'N', '@C@, @P@', 'N',
+             347, 'N', 'N'
+            );
+
+-- add country Montenegro		
+INSERT INTO C_COUNTRY
+            (c_country_id, ad_client_id, ad_org_id, isactive, created,
+             createdby, updated, updatedby, NAME,
+             description, countrycode, hasregion, displaysequence,
+             haspostal_add, c_currency_id, isaddresslinesreverse,
+             isaddresslineslocalreverse
+            )
+     VALUES (350, 0, 0, 'Y', TO_DATE ('2003-03-09', 'YYYY-MM-DD'),
+             0, TO_DATE ('2000-01-02', 'YYYY-MM-DD'), 0, 'Montenegro',
+             'Montenegro', 'ME', 'N', '@C@, @P@',
+             'N', 102, 'N',
+             'N'
+            );
+
+-- deactivate Yugoslavia
+UPDATE C_COUNTRY
+   SET isactive = 'N'
+ WHERE c_country_id = 346;
 
 
 -- deactivate Yugoslavia currency
-UPDATE C_CURRENCY SET ISACTIVE = 'N' WHERE C_CURRENCY_ID = 314;
+UPDATE C_CURRENCY
+   SET isactive = 'N'
+ WHERE c_currency_id = 314;
 
-COMMIT;
-
+COMMIT ;
