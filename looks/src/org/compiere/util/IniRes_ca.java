cb6c8a33230123324b0723ff7e8eea9255b3a5fb
trifonnt <devnull@localhost>
2007-04-30 14:23:27 +0000

    Contribution from Jordi Luna - recavap 1709787  	  New & Old (fixed) Localizations for Spain & Catalonia https://sourceforge.net/tracker/index.php?func=detail&aid=1709787&group_id=176962&atid=883808

--- looks/src/org/compiere/util/IniRes_ca.java
+++ looks/src/org/compiere/util/IniRes_ca.java
@@ -22,6 +22,7 @@ import java.util.ListResourceBundle;
  *  License Dialog Translation
  *
  *  @author Jaume Teixi
+ *  @author 	Jordi Luna
  *  @version    $Id: IniRes_ca.java,v 1.2 2006/07/30 00:52:23 jjanke Exp $
  */
 public class IniRes_ca extends ListResourceBundle
@@ -29,12 +30,12 @@ public class IniRes_ca extends ListResourceBundle
 	/** Translation Content     */
 	static final Object[][] contents = new String[][]
 	{
-	{ "Adempiere_License",   "Acord de Llic�ncia" },
-	{ "Do_you_accept",      "Accepteu la Llic�ncia ?" },
+	{ "Adempiere_License",  "Acord de Llic\u00e8ncia" },
+	{ "Do_you_accept",      "Accepteu la Llic\u00e8ncia ?" },
 	{ "No",                 "No" },
-	{ "Yes_I_Understand",   "S�, l'Entenc i l'Accepto" },
+	{ "Yes_I_Understand",   "Si, l'entenc i l'accepto" },
 	{ "license_htm",        "org/adempiere/license.htm" },
-	{ "License_rejected",   "Llic�ncia Refusada o Expirada" }
+	{ "License_rejected",   "Llic\u00e8ncia rebutjada o caducada" }
 	};
 
 	/**
--- looks/src/org/compiere/util/IniRes_es.java
+++ looks/src/org/compiere/util/IniRes_es.java
@@ -22,6 +22,7 @@ import java.util.ListResourceBundle;
  *  License Dialog Translation
  *
  *  @author     Erwin Cortes
+ *  @author     Jordi Luna
  *  @version    $Id: IniRes_es.java,v 1.2 2006/07/30 00:52:23 jjanke Exp $
  */
 public class IniRes_es extends ListResourceBundle
@@ -29,12 +30,12 @@ public class IniRes_es extends ListResourceBundle
 	/** Translation Content     */
 	static final Object[][] contents = new String[][]
 	{
-	{ "Adempiere_License",   "Términos de Licencia" },
+	{ "Adempiere_License",  "Acuerdo de Licencia" },
 	{ "Do_you_accept",      "Acepta la Licencia?" },
 	{ "No",                 "No" },
-	{ "Yes_I_Understand",   "SI, Entendí y Acepto" },
+	{ "Yes_I_Understand",   "S\u00ed, entiendo y acepto" },
 	{ "license_htm",        "org/adempiere/license.htm" },
-	{ "License_rejected",   "Licencia refutada o Expirada" }
+	{ "License_rejected",   "Licencia rechazada o caducada" }
 	};
 
 	/**
