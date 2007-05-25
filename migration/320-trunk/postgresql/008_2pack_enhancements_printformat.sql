8cb930cc9d268533becac209f8bc4845e1e17006
wght <devnull@localhost>
2007-05-25 17:40:23 +0000

    Fixed alter statement.

--- migration/320-trunk/postgresql/008_2pack_enhancements_printformat.sql
+++ migration/320-trunk/postgresql/008_2pack_enhancements_printformat.sql
@@ -1,4 +1,4 @@
-ALTER TABLE AD_PACKAGE_EXP_DETAIL ADD ad_message_id NUMERIC(10,0);
+ALTER TABLE AD_PACKAGE_EXP_DETAIL ADD ad_printformat_id NUMERIC(10,0);
 
 INSERT INTO AD_COLUMN
             (ad_column_id, ad_client_id, ad_org_id, isactive,
