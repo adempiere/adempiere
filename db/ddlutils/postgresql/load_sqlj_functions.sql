1e09ca84363f7020052b58cfae7b990e764c5388
hengsin <devnull@localhost>
2007-06-04 02:41:59 +0000

    * remove file that is generated on every run from repository.

--- db/ddlutils/postgresql/load_sqlj_functions.sql
+++
@@ -1,9 +0,0 @@
-
-	CREATE SCHEMA adempiere;
-	
-	SET search_path TO adempiere,sqlj;
-	
-	SELECT sqlj.install_jar('file:///e-evolution/adempiere/trunk/sqlj/sqlj.jar', 'sqlj', true);
-	
-	SELECT sqlj.set_classpath('adempiere', 'sqlj');
-	
