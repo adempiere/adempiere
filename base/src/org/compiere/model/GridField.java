4708401dfd39592d287a6615b156d9dba7ce9940
teo_sarca <devnull@localhost>
2007-06-11 10:02:12 +0000

    [ 1734881 ] Any window with product attribute on first tab is not workin http://sourceforge.net/tracker/index.php?func=detail&aid=1734881&group_id=176962&atid=879332
    Revert to rev. 2533.

--- base/src/org/compiere/model/GridField.java
+++ base/src/org/compiere/model/GridField.java
@@ -122,9 +122,7 @@ public class GridField
 			return;
 		log.config("(" + m_vo.ColumnName + ")");
 
-		
-		if (DisplayType.isLookup(m_vo.displayType) && m_vo.IsDisplayed || m_vo.displayType == DisplayType.TableDir && m_vo.IsDisplayed
-				||m_vo.displayType == DisplayType.PAttribute)
+		if (DisplayType.isLookup(m_vo.displayType) && m_vo.IsDisplayed)
 		{
 			if (m_vo.lookupInfo == null)
 			{
@@ -158,11 +156,11 @@ public class GridField
 			MAccountLookup ma = new MAccountLookup (m_vo.ctx, m_vo.WindowNo);
 			m_lookup = ma;
 		}
-		//else if (m_vo.displayType == DisplayType.PAttribute)    //  not cached
-		//{
-		//	MPAttributeLookup pa = new MPAttributeLookup (m_vo.ctx, m_vo.WindowNo);
-		//	m_lookup = pa;
-		//}
+		else if (m_vo.displayType == DisplayType.PAttribute)    //  not cached
+		{
+			MPAttributeLookup pa = new MPAttributeLookup (m_vo.ctx, m_vo.WindowNo);
+			m_lookup = pa;
+		}
 	}   //  m_lookup
 
 	/**
@@ -369,6 +367,7 @@ public class GridField
 			if (!retValue)
 				return false;
 		}
+		
 
 		//  Always editable if Active
 		if (m_vo.ColumnName.equals("Processing")
