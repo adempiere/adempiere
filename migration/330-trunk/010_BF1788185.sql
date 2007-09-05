c62da20ad8a6f2d4d8a05dd12a82d30cf0a5e89d
globalqss <devnull@localhost>
2007-09-05 03:39:21 +0000

    Bug Fix [ 1788185 ] Inconsistence in tax indicator column

---
+++ migration/330-trunk/010_BF1788185.sql
@@ -0,0 +1,9 @@
+ALTER TABLE I_INVOICE MODIFY(taxindicator NVARCHAR2(10));
+
+ALTER TABLE I_ORDER MODIFY(taxindicator NVARCHAR2(10));
+
+UPDATE AD_COLUMN
+   SET fieldlength = 10
+ WHERE ad_column_id IN (8991, 9186);
+
+COMMIT;
