211459e71ad7a266b7e74e6325bc40b1c585ce74
shameem_z <devnull@localhost>
2007-09-28 13:29:41 +0000

    Refactoring U_Menu to U_WebMenu

---
+++ base/src/org/compiere/model/I_U_WebMenu.java
@@ -0,0 +1,166 @@
+/**********************************************************************
+ * This file is part of Adempiere ERP Bazaar                          *
+ * http://www.adempiere.org                                           *
+ *                                                                    *
+ * Copyright (C) Trifon Trifonov.                                     *
+ * Copyright (C) Contributors                                         *
+ *                                                                    *
+ * This program is free software;
+ you can redistribute it and/or      *
+ * modify it under the terms of the GNU General Public License        *
+ * as published by the Free Software Foundation;
+ either version 2     *
+ * of the License, or (at your option) any later version.             *
+ *                                                                    *
+ * This program is distributed in the hope that it will be useful,    *
+ * but WITHOUT ANY WARRANTY;
+ without even the implied warranty of     *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the       *
+ * GNU General Public License for more details.                       *
+ *                                                                    *
+ * You should have received a copy of the GNU General Public License  *
+ * along with this program;
+ if not, write to the Free Software        *
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,         *
+ * MA 02110-1301, USA.                                                *
+ *                                                                    *
+ * Contributors:                                                      *
+ * - Trifon Trifonov (trifonnt@users.sourceforge.net)                 *
+ *                                                                    *
+ * Sponsors:                                                          *
+ * - Company (http://www.site.com)                                    *
+ **********************************************************************/
+package org.compiere.model;
+
+import java.math.BigDecimal;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for U_WebMenu
+ *  @author Trifon Trifonov (generated) 
+ *  @version Release 3.3.0
+ */
+public interface I_U_WebMenu 
+{
+
+    /** TableName=U_WebMenu */
+    public static final String Table_Name = "U_WebMenu";
+
+    /** AD_Table_ID=52003 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 4 - System 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(4);
+
+    /** Load Meta Data */
+
+    /** Column name Category */
+    public static final String COLUMNNAME_Category = "Category";
+
+	/** Set Category	  */
+	public void setCategory (String Category);
+
+	/** Get Category	  */
+	public String getCategory();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description	  */
+	public void setDescription (String Description);
+
+	/** Get Description	  */
+	public String getDescription();
+
+    /** Column name HasSubMenu */
+    public static final String COLUMNNAME_HasSubMenu = "HasSubMenu";
+
+	/** Set HasSubMenu	  */
+	public void setHasSubMenu (boolean HasSubMenu);
+
+	/** Get HasSubMenu	  */
+	public boolean isHasSubMenu();
+
+    /** Column name Help */
+    public static final String COLUMNNAME_Help = "Help";
+
+	/** Set Help	  */
+	public void setHelp (String Help);
+
+	/** Get Help	  */
+	public String getHelp();
+
+    /** Column name ImageLink */
+    public static final String COLUMNNAME_ImageLink = "ImageLink";
+
+	/** Set ImageLink	  */
+	public void setImageLink (String ImageLink);
+
+	/** Get ImageLink	  */
+	public String getImageLink();
+
+    /** Column name MenuLink */
+    public static final String COLUMNNAME_MenuLink = "MenuLink";
+
+	/** Set MenuLink	  */
+	public void setMenuLink (String MenuLink);
+
+	/** Get MenuLink	  */
+	public String getMenuLink();
+
+    /** Column name Module */
+    public static final String COLUMNNAME_Module = "Module";
+
+	/** Set Module	  */
+	public void setModule (String Module);
+
+	/** Get Module	  */
+	public String getModule();
+
+    /** Column name Name */
+    public static final String COLUMNNAME_Name = "Name";
+
+	/** Set Name	  */
+	public void setName (String Name);
+
+	/** Get Name	  */
+	public String getName();
+
+    /** Column name ParentMenu_ID */
+    public static final String COLUMNNAME_ParentMenu_ID = "ParentMenu_ID";
+
+	/** Set ParentMenu_ID	  */
+	public void setParentMenu_ID (int ParentMenu_ID);
+
+	/** Get ParentMenu_ID	  */
+	public int getParentMenu_ID();
+
+    /** Column name Position */
+    public static final String COLUMNNAME_Position = "Position";
+
+	/** Set Position	  */
+	public void setPosition (String Position);
+
+	/** Get Position	  */
+	public String getPosition();
+
+    /** Column name Sequence */
+    public static final String COLUMNNAME_Sequence = "Sequence";
+
+	/** Set Sequence	  */
+	public void setSequence (BigDecimal Sequence);
+
+	/** Get Sequence	  */
+	public BigDecimal getSequence();
+
+    /** Column name U_WebMenu_ID */
+    public static final String COLUMNNAME_U_WebMenu_ID = "U_WebMenu_ID";
+
+	/** Set U_WebMenu_ID	  */
+	public void setU_WebMenu_ID (int U_WebMenu_ID);
+
+	/** Get U_WebMenu_ID	  */
+	public int getU_WebMenu_ID();
+}
---
+++ base/src/org/compiere/model/X_U_WebMenu.java
@@ -0,0 +1,323 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.compiere.model;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.util.Properties;
+import org.compiere.util.Env;
+
+/** Generated Model for U_WebMenu
+ *  @author Adempiere (generated) 
+ *  @version Release 3.3.0 - $Id$ */
+public class X_U_WebMenu extends PO implements I_U_WebMenu, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+    /** Standard Constructor */
+    public X_U_WebMenu (Properties ctx, int U_WebMenu_ID, String trxName)
+    {
+      super (ctx, U_WebMenu_ID, trxName);
+      /** if (U_WebMenu_ID == 0)
+        {
+			setHasSubMenu (false);
+// 'N'
+			setMenuLink (null);
+			setModule (null);
+			setName (null);
+			setU_WebMenu_ID (0);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_U_WebMenu (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 4 - System 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID);
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_U_WebMenu[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Category.
+		@param Category Category	  */
+	public void setCategory (String Category)
+	{
+
+		if (Category != null && Category.length() > 120)
+		{
+			log.warning("Length > 120 - truncated");
+			Category = Category.substring(0, 119);
+		}
+		set_Value (COLUMNNAME_Category, Category);
+	}
+
+	/** Get Category.
+		@return Category	  */
+	public String getCategory () 
+	{
+		return (String)get_Value(COLUMNNAME_Category);
+	}
+
+	/** Set Description.
+		@param Description Description	  */
+	public void setDescription (String Description)
+	{
+
+		if (Description != null && Description.length() > 200)
+		{
+			log.warning("Length > 200 - truncated");
+			Description = Description.substring(0, 199);
+		}
+		set_Value (COLUMNNAME_Description, Description);
+	}
+
+	/** Get Description.
+		@return Description	  */
+	public String getDescription () 
+	{
+		return (String)get_Value(COLUMNNAME_Description);
+	}
+
+	/** Set HasSubMenu.
+		@param HasSubMenu HasSubMenu	  */
+	public void setHasSubMenu (boolean HasSubMenu)
+	{
+		set_Value (COLUMNNAME_HasSubMenu, Boolean.valueOf(HasSubMenu));
+	}
+
+	/** Get HasSubMenu.
+		@return HasSubMenu	  */
+	public boolean isHasSubMenu () 
+	{
+		Object oo = get_Value(COLUMNNAME_HasSubMenu);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Help.
+		@param Help Help	  */
+	public void setHelp (String Help)
+	{
+
+		if (Help != null && Help.length() > 2000)
+		{
+			log.warning("Length > 2000 - truncated");
+			Help = Help.substring(0, 1999);
+		}
+		set_Value (COLUMNNAME_Help, Help);
+	}
+
+	/** Get Help.
+		@return Help	  */
+	public String getHelp () 
+	{
+		return (String)get_Value(COLUMNNAME_Help);
+	}
+
+	/** Set ImageLink.
+		@param ImageLink ImageLink	  */
+	public void setImageLink (String ImageLink)
+	{
+
+		if (ImageLink != null && ImageLink.length() > 510)
+		{
+			log.warning("Length > 510 - truncated");
+			ImageLink = ImageLink.substring(0, 509);
+		}
+		set_Value (COLUMNNAME_ImageLink, ImageLink);
+	}
+
+	/** Get ImageLink.
+		@return ImageLink	  */
+	public String getImageLink () 
+	{
+		return (String)get_Value(COLUMNNAME_ImageLink);
+	}
+
+	/** Set MenuLink.
+		@param MenuLink MenuLink	  */
+	public void setMenuLink (String MenuLink)
+	{
+		if (MenuLink == null)
+			throw new IllegalArgumentException ("MenuLink is mandatory.");
+
+		if (MenuLink.length() > 510)
+		{
+			log.warning("Length > 510 - truncated");
+			MenuLink = MenuLink.substring(0, 509);
+		}
+		set_Value (COLUMNNAME_MenuLink, MenuLink);
+	}
+
+	/** Get MenuLink.
+		@return MenuLink	  */
+	public String getMenuLink () 
+	{
+		return (String)get_Value(COLUMNNAME_MenuLink);
+	}
+
+	/** Set Module.
+		@param Module Module	  */
+	public void setModule (String Module)
+	{
+		if (Module == null)
+			throw new IllegalArgumentException ("Module is mandatory.");
+
+		if (Module.length() > 120)
+		{
+			log.warning("Length > 120 - truncated");
+			Module = Module.substring(0, 119);
+		}
+		set_Value (COLUMNNAME_Module, Module);
+	}
+
+	/** Get Module.
+		@return Module	  */
+	public String getModule () 
+	{
+		return (String)get_Value(COLUMNNAME_Module);
+	}
+
+	/** Set Name.
+		@param Name Name	  */
+	public void setName (String Name)
+	{
+		if (Name == null)
+			throw new IllegalArgumentException ("Name is mandatory.");
+
+		if (Name.length() > 120)
+		{
+			log.warning("Length > 120 - truncated");
+			Name = Name.substring(0, 119);
+		}
+		set_Value (COLUMNNAME_Name, Name);
+	}
+
+	/** Get Name.
+		@return Name	  */
+	public String getName () 
+	{
+		return (String)get_Value(COLUMNNAME_Name);
+	}
+
+	/** ParentMenu_ID AD_Reference_ID=52000 */
+	public static final int PARENTMENU_ID_AD_Reference_ID=52000;
+	/** Set ParentMenu_ID.
+		@param ParentMenu_ID ParentMenu_ID	  */
+	public void setParentMenu_ID (int ParentMenu_ID)
+	{
+		if (ParentMenu_ID <= 0) 
+			set_Value (COLUMNNAME_ParentMenu_ID, null);
+		else 
+			set_Value (COLUMNNAME_ParentMenu_ID, Integer.valueOf(ParentMenu_ID));
+	}
+
+	/** Get ParentMenu_ID.
+		@return ParentMenu_ID	  */
+	public int getParentMenu_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_ParentMenu_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Position.
+		@param Position Position	  */
+	public void setPosition (String Position)
+	{
+
+		if (Position != null && Position.length() > 10)
+		{
+			log.warning("Length > 10 - truncated");
+			Position = Position.substring(0, 9);
+		}
+		set_Value (COLUMNNAME_Position, Position);
+	}
+
+	/** Get Position.
+		@return Position	  */
+	public String getPosition () 
+	{
+		return (String)get_Value(COLUMNNAME_Position);
+	}
+
+	/** Set Sequence.
+		@param Sequence Sequence	  */
+	public void setSequence (BigDecimal Sequence)
+	{
+		set_Value (COLUMNNAME_Sequence, Sequence);
+	}
+
+	/** Get Sequence.
+		@return Sequence	  */
+	public BigDecimal getSequence () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_Sequence);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
+	/** Set U_WebMenu_ID.
+		@param U_WebMenu_ID U_WebMenu_ID	  */
+	public void setU_WebMenu_ID (int U_WebMenu_ID)
+	{
+		if (U_WebMenu_ID < 1)
+			 throw new IllegalArgumentException ("U_WebMenu_ID is mandatory.");
+		set_ValueNoCheck (COLUMNNAME_U_WebMenu_ID, Integer.valueOf(U_WebMenu_ID));
+	}
+
+	/** Get U_WebMenu_ID.
+		@return U_WebMenu_ID	  */
+	public int getU_WebMenu_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_U_WebMenu_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+}
