95cc951d7c6ab9ff02d956d0fbd17f70b5845e37
trifonnt <devnull@localhost>
2007-11-22 13:39:19 +0000

    Moved to contributions as it is not used in trunk.

--- _Project-ID-AdempiereJasper/doc/readme.txt
+++
@@ -1,1 +0,0 @@
-Here we will place all documentation.
--- _Project-ID-AdempiereJasper/run-druid.bat
+++
@@ -1,3 +0,0 @@
-@IF NOT EXIST %JAVA_HOME%\bin ECHO "** JAVA_HOME NOT found"
-
-%JAVA_HOME%/bin/java -Xms64M -Xmx256M -jar ./../druid/druid.jar -proj:./druid/Project-ID-TEMPLATE.druid
