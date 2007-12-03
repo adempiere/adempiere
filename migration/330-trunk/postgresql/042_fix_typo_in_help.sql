264463001f2d9c72f05dbce409ffeaecbeb94aca
globalqss <devnull@localhost>
2007-12-03 18:11:21 +0000

    Fix type in window help Reported by Offbyte in wiki manual

---
+++ migration/330-trunk/postgresql/042_fix_typo_in_help.sql
@@ -0,0 +1,4 @@
+UPDATE AD_WINDOW
+   SET HELP =
+          'The User Window allows you to maintain User of the system.  Users can log into the system and have access to functionality via one or more roles.  A user can also be a business partner contact.'
+ WHERE ad_window_id = 108 AND HELP LIKE '%aprtner%';
