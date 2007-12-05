1acb2446a94aa3077937db0ceaae027388ea6817
globalqss <devnull@localhost>
2007-12-05 20:14:37 +0000

    Fix migration script with problems

--- migration/330-trunk/041_FR1814291.sql
+++ migration/330-trunk/041_FR1814291.sql
@@ -1,3 +1,5 @@
+SET DEFINE OFF
+
 -- Dec 1, 2007 1:51:24 AM COT
 -- FR 1814291 - Sales Commitment Offset
 UPDATE AD_REF_LIST SET NAME='PO Commitment & Reservation',Updated=TO_DATE('2007-12-01 01:51:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=785
