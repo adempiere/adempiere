42278dc97601787ac6754bd89a5fb66b2a876f7e
globalqss <devnull@localhost>
2007-12-05 23:54:16 +0000

    Fix errors in view definitions

--- db/ddlutils/oracle/views/M_PRODUCT_STOCK_V.sql
+++ db/ddlutils/oracle/views/M_PRODUCT_STOCK_V.sql
@@ -2,12 +2,12 @@
 CREATE OR REPLACE VIEW M_PRODUCT_STOCK_V
 AS
 SELECT 
-ms.AD_Client_ID, ms.AD_Org_ID, ms.IsActive, ms.Created, ms.CreatedBy, ms.Updated, ms.UpdatedBy,
-mp.value, mp.help, (ms.qtyonhand - ms.qtyreserved) AS qtyavailable, ms.qtyonhand, 
-ms.qtyreserved, mp.description, mw.name as warehouse, mw.m_warehouse_id, mw.ad_client_id, 
+ms.IsActive, ms.Created, ms.CreatedBy, ms.Updated, ms.UpdatedBy,
+mp.VALUE, mp.help, (ms.qtyonhand - ms.qtyreserved) AS qtyavailable, ms.qtyonhand, 
+ms.qtyreserved, mp.description, mw.NAME AS warehouse, mw.m_warehouse_id, mw.ad_client_id, 
 mw.ad_org_id, mp.documentnote
-FROM m_storage ms 
-JOIN m_product mp ON ms.m_product_id = mp.m_product_id
-JOIN m_locator ml ON ms.m_locator_id = ml.m_locator_id
-JOIN m_warehouse mw ON ml.m_warehouse_id = mw.m_warehouse_id 
-ORDER BY mw.name;
+FROM M_STORAGE ms 
+JOIN M_PRODUCT mp ON ms.m_product_id = mp.m_product_id
+JOIN M_LOCATOR ml ON ms.m_locator_id = ml.m_locator_id
+JOIN M_WAREHOUSE mw ON ml.m_warehouse_id = mw.m_warehouse_id 
+ORDER BY mw.NAME;
