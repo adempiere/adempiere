28bb5d97f3ae0934feeb28938b24e0803c35f2a0
globalqss <devnull@localhost>
2008-01-03 23:46:05 +0000

    FR [ 1782412 ] Add Document Action Access Functionality Beautify window appearance

---
+++ migration/331b-trunk/054_FR1782412 _beautifywindow.sql
@@ -0,0 +1,29 @@
+-- Jan 3, 2008 6:15:05 PM COT
+-- Beautify window [ 1782412 ] Add Document Action Access Functionality
+UPDATE AD_Field SET SeqNo=30,IsDisplayed='Y' WHERE AD_Field_ID=53247
+;
+
+UPDATE AD_Field SET SeqNo=40,IsDisplayed='Y' WHERE AD_Field_ID=53244
+;
+
+UPDATE AD_Field SET SeqNo=50,IsDisplayed='Y' WHERE AD_Field_ID=53246
+;
+
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=53241
+;
+
+UPDATE AD_Field SET DisplayLength=20,Updated=TO_DATE('2008-01-03 18:15:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53242
+;
+
+UPDATE AD_Field SET DisplayLength=20, IsSameLine='Y',Updated=TO_DATE('2008-01-03 18:15:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53245
+;
+
+UPDATE AD_Field SET DisplayLength=20,Updated=TO_DATE('2008-01-03 18:16:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53247
+;
+
+UPDATE AD_Field SET DisplayLength=20,Updated=TO_DATE('2008-01-03 18:16:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53244
+;
+
+UPDATE AD_Field SET DisplayLength=20,Updated=TO_DATE('2008-01-03 18:16:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53246
+;
+
