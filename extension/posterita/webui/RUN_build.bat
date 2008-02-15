304872dbcc18ee0dad678fde50e8237b667ab95d
teo_sarca <devnull@localhost>
2008-02-15 12:46:54 +0000

    extension/posterita/webui/RUN_build.bat - error when java class path contains spaces

--- extension/posterita/webui/RUN_build.bat
+++ extension/posterita/webui/RUN_build.bat
@@ -10,15 +10,16 @@
 
 @Rem Set ant classpath
 @SET ANT_CLASSPATH=%CLASSPATH%;..\..\..\tools\lib\ant.jar;..\..\..\tools\lib\ant-launcher.jar;..\..\..\tools\lib\ant-swing.jar;..\..\..\tools\lib\ant-commons-net.jar;..\..\..\tools\lib\commons-net-1.4.0.jar
-@SET ANT_CLASSPATH=%ANT_CLASSPATH%;"%JAVA_HOME%\lib\tools.jar"
+@SET ANT_CLASSPATH=%ANT_CLASSPATH%;%JAVA_HOME%\lib\tools.jar
 
 @SET ANT_OPTS=-Xms128m -Xmx512m
 
+echo %ANT_CLASSPATH%
 @echo Cleanup ...
-@"%JAVA_HOME%\bin\java" %ANT_OPTS% -classpath %ANT_CLASSPATH% -Dant.home="." org.apache.tools.ant.Main clean
+@"%JAVA_HOME%\bin\java" %ANT_OPTS% -classpath "%ANT_CLASSPATH%" -Dant.home="." org.apache.tools.ant.Main clean
 
 @echo Building ...
-@"%JAVA_HOME%\bin\java" %ANT_OPTS% -classpath %ANT_CLASSPATH% -Dant.home="." org.apache.tools.ant.Main -logger org.apache.tools.ant.listener.MailLogger war
+@"%JAVA_HOME%\bin\java" %ANT_OPTS% -classpath "%ANT_CLASSPATH%" -Dant.home="." org.apache.tools.ant.Main -logger org.apache.tools.ant.listener.MailLogger war
 @IF ERRORLEVEL 1 goto ERROR
 
 @Echo Done ...
