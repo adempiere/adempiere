4b89271b07283640ee5f4876e4eb74a1e02dc46c
globalqss <devnull@localhost>
2008-03-03 20:38:44 +0000

    1866483 - Jasper on Financial Reports It's convenient to allow executing the configured financial report with adempiere reporter, and jasper Exporting to CSV have different results from Jasper than from Adempiere reporter

---
+++ migration/331b-trunk/108_FR_1866483JasperFinancialReports.sql
@@ -0,0 +1,15 @@
+-- Jan 7, 2008 9:37:36 PM COT
+-- 1866483 - Jasper on Financial Reports
+
+-- Is convenient to allow executing the configured financial report with adempiere reporter, and jasper
+-- Exporting to CSV have different results from Jasper than from Adempiere reporter
+
+UPDATE AD_FIELD SET DisplayLogic=NULL,Updated=TO_DATE('2008-02-27 12:57:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=4737
+;
+
+UPDATE AD_FIELD SET SeqNo=150,IsDisplayed='Y' WHERE AD_Field_ID=54235
+;
+
+UPDATE AD_FIELD SET SeqNo=140,IsDisplayed='Y' WHERE AD_Field_ID=6265
+;
+
