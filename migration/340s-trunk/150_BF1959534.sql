473352e95b6a219068804affdccc9060cc115b40
teo_sarca <devnull@localhost>
2008-05-07 14:09:32 +0000

    BF [ 1959534 ] C_TaxDeclarationAcct.Fact_Acct_ID fix reference type

---
+++ migration/340s-trunk/150_BF1959534.sql
@@ -0,0 +1,4 @@
+-- May 7, 2008 4:57:14 PM EEST
+-- Default comment for updating dictionary
+UPDATE AD_Column SET AD_Reference_ID=30,Updated=TO_DATE('2008-05-07 16:57:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=14495
+;
