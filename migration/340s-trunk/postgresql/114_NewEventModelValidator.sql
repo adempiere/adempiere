af0e4282a48f2008b336de414412a90afe5c2cc2
globalqss <devnull@localhost>
2008-05-30 19:55:09 +0000

    Wrong updates in migration script

--- migration/340s-trunk/postgresql/114_NewEventModelValidator.sql
+++ migration/340s-trunk/postgresql/114_NewEventModelValidator.sql
@@ -1,18 +1,3 @@
--- Mar 5, 2008 3:39:11 AM CST
--- Default comment for updating dictionary
-UPDATE AD_SysConfig SET Value='New Event Model Validator',Updated=TO_TIMESTAMP('2008-03-05 03:39:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_SysConfig_ID=50003
-;
-
--- Mar 5, 2008 3:39:42 AM CST
--- New Event Model Validator
-UPDATE AD_SysConfig SET Value='user',Updated=TO_TIMESTAMP('2008-03-05 03:39:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_SysConfig_ID=50001
-;
-
--- Mar 5, 2008 3:39:46 AM CST
--- New Event Model Validator
-UPDATE AD_SysConfig SET Value='password',Updated=TO_TIMESTAMP('2008-03-05 03:39:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_SysConfig_ID=50002
-;
-
 -- Mar 5, 2008 3:39:53 AM CST
 -- New Event Model Validator
 INSERT INTO AD_Ref_List (AD_Client_ID,AD_Org_ID,AD_Ref_List_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,53337,53237,TO_TIMESTAMP('2008-03-05 03:39:52','YYYY-MM-DD HH24:MI:SS'),0,'D','Y','Table After New Replication',TO_TIMESTAMP('2008-03-05 03:39:52','YYYY-MM-DD HH24:MI:SS'),0,'TANR')
