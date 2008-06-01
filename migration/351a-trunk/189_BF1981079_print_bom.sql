7f3717bdca20606ac8b96082d314dcbb87bf7bb7
tspc <devnull@localhost>
2008-06-01 05:01:09 +0000

    BF1981079 Print Multi-Level Bom -fix error in Oracle script

--- migration/351a-trunk/189_BF1981079_print_bom.sql
+++ migration/351a-trunk/189_BF1981079_print_bom.sql
@@ -1,8 +1,8 @@
 -- June 1, 2008 1:20:00 PM EST
 -- BF1981079 - MultiLevel BOM & Formula Detail
-DROP VIEW "rv_pp_product_bomline";
+DROP VIEW "RV_PP_PRODUCT_BOMLINE";
 
-CREATE VIEW "rv_pp_product_bomline" ("seqno", "levelno", "levels", "ad_client_id", "ad_org_id", "createdby", "updatedby", "updated", "created", "ad_pinstance_id", "implosion", "m_product_id", "isactive", "pp_product_bom_id", "pp_product_bomline_id", "description", "iscritical", "componenttype", "tm_product_id", "c_uom_id", "issuemethod", "line", "m_attributesetinstance_id", "scrap", "validfrom", "validto", "qtybom", "qtybatch", "isqtypercentage") AS 
+CREATE VIEW "RV_PP_PRODUCT_BOMLINE" ("SEQNO", "LEVELNO", "LEVELS", "AD_CLIENT_ID", "AD_ORG_ID", "CREATEDBY", "UPDATEDBY", "UPDATED", "CREATED", "AD_PINSTANCE_ID", "IMPLOSION", "M_PRODUCT_ID", "ISACTIVE", "PP_PRODUCT_BOM_ID", "PP_PRODUCT_BOMLINE_ID", "DESCRIPTION", "ISCRITICAL", "COMPONENTTYPE", "TM_PRODUCT_ID", "C_UOM_ID", "ISSUEMETHOD", "LINE", "M_ATTRIBUTESETINSTANCE_ID", "SCRAP", "VALIDFROM", "VALIDTO", "QTYBOM", "QTYBATCH", "ISQTYPERCENTAGE") AS 
  SELECT t.seqno, t.levelno, t.levels, t.ad_client_id, t.ad_org_id, t.createdby, t.updatedby, t.updated, t.created, t.ad_pinstance_id, t.implosion, t.sel_product_id, bl.isactive, bl.pp_product_bom_id, bl.pp_product_bomline_id, bl.description, bl.iscritical, bl.componenttype, t.m_product_id, bl.c_uom_id, bl.issuemethod, bl.line, bl.m_attributesetinstance_id, bl.scrap, bl.validfrom, bl.validto, bl.qtybom, bl.qtybatch, bl.isqtypercentage
  FROM t_bomline t 
  LEFT JOIN pp_product_bomline bl ON t.pp_product_bomline_id = bl.pp_product_bomline_id 
