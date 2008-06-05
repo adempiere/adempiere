f883c985a34fc7950402b1f322ed2bf99743193d
teo_sarca <devnull@localhost>
2008-06-05 17:20:42 +0000

    BF - Internal Use Inventory / Attributes it's an advanced tab

---
+++ migration/351a-trunk/197_Fix_InternalUseWindow_Attributes.sql
@@ -0,0 +1,5 @@
+-- 05.06.2008 20:16:23 EEST
+-- Internal Use Inventory / Attributes it's an advanced tab
+UPDATE AD_Tab SET IsAdvancedTab='Y',Updated=TO_DATE('2008-06-05 20:16:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53134
+;
+
