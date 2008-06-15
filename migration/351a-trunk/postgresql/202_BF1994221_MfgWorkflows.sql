c8a5cfb12db64f84cc0b19700f8ec6af96218d87
tspc <devnull@localhost>
2008-06-15 05:10:19 +0000

    BF1994221 Mfg Workflows with wrong start node id

---
+++ migration/351a-trunk/postgresql/202_BF1994221_MfgWorkflows.sql
@@ -0,0 +1,24 @@
+-- Jun 15, 2008 12:35:47 PM EST
+-- Fix Manufacturing Workflows
+UPDATE AD_Workflow SET IsValid='Y', AD_WF_Node_ID=50029,Updated=TO_TIMESTAMP('2008-06-15 12:35:47','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Workflow_ID=50008
+;
+
+-- Jun 15, 2008 12:39:50 PM EST
+-- Fix Manufacturing Workflows
+UPDATE AD_Workflow SET IsValid='Y', AD_WF_Node_ID=50033,Updated=TO_TIMESTAMP('2008-06-15 12:39:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Workflow_ID=50009
+;
+
+-- Jun 15, 2008 1:09:43 PM EST
+-- Fix Manufacturing Workflows
+UPDATE AD_WF_NodeNext SET SeqNo=100,Updated=TO_TIMESTAMP('2008-06-15 13:09:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_WF_NodeNext_ID=50010
+;
+
+-- Jun 15, 2008 1:13:45 PM EST
+-- Fix Manufacturing Workflows
+UPDATE AD_Workflow SET IsValid='Y', AD_WF_Node_ID=50016,Updated=TO_TIMESTAMP('2008-06-15 13:13:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Workflow_ID=50005
+;
+
+-- Jun 15, 2008 1:18:01 PM EST
+-- Fix Manufacturing Workflows
+UPDATE AD_Workflow SET IsValid='Y', AD_WF_Node_ID=50020,Updated=TO_TIMESTAMP('2008-06-15 13:18:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Workflow_ID=50006
+;
