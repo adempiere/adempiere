cd89ee894c842ce7766e5d15835d17deceb422a1
tspc <devnull@localhost>
2008-06-26 00:51:34 +0000

    BF1995132 Checkbox: "Required calculate DRP" display invalid + Fixed tab level of child tabs. + Set Linked Column on transaction tab

---
+++ migration/351a-trunk/211_BF1995132.sql
@@ -0,0 +1,50 @@
+-- Jun 26, 2008 9:57:54 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Tab SET TabLevel=1,Updated=TO_DATE('2008-06-26 09:57:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53030
+;
+
+-- Jun 26, 2008 9:58:19 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Tab SET TabLevel=1,Updated=TO_DATE('2008-06-26 09:58:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53033
+;
+
+-- Jun 26, 2008 9:58:28 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Tab SET TabLevel=1,Updated=TO_DATE('2008-06-26 09:58:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53031
+;
+
+-- Jun 26, 2008 9:58:36 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Tab SET TabLevel=1,Updated=TO_DATE('2008-06-26 09:58:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53034
+;
+
+-- Jun 26, 2008 9:58:43 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Tab SET AD_Column_ID=3668,Updated=TO_DATE('2008-06-26 09:58:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53034
+;
+
+-- Jun 26, 2008 10:00:40 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET SeqNo=150,IsDisplayed='Y' WHERE AD_Field_ID=53523
+;
+
+-- Jun 26, 2008 10:00:40 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET SeqNo=160,IsDisplayed='Y' WHERE AD_Field_ID=53522
+;
+
+-- Jun 26, 2008 10:00:40 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET SeqNo=170,IsDisplayed='Y' WHERE AD_Field_ID=53525
+;
+
+-- Jun 26, 2008 10:00:40 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET SeqNo=180,IsDisplayed='Y' WHERE AD_Field_ID=53524
+;
+
+-- Jun 26, 2008 10:03:16 AM EST
+-- Default comment for updating dictionary
+UPDATE AD_Field SET IsSameLine='N',Updated=TO_DATE('2008-06-26 10:03:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=53526
+;
+
