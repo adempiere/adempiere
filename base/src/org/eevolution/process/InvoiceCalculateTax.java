91a3ff70a70873820d684bf93d5c07500df3de28
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2008-07-01 01:34:33 +0000

    simplify the comments

--- base/src/org/eevolution/process/InvoiceCalculateTax.java
+++ base/src/org/eevolution/process/InvoiceCalculateTax.java
@@ -1,51 +1,41 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2007 e-Evolution,SC. All Rights Reserved.               *
+ * Contributor(s): Victor Perez www.e-evolution.com                           */
+
 package org.eevolution.process;
 
-/******************************************************************************
- * The contents of this file are subject to the   Compiere License  Version 1.1
- * ("License"); You may not use this file except in compliance with the License
- * You may obtain a copy of the License at http://www.compiere.org/license.html
- * Software distributed under the License is distributed on an  "AS IS"  basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- * The Original Code is             Compiere  ERP & CRM Smart Business Solution
- * The Initial Developer of the Original Code is Jorg Janke  and ComPiere, Inc.
- * Portions created by Jorg Janke are Copyright (C) 1999-2003 Jorg Janke, parts
- * created by ComPiere are Copyright (C) ComPiere, Inc.;   All Rights Reserved.
-* created by Victor Perez are Copyright (C) e-Evolution,SC. All Rights Reserved.
- * Contributor(s): Victor Perez
- *****************************************************************************/
-//package org.eevolution.process;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.Properties;
-import java.util.logging.*;
 import java.math.BigDecimal;
 
 
 import org.compiere.model.*;
-import org.compiere.print.MPrintFormat;
-import org.compiere.print.ReportEngine;
-import org.compiere.process.DocAction;
-import org.compiere.process.ProcessInfo;
 import org.compiere.process.ProcessInfoParameter;
 import org.compiere.process.SvrProcess;
-import org.compiere.report.FinBalance;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
-import org.compiere.util.Language;
-import org.compiere.model.*;
 import org.compiere.util.*;
 
 
 /**
- *  BOM lines explosion for print
- *
- *        @author Sergio Ramazzina,Victor Perez
- *        @version $Id: PrintBOM.java,v 1.2 2005/04/19 12:54:30 srama Exp $
+ *  Invoice Calculate Tax let re calculate Tax Invoice
+ *  
+ *        @author Victor Perez
  */
+
 public class InvoiceCalculateTax extends SvrProcess {
 	
 	
