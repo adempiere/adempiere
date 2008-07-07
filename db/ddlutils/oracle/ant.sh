73c03f688c51710508e147526f7d49cff48a9c28
globalqss <devnull@localhost>
2008-07-07 18:38:16 +0000

    Minor - add ant.sh to help if ant not installed

---
+++ db/ddlutils/oracle/ant.sh
@@ -0,0 +1,1 @@
+java -Xms128m -Xmx512m -classpath "../../../tools/lib/ant.jar;../../../tools/lib/ant-launcher.jar;../../../tools/lib/ant-swing.jar;../../../tools/lib/ant-commons-net.jar;../../../tools/lib/commons-net.jar" -Dant.home="." org.apache.tools.ant.Main $*
