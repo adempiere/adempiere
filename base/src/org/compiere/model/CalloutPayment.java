bf534d6d1bbf7452b16e033d29ef60ce814940a3
globalqss <devnull@localhost>
2008-07-18 18:51:24 +0000

    Fix [ 2021745 ] Cannot assign project to payment with charge

--- base/src/org/compiere/model/CalloutPayment.java
+++ base/src/org/compiere/model/CalloutPayment.java
@@ -1,18 +1,19 @@
-/*******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution * Copyright (C)
- * 1999-2006 ComPiere, Inc. All Rights Reserved. * This program is free
- * software; you can redistribute it and/or modify it * under the terms version
- * 2 of the GNU General Public License as published * by the Free Software
- * Foundation. This program is distributed in the hope * that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied * warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. * See the GNU General
- * Public License for more details. * You should have received a copy of the GNU
- * General Public License along * with this program; if not, write to the Free
- * Software Foundation, Inc., * 59 Temple Place, Suite 330, Boston, MA
- * 02111-1307 USA. * For the text or an alternative of this public license, you
- * may reach us * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA
- * 95054, USA * or via info@compiere.org or http://www.compiere.org/license.html *
- ******************************************************************************/
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
 package org.compiere.model;
 
 import java.math.*;
@@ -27,6 +28,8 @@ import org.compiere.util.*;
  * @version $Id: CalloutPayment.java,v 1.3 2006/07/30 00:51:03 jjanke Exp $
  * @author Teo Sarca, SC ARHIPAC SERVICE SRL
  *         <li>BF [ 1803316 ] CalloutPayment: use C_Order.Bill_BPartner_ID
+ * @author j2garcia - GlobalQSS
+ *         <li>BF [ 2021745 ] Cannot assign project to payment with charge
  */
 public class CalloutPayment extends CalloutEngine
 {
@@ -183,26 +186,8 @@ public class CalloutPayment extends CalloutEngine
 		return docType (ctx, WindowNo, mTab, mField, value);
 	} // order
 
-	/**
-	 * Payment_Project. - reset - C_BPartner_ID, Invoice, Order, Project,
-	 * Discount, WriteOff
-	 * @param ctx context
-	 * @param WindowNo current Window No
-	 * @param mTab Grid Tab
-	 * @param mField Grid Field
-	 * @param value New Value
-	 * @return null or error message
-	 */
-	public String project(Properties ctx, int WindowNo, GridTab mTab,
-		GridField mField, Object value)
-	{
-		Integer C_Project_ID = (Integer)value;
-		if (isCalloutActive () // assuming it is resetting value
-			|| C_Project_ID == null || C_Project_ID.intValue () == 0)
-			return "";
-		mTab.setValue ("C_Charge_ID", null);
-		return "";
-	} // project
+	// 2008/07/18 Globalqss [ 2021745 ]
+	// Deleted project method
 
 	/**
 	 * Payment_Charge. - reset - C_BPartner_ID, Invoice, Order, Project,
@@ -223,7 +208,8 @@ public class CalloutPayment extends CalloutEngine
 			return "";
 		mTab.setValue ("C_Invoice_ID", null);
 		mTab.setValue ("C_Order_ID", null);
-		mTab.setValue ("C_Project_ID", null);
+		// 2008/07/18 Globalqss [ 2021745 ]
+		// mTab.setValue ("C_Project_ID", null);
 		mTab.setValue ("IsPrepayment", Boolean.FALSE);
 		//
 		mTab.setValue ("DiscountAmt", Env.ZERO);
