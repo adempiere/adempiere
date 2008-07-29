4bb3c9bcc42ec2396cb5d12cdc5be8fd43c1dece
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2008-07-29 17:37:42 +0000

    Fix small bug

--- migration/351a-trunk/postgresql/244_FR2030865DistributionRunDO.sql
+++ migration/351a-trunk/postgresql/244_FR2030865DistributionRunDO.sql
@@ -1,14 +1,11 @@
 -- Jul 29, 2008 11:00:46 AM CDT
 -- Fix Payroll bug
-INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,53675,0,'CreateDO',TO_TIMESTAMP('2008-07-29 11:00:37','YYYY-MM-DD HH24:MI:SS'),0,'EE01','Y','Create Distribution Order','
-Create Distribution Order',TO_TIMESTAMP('2008-07-29 11:00:37','YYYY-MM-DD HH24:MI:SS'),0)
+INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,53675,0,'CreateDO',TO_TIMESTAMP('2008-07-29 11:00:37','YYYY-MM-DD HH24:MI:SS'),0,'EE01','Y','Create Distribution Order','Create Distribution Order',TO_TIMESTAMP('2008-07-29 11:00:37','YYYY-MM-DD HH24:MI:SS'),0)
 ;
 
 -- Jul 29, 2008 11:00:46 AM CDT
 -- Fix Payroll bug
-INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Element_ID, t.Description,t.Help,t.Name,t.PO
-_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Element_ID=53675 AND EXI
-STS (SELECT * FROM AD_Element_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Element_ID!=t.AD_Element_ID)
+INSERT INTO AD_Element_Trl (AD_Language,AD_Element_ID, Description,Help,Name,PO_Description,PO_Help,PO_Name,PO_PrintName,PrintName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Element_ID, t.Description,t.Help,t.Name,t.PO_Description,t.PO_Help,t.PO_Name,t.PO_PrintName,t.PrintName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Element t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Element_ID=53675 AND EXISTS (SELECT * FROM AD_Element_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Element_ID!=t.AD_Element_ID)
 ;
 
 
