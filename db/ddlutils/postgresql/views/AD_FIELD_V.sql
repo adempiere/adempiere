680841ce1b867720f1a8321241a99e01ec96c1c4
globalqss <devnull@localhost>
2008-07-29 20:40:22 +0000

    Integrate Karsten's autocompletion from trunk revisions 5521 and 5670 FR [2003044] Autocomplete for Textfields http://sourceforge.net/tracker/index.php?func=detail&aid=2003044&group_id=176962&atid=879335 [Ctrl + Tab] iterates through all available matches

--- db/ddlutils/postgresql/views/AD_FIELD_V.sql
+++ db/ddlutils/postgresql/views/AD_FIELD_V.sql
@@ -1,65 +1,22 @@
-CREATE OR
-REPLACE VIEW ad_field_v AS 
-SELECT t.ad_window_id,
-	f.ad_tab_id,
-	f.ad_field_id,
-	tbl.ad_table_id,
-	f.ad_column_id,
-	f.NAME,
-	f.description,
-	f.help,
-	f.isdisplayed,
-	f.displaylogic,
-	f.displaylength,
-	f.seqno,
-	f.sortno,
-	f.issameline,
-	f.isheading,
-	f.isfieldonly,
-	f.isreadonly,
-	f.isencrypted                                  AS isencryptedfield,
-	f.obscuretype,
-	c.columnname,
-	c.columnsql,
-	c.fieldlength,
-	c.vformat,
-	COALESCE(f.defaultvalue, c.defaultvalue) AS defaultvalue,
-	c.iskey,
-	c.isparent,
-	COALESCE(f.ismandatory, c.ismandatory)         AS ismandatory,
-	c.isidentifier,
-	c.istranslated,
-	COALESCE(f.ad_reference_value_id, c.ad_reference_value_id) AS ad_reference_value_id,
-	c.callout,
-	COALESCE(f.ad_reference_id, c.ad_reference_id) AS ad_reference_id,
-	COALESCE(f.ad_val_rule_id, c.ad_val_rule_id) AS ad_val_rule_id,
-	c.ad_process_id,
-	c.isalwaysupdateable,
-	c.readonlylogic,
-	c.isupdateable,
-	c.isencrypted                                  AS isencryptedcolumn,
-	c.isselectioncolumn,
-	tbl.tablename,
-	c.valuemin,
-	c.valuemax,
-	fg.NAME                                        AS fieldgroup,
-	vr.code                                        AS validationcode,
-	f.Included_Tab_ID,
-	fg.FieldGroupType,
-	fg.IsCollapsedByDefault						   AS iscollapsedbydefault,
-	COALESCE(f.infofactoryclass, c.infofactoryclass) as infofactoryclass 
-FROM ((((((AD_FIELD f 
-		JOIN AD_TAB t 
-		ON ((f.ad_tab_id = t.ad_tab_id))) 
-		LEFT JOIN AD_FIELDGROUP fg 
-		ON ((f.ad_fieldgroup_id = fg.ad_fieldgroup_id))) 
-		LEFT JOIN AD_COLUMN c 
-		ON ((f.ad_column_id = c.ad_column_id))) 
-		JOIN AD_TABLE tbl 
-		ON ((c.ad_table_id = tbl.ad_table_id))) 
-		JOIN AD_REFERENCE r 
-		ON ((c.ad_reference_id = r.ad_reference_id))) 
-		LEFT JOIN AD_VAL_RULE vr 
-		ON ((vr.ad_val_rule_id = COALESCE(f.ad_val_rule_id, c.ad_val_rule_id)))) 
-WHERE ((f.isactive = 'Y'::bpchar) AND
-	(c.isactive = 'Y'::bpchar));
+CREATE OR REPLACE VIEW ad_field_v AS 
+ SELECT t.ad_window_id, f.ad_tab_id, f.ad_field_id, tbl.ad_table_id, f.ad_column_id, 
+ f.name, f.description, f.help, f.isdisplayed, f.displaylogic, f.displaylength, f.seqno, 
+ f.sortno, f.issameline, f.isheading, f.isfieldonly, f.isreadonly, f.isencrypted AS isencryptedfield, 
+ f.obscuretype, c.columnname, c.columnsql, c.fieldlength, c.vformat, 
+ COALESCE(f.defaultvalue, c.defaultvalue) AS defaultvalue, c.iskey, c.isparent, 
+ COALESCE(f.ismandatory, c.ismandatory) AS ismandatory, c.isidentifier, c.istranslated, 
+ COALESCE(f.ad_reference_value_id, c.ad_reference_value_id) AS ad_reference_value_id, c.callout, 
+ COALESCE(f.ad_reference_id, c.ad_reference_id) AS ad_reference_id, 
+ COALESCE(f.ad_val_rule_id, c.ad_val_rule_id) AS ad_val_rule_id, c.ad_process_id, 
+ c.isalwaysupdateable, c.readonlylogic, c.mandatorylogic, c.isupdateable, 
+ c.isencrypted AS isencryptedcolumn, c.isselectioncolumn, tbl.tablename, c.valuemin, c.valuemax, 
+ fg.name AS fieldgroup, vr.code AS validationcode, f.included_tab_id, fg.fieldgrouptype, fg.iscollapsedbydefault, 
+ COALESCE(f.infofactoryclass, c.infofactoryclass) AS infofactoryclass, c.isautocomplete
+   FROM ad_field f
+   JOIN ad_tab t ON f.ad_tab_id = t.ad_tab_id
+   LEFT JOIN ad_fieldgroup fg ON f.ad_fieldgroup_id = fg.ad_fieldgroup_id
+   LEFT JOIN ad_column c ON f.ad_column_id = c.ad_column_id
+   JOIN ad_table tbl ON c.ad_table_id = tbl.ad_table_id
+   JOIN ad_reference r ON c.ad_reference_id = r.ad_reference_id
+   LEFT JOIN ad_val_rule vr ON vr.ad_val_rule_id = COALESCE(f.ad_val_rule_id, c.ad_val_rule_id)
+  WHERE f.isactive = 'Y'::bpchar AND c.isactive = 'Y'::bpchar;
