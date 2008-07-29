6f7a179f2b51af96a271a5da840435b1b0ab1816
teo_sarca <devnull@localhost>
2008-07-29 09:02:45 +0000

    BF [ 2030865 ] Let use the Distribution List and Distribution Run for DO

--- migration/351a-trunk/244_FR2030865DistributionRunDO.sql
+++ migration/351a-trunk/244_FR2030865DistributionRunDO.sql
@@ -115,8 +115,7 @@ Insert INTO AD_TREENODEMM(AD_Client_ID, AD_Org_ID, CreatedBy, UpdatedBy, Parent_
 
 -- Jul 28, 2008 8:59:40 PM CDT
 -- Distribution Run Orders
-UPDATE AD_Val_Rule SET Code='C_DocType.DocBaseType IN (''SOO'', ''POO'',''DOO'')',Updated=TO_DATE('2008-07-28 20:59:40','YYYY-MM-D
-D HH24:MI:SS'),UpdatedBy=0 WHERE AD_Val_Rule_ID=172
+UPDATE AD_Val_Rule SET Code='C_DocType.DocBaseType IN (''SOO'', ''POO'',''DOO'')',Updated=TO_DATE('2008-07-28 20:59:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Val_Rule_ID=172
 ;
 
 
