e90123c33fc442804a2089cf77c2bab454f09866
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2008-08-05 15:48:51 +0000

    [ 2038848 ] Product Planning window - can't save if Fixed Order Qty http://sourceforge.net/tracker/?func=detail&atid=879332&aid=2038848&group_id=176962

---
+++ migration/352a-trunk/postgresql/258_FB_2038848_OrderPolicy.sql
@@ -0,0 +1,10 @@
+-- Aug 5, 2008 10:34:58 AM CDT
+-- Manufaturing
+UPDATE AD_Ref_List SET Name='Fixed Order Quantity', Value='FOQ',Updated=TO_TIMESTAMP('2008-08-05 10:34:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Ref_List_ID=53272
+;
+
+-- Aug 5, 2008 10:34:58 AM CDT
+-- Manufaturing
+UPDATE AD_Ref_List_Trl SET IsTranslated='N' WHERE AD_Ref_List_ID=53272
+;
+
