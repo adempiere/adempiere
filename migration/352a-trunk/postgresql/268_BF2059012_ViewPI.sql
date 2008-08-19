c3d4b0f2842ab3a3c8e0025d68aa4521fe8a6d07
tspc <devnull@localhost>
2008-08-19 06:00:19 +0000

    BF2059012 Missing class ViewPI

---
+++ migration/352a-trunk/postgresql/268_BF2059012_ViewPI.sql
@@ -0,0 +1,4 @@
+-- Aug 19, 2008 3:25:12 PM EST
+-- BF2059012_ViewPI
+UPDATE AD_Form SET Classname='org.adempiere.apps.graph.ViewPI',Updated=TO_TIMESTAMP('2008-08-19 15:25:12','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Form_ID=119
+;
