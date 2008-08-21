a408b7149d552de724809ecbf2e66219517c6384
teo_sarca <devnull@localhost>
2008-08-21 12:15:40 +0000

    BF [ 2064671 ] Column AD_Process.Classname too small

---
+++ migration/352a-trunk/270_BF2064671.sql
@@ -0,0 +1,15 @@
+-- Aug 21, 2008 3:09:16 PM EEST
+-- 
+UPDATE AD_Column SET FieldLength=255,Updated=TO_DATE('2008-08-21 15:09:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=4656
+;
+
+-- Aug 21, 2008 3:09:19 PM EEST
+-- 
+ALTER TABLE AD_Process MODIFY Classname NVARCHAR2(255) DEFAULT  NULL 
+;
+
+-- Aug 21, 2008 3:10:08 PM EEST
+-- 
+UPDATE AD_Column SET AD_Reference_ID=10,Updated=TO_DATE('2008-08-21 15:10:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=50182
+;
+
