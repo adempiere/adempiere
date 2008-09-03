8bb00ee1f267f90a4d9197fcded0d3f8201bd735
teo_sarca <devnull@localhost>
2008-09-03 09:54:00 +0000

    BF [ 2090815 ] Percent utilization signum in S_Resource

---
+++ migration/352a-trunk/284_BF2090815.sql
@@ -0,0 +1,25 @@
+-- Sep 3, 2008 12:46:45 PM EEST
+-- 
+UPDATE AD_Column SET AD_Reference_ID=22, DefaultValue='100', IsMandatory='Y', ValueMin='0',Updated=TO_DATE('2008-09-03 12:46:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=53272
+;
+
+-- Sep 3, 2008 12:46:49 PM EEST
+-- 
+ALTER TABLE S_Resource MODIFY PercentUtilization NUMBER DEFAULT 100
+;
+
+-- Sep 3, 2008 12:46:49 PM EEST
+-- 
+UPDATE S_Resource SET PercentUtilization=100 WHERE PercentUtilization IS NULL
+;
+
+-- Sep 3, 2008 12:46:49 PM EEST
+-- 
+ALTER TABLE S_Resource MODIFY PercentUtilization NOT NULL
+;
+
+-- Sep 3, 2008 12:46:49 PM EEST
+-- 
+ALTER TABLE S_Resource ADD CONSTRAINT CHK_COL_53272 CHECK (PercentUtilization>=0)
+;
+
