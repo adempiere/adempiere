8ee2bce53fda52e2f412c3b84cb00bc308a7ebad
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2008-09-23 22:10:20 +0000

    Implementing forecast report https://sourceforge.net/tracker/index.php?func=detail&aid=2002744&group_id=176962&atid=879335

---
+++ db/ddlutils/oracle/views/RV_M_FORECAST_PERIOD.sql
@@ -0,0 +1,16 @@
+--DROP VIEW RV_M_Forecast_Period;
+CREATE OR REPLACE VIEW RV_M_Forecast_Period AS
+SELECT AD_Client_ID , 
+AD_Org_ID, 
+M_Forecast_ID , 
+MAX(Name) , 
+C_Calendar_ID , 
+C_Year_ID , 
+C_Period_ID, 
+M_Product_ID , 
+C_UOM_ID, 
+SUM(Qty) AS Qty,
+SUM(QtyCalculated) AS QtyCalculated,
+SUM(TotalAmt) AS Total
+FROM RV_M_Forecast f GROUP BY AD_Client_ID , AD_Org_ID , M_Forecast_ID , M_Product_ID , C_UOM_ID, C_Calendar_ID , C_Year_ID , C_Period_ID 
+;
