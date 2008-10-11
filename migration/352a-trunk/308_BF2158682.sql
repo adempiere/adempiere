89cd21b5699f5d5a3690f947e1265a4228aed710
globalqss <devnull@localhost>
2008-10-11 04:05:22 +0000

    Fix bug 2158682 - Import Loader Format window showing replication tables

---
+++ migration/352a-trunk/308_BF2158682.sql
@@ -0,0 +1,5 @@
+-- Oct 10, 2008 11:01:13 PM COT
+-- Bug 2158682 - Import Loader Format window showing replication tables
+UPDATE AD_Val_Rule SET Code='AD_Table.TableName LIKE ''I#_%'' ESCAPE ''#''',Updated=TO_DATE('2008-10-10 23:01:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=141
+;
+
