9621f0f69b5995980b5653ce2e0244211cae3e63
teo_sarca <devnull@localhost>
2008-12-11 13:25:25 +0000

    BF [ 2417012 ] Create PO from Requsition display all requisitions

---
+++ migration/352a-trunk/postgresql/355_BF2417012.sql
@@ -0,0 +1,18 @@
+-- 11.12.2008 15:19:17 EET
+-- [ 2417012 ] Create PO from Requsition display all requisitions
+INSERT INTO AD_Val_Rule (AD_Client_ID,AD_Org_ID,AD_Val_Rule_ID,Code,Created,CreatedBy,EntityType,IsActive,Name,Type,Updated,UpdatedBy) VALUES (0,0,52037,'M_Requisition.DocStatus IN (''CO'')
+AND EXISTS (SELECT 1 FROM M_RequisitionLine rl
+        WHERE rl.M_Requisition_ID=M_Requisition.M_Requisition_ID
+              AND rl.C_OrderLine_ID IS NULL)',TO_TIMESTAMP('2008-12-11 15:19:15','YYYY-MM-DD HH24:MI:SS'),0,'D','Y','M_Requistion PO Create Candidate','S',TO_TIMESTAMP('2008-12-11 15:19:15','YYYY-MM-DD HH24:MI:SS'),0)
+;
+
+-- 11.12.2008 15:19:17 EET
+-- [ 2417012 ] Create PO from Requsition display all requisitions
+INSERT INTO AD_Val_Rule_Trl (AD_Language,AD_Val_Rule_ID, Description, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Val_Rule_ID, t.Description, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Val_Rule t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Val_Rule_ID=52037 AND EXISTS (SELECT * FROM AD_Val_Rule_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Val_Rule_ID!=t.AD_Val_Rule_ID)
+;
+
+-- 11.12.2008 15:19:55 EET
+-- [ 2417012 ] Create PO from Requsition display all requisitions
+UPDATE AD_Process_Para SET AD_Val_Rule_ID=52037,Updated=TO_TIMESTAMP('2008-12-11 15:19:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Process_Para_ID=697
+;
+
