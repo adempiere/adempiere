e3a504f6669fd0351cf79bbcd18d14aebbed8e02
globalqss <devnull@localhost>
2009-01-21 18:37:07 +0000

    Fix [2526697] - Product Substitute and Related tabs broken https://sourceforge.net/tracker2/?func=detail&aid=2526697&group_id=176962&atid=879332

---
+++ migration/353a-trunk/postgresql/401_BF2526697.sql
@@ -0,0 +1,17 @@
+-- Jan 21, 2009 1:25:48 PM COT
+-- Product Substitute and Related tabs broken
+UPDATE AD_Tab SET AD_Column_ID=1782,Updated=TO_TIMESTAMP('2009-01-21 13:25:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=181
+;
+
+UPDATE AD_Tab SET AD_Column_ID=10857,Updated=TO_TIMESTAMP('2009-01-21 13:25:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=630
+;
+
+UPDATE AD_Column SET IsParent='Y', IsUpdateable='N',Updated=TO_TIMESTAMP('2009-01-21 13:27:04','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=1783
+;
+
+UPDATE AD_Column SET IsParent='Y', IsUpdateable='N',Updated=TO_TIMESTAMP('2009-01-21 13:27:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=10852
+;
+
+UPDATE AD_Column SET IsParent='Y', IsUpdateable='N',Updated=TO_TIMESTAMP('2009-01-21 13:27:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=10853
+;
+
