79faf3db3d61de1d479c44471066631119bbaa7c
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-01-27 06:58:25 +0000

    Remove material issues and receipt from Activity Control http://sourceforge.net/tracker2/?func=detail&aid=2530189&group_id=176962&atid=879332

---
+++ migration/353a-trunk/oracle/404_BF2530189_ActivityControl.sql
@@ -0,0 +1,5 @@
+-- Jan 27, 2009 12:39:32 AM ECT
+-- Remove material issues and receipt from Activity Control
+UPDATE AD_Tab SET WhereClause='CostCollectorType=''160''',Updated=TO_DATE('2009-01-27 00:39:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53049
+;
+
