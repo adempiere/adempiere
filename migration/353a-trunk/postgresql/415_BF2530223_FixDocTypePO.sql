0dbffe4acacf8ca92e82da5151c3dc7705c5a639
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-02-06 23:29:48 +0000

    It creates incorrect document type for PO from outsourced http://sourceforge.net/tracker/?func=detail&atid=879332&aid=2530223&group_id=176962

---
+++ migration/353a-trunk/postgresql/415_BF2530223_FixDocTypePO.sql
@@ -0,0 +1,5 @@
+-- Feb 6, 2009 6:24:58 PM ECT
+-- Fix DocType Purchase Order
+UPDATE C_DocType SET IsDefault='Y',Updated=TO_TIMESTAMP('2009-02-06 18:24:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_DocType_ID=126
+;
+
