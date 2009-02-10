7f270e0e05ea81bcc1a04c2f5b9e9888b8c6d5f3
teo_sarca <devnull@localhost>
2009-02-10 21:28:49 +0000

    BF [ 2583992 ] error: nulljava.lang.ClassNotFoundException: org.eevolution. https://sourceforge.net/tracker/index.php?func=detail&aid=2583992&group_id=176962&atid=879332

---
+++ migration/353a-trunk/oracle/414_004_BF2583992_Enable_Validator.sql
@@ -0,0 +1,5 @@
+-- Feb 10, 2009 11:27:30 PM EET
+-- 
+UPDATE AD_ModelValidator SET IsActive='Y', SeqNo=1,Updated=TO_DATE('2009-02-10 23:27:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_ModelValidator_ID=50000
+;
+
---
+++ migration/353a-trunk/postgresql/418_BF2583992.sql
@@ -0,0 +1,5 @@
+-- Feb 10, 2009 11:22:27 PM EET
+-- 
+UPDATE AD_ModelValidator SET IsActive='N', Updated=TO_TIMESTAMP('2009-02-10 23:22:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_ModelValidator_ID=50000
+;
+
