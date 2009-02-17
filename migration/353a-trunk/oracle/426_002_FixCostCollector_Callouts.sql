3061689700237715f46c4e404fdedec87ddc5dc5
teo_sarca <devnull@localhost>
2009-02-17 14:30:25 +0000

    libero: fix cost collector callout names

---
+++ migration/353a-trunk/oracle/426_002_FixCostCollector_Callouts.sql
@@ -0,0 +1,10 @@
+-- Feb 17, 2009 4:26:46 PM EET
+-- 
+UPDATE AD_Column SET Callout='org.eevolution.model.CalloutCostCollector.order',Updated=TO_DATE('2009-02-17 16:26:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=53830
+;
+
+-- Feb 17, 2009 4:27:08 PM EET
+-- 
+UPDATE AD_Column SET Callout=NULL,Updated=TO_DATE('2009-02-17 16:27:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=53810
+;
+
