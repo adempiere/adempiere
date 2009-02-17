1acc102bd87da0b86456ba209a057a7526689660
teo_sarca <devnull@localhost>
2009-02-17 19:25:39 +0000

    [ 1658905 ] Create PO from requisition not working for org "*" https://sourceforge.net/tracker/index.php?func=detail&aid=1658905&group_id=176962&atid=879332
    Changed table AccessLevel for M_Requisition and M_RequisitionLine from CLient+Org to Org.

---
+++ migration/353a-trunk/oracle/428_BF1658905.sql
@@ -0,0 +1,10 @@
+-- Feb 17, 2009 9:21:53 PM EET
+-- 
+UPDATE AD_Table SET AccessLevel='1',Updated=TO_DATE('2009-02-17 21:21:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Table_ID=702
+;
+
+-- Feb 17, 2009 9:22:06 PM EET
+-- 
+UPDATE AD_Table SET AccessLevel='1',Updated=TO_DATE('2009-02-17 21:22:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Table_ID=703
+;
+
