bbc6dd9315f3c2998d3634dd5aea50309b0b7786
teo_sarca <devnull@localhost>
2009-02-18 10:29:49 +0000

    [ 2612129 ] Manufacturing window included tab not refreshed https://sourceforge.net/tracker/index.php?func=detail&aid=2612129&group_id=176962&atid=934929
    As a workaround i changed the Manufacturing Order Window, Components Tab and i set the where clause: PP_Order_ID=@PP_Order_ID@ AND PP_Order_BOM_ID=@PP_Order_BOM_ID@
    More comments regarding this Swing issue on proper tracker (see #2596107).

---
+++ migration/353a-trunk/oracle/426_003_BF2612129.sql
@@ -0,0 +1,5 @@
+-- Feb 18, 2009 12:13:16 PM EET
+-- 
+UPDATE AD_Tab SET WhereClause='PP_Order_ID=@PP_Order_ID@ AND PP_Order_BOM_ID=@PP_Order_BOM_ID@',Updated=TO_DATE('2009-02-18 12:13:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53039
+;
+
