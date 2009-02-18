98efd081160c1d65af0f01f9dc19324cf2ca4c56
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-02-18 01:05:07 +0000

    v.353a - Error: NotSupported Component Type OP  http://sourceforge.net/tracker/?func=detail&atid=934929&aid=2539813&group_id=176962

---
+++ migration/353a-trunk/postgresql/429_BF2539813.sql
@@ -0,0 +1,5 @@
+-- Feb 17, 2009 5:20:59 PM ECT
+-- Libero
+UPDATE PP_Product_BOMLine SET ComponentType='CO',Updated=TO_TIMESTAMP('2009-02-17 17:20:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE PP_Product_BOMLine_ID=102
+;
+
