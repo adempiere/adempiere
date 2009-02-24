297e1da47f006173d1fd6621530e363417b446e7
teo_sarca <devnull@localhost>
2009-02-24 10:39:40 +0000

    [ 2571538 ] Units by Cycle do not is implement https://sourceforge.net/tracker/index.php?func=detail&aid=2571538&group_id=176962&atid=934929

---
+++ migration/353a-trunk/oracle/426_005_FR2571538_UnitsCycles.sql
@@ -0,0 +1,5 @@
+-- Feb 24, 2009 11:04:25 AM EET
+-- 
+UPDATE AD_Column SET Callout='org.eevolution.model.CalloutCostCollector.duration',Updated=TO_DATE('2009-02-24 11:04:23','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=53826
+;
+
