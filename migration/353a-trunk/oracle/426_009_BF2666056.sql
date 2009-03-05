edd4f17c4bbd24646bd27adfb6146b3c01a89b1d
teo_sarca <devnull@localhost>
2009-03-05 19:54:00 +0000

    [ 2666056 ] Product Planning: do not allow to create products https://sourceforge.net/tracker/index.php?func=detail&aid=2666056&group_id=176962&atid=934929

---
+++ migration/353a-trunk/oracle/426_009_BF2666056.sql
@@ -0,0 +1,5 @@
+-- 05.03.2009 21:51:10 EET
+-- [ 2664599 ] Fix CreateCostElement process parameters
+UPDATE AD_Tab SET IsInsertRecord='N',Updated=TO_DATE('2009-03-05 21:51:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53032
+;
+
