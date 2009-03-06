767a039d47a460614a0d6fd3a209f342b7df741d
teo_sarca <devnull@localhost>
2009-03-06 12:41:05 +0000

    [ 2668133 ] Inactivate deprecated/incomplete processes https://sourceforge.net/tracker/index.php?func=detail&aid=2668133&group_id=176962&atid=934929
    Deactivated:
    Product Costing
    Cost Workflow & Process Details

---
+++ migration/353a-trunk/postgresql/426_010_BF2668133_Inactivate_Old_Processes.sql
@@ -0,0 +1,20 @@
+-- 06.03.2009 08:58:57 EET
+-- 
+UPDATE AD_Process SET IsActive='N',Updated=TO_TIMESTAMP('2009-03-06 08:58:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Process_ID=53054
+;
+
+-- 06.03.2009 08:58:57 EET
+-- 
+UPDATE AD_Menu SET Name='Product Costing', IsActive='N', Description='To watch the cost elements for every set of Product, Organization, Accounting Schema, Warehouse, Resource and Cost Type',Updated=TO_TIMESTAMP('2009-03-06 08:58:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Menu_ID=53077
+;
+
+-- 06.03.2009 09:04:28 EET
+-- 
+UPDATE AD_Process SET IsActive='N',Updated=TO_TIMESTAMP('2009-03-06 09:04:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Process_ID=53060
+;
+
+-- 06.03.2009 09:04:28 EET
+-- 
+UPDATE AD_Menu SET Name='Cost Workflow & Process Details', IsActive='N', Description='This report show every cost element to a BOM or Formula ',Updated=TO_TIMESTAMP('2009-03-06 09:04:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Menu_ID=53080
+;
+
