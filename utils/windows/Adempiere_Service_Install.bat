c42583034c66311c6f9c1ed804fa5346593d7f92
hengsin <devnull@localhost>
2009-03-06 03:07:38 +0000

    Incorporate suggestion from this form post - https://sourceforge.net/forum/message.php?msg_id=6610130

--- utils/windows/Adempiere_Service_Install.bat
+++ utils/windows/Adempiere_Service_Install.bat
@@ -4,7 +4,7 @@ REM Adempiere_Service_Install.bat - globalqss - based on http://javaservice.obje
 
 if (%ADEMPIERE_HOME%) == () (CALL myEnvironment.bat Server) else (CALL %ADEMPIERE_HOME%\utils\myEnvironment.bat Server)
 
-%ADEMPIERE_HOME%\utils\windows\AdempiereService.exe -install ADempiere "%JAVA_HOME%\jre\bin\server\jvm.dll" -Xmx256M -Djava.class.path="%JAVA_HOME%\lib\tools.jar";"%ADEMPIERE_HOME%\jboss\bin\run.jar" -server %ADEMPIERE_JAVA_OPTIONS% -Djetty.port=%ADEMPIERE_WEB_PORT% -Djetty.ssl=%ADEMPIERE_SSL_PORT% -Djetty.keystore=%ADEMPIERE_KEYSTORE% -Djetty.password=%ADEMPIERE_KEYSTORE_PASSWORD% -start org.jboss.Main -params -c adempiere -stop org.jboss.Main -method systemExit -out "%ADEMPIERE_HOME%\jboss\bin\out.txt" -current "%ADEMPIERE_HOME%\jboss\bin" -manual -overwrite -description "ADempiere JBoss server"
+%ADEMPIERE_HOME%\utils\windows\AdempiereService.exe -install ADempiere "%JAVA_HOME%\jre\bin\server\jvm.dll" -Xmx256M -Djava.class.path="%JAVA_HOME%\lib\tools.jar";"%ADEMPIERE_HOME%\jboss\bin\run.jar" -server %ADEMPIERE_JAVA_OPTIONS% -Djetty.port=%ADEMPIERE_WEB_PORT% -Djetty.ssl=%ADEMPIERE_SSL_PORT% -Djetty.keystore=%ADEMPIERE_KEYSTORE% -Djetty.password=%ADEMPIERE_KEYSTORE_PASSWORD% -start org.jboss.Main -params -c adempiere -b %ADEMPIERE_APPS_SERVER% -stop org.jboss.Main -method systemExit -out "%ADEMPIERE_HOME%\jboss\bin\out.txt" -current "%ADEMPIERE_HOME%\jboss\bin" -manual -overwrite -description "ADempiere JBoss server"
 
 rem IT DEPENDS ON OTHER SERVICE?
 rem add this option for dependency with postgresql service (replace with proper service name)
