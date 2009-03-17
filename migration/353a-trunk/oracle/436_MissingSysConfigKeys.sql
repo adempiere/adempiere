17fd5c50b01ef15eee866ac9ec8b651a49e38d31
globalqss <devnull@localhost>
2009-03-17 05:53:47 +0000

    Missing Sysconfig keys

---
+++ migration/353a-trunk/oracle/436_MissingSysConfigKeys.sql
@@ -0,0 +1,33 @@
+-- Mar 17, 2009 12:16:31 AM COT
+-- Missing system configurator keys
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50018,'S',TO_DATE('2009-03-17 00:16:11','YYYY-MM-DD HH24:MI:SS'),100,'Default paging size for grid view in zk webui','D','Y','ZK_PAGING_SIZE',TO_DATE('2009-03-17 00:16:11','YYYY-MM-DD HH24:MI:SS'),100,'10')
+;
+
+-- Mar 17, 2009 12:19:46 AM COT
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50019,'S',TO_DATE('2009-03-17 00:19:37','YYYY-MM-DD HH24:MI:SS'),100,'Grid view will enter in edit mode','D','Y','ZK_GRID_EDIT_MODELESS',TO_DATE('2009-03-17 00:19:37','YYYY-MM-DD HH24:MI:SS'),100,'Y')
+;
+
+-- Mar 17, 2009 12:20:14 AM COT
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50020,'S',TO_DATE('2009-03-17 00:20:03','YYYY-MM-DD HH24:MI:SS'),100,'Milliseconds of wait to run the dashboard refresh on zk webui client','D','Y','ZK_DASHBOARD_REFRESH_INTERVAL',TO_DATE('2009-03-17 00:20:03','YYYY-MM-DD HH24:MI:SS'),100,'60000')
+;
+
+-- Mar 17, 2009 12:21:25 AM COT
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50021,'S',TO_DATE('2009-03-17 00:21:14','YYYY-MM-DD HH24:MI:SS'),100,'package+classname of zk desktop class','D','Y','ZK_DESKTOP_CLASS',TO_DATE('2009-03-17 00:21:14','YYYY-MM-DD HH24:MI:SS'),100,'org.adempiere.webui.desktop.DefaultDesktop')
+;
+
+-- Mar 17, 2009 12:23:29 AM COT
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50022,'S',TO_DATE('2009-03-17 00:23:20','YYYY-MM-DD HH24:MI:SS'),100,'Enable/disable validation -> Select the Product UoM as the From Unit of Measure','D','Y','ProductUOMConversionUOMValidate',TO_DATE('2009-03-17 00:23:20','YYYY-MM-DD HH24:MI:SS'),100,'Y')
+;
+
+-- Mar 17, 2009 12:23:57 AM COT
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50023,'S',TO_DATE('2009-03-17 00:23:43','YYYY-MM-DD HH24:MI:SS'),100,'Enable/disable validation -> The Product UoM needs to be the smallest UoM - Multiplier must be > 0','D','Y','ProductUOMConversionRateValidate',TO_DATE('2009-03-17 00:23:43','YYYY-MM-DD HH24:MI:SS'),100,'Y')
+;
+
+-- Mar 17, 2009 12:24:48 AM COT
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50024,'S',TO_DATE('2009-03-17 00:24:37','YYYY-MM-DD HH24:MI:SS'),100,'Milliseconds of wait to run the infoupdater class on the menu window','D','Y','MENU_INFOUPDATER_SLEEP_MS',TO_DATE('2009-03-17 00:24:37','YYYY-MM-DD HH24:MI:SS'),100,'60000')
+;
+
+-- Mar 17, 2009 12:26:34 AM COT
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50025,'S',TO_DATE('2009-03-17 00:26:23','YYYY-MM-DD HH24:MI:SS'),100,'url for the logo in zkwebui','D','Y','WEBUI_LOGOURL',TO_DATE('2009-03-17 00:26:23','YYYY-MM-DD HH24:MI:SS'),100,'/images/AD10030.png')
+;
+
