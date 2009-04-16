7b01bba8fd12c66fc724e72ca83547ac9f4cbb04
teo_sarca <devnull@localhost>
2009-04-16 15:02:59 +0000

    Cannot add new node from Workflow window https://sourceforge.net/tracker/?func=detail&aid=2768731&group_id=176962&atid=879332
    Fixed for AD_WF_Node.AD_Client_ID and AD_WF_NodeNext.AD_Client_ID

---
+++ migration/353a-trunk/oracle/447_BF2768731.sql
@@ -0,0 +1,10 @@
+-- 16.04.2009 17:57:33 EEST
+-- 
+UPDATE AD_Column SET DefaultValue='@#AD_Client_ID@',Updated=TO_DATE('2009-04-16 17:57:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=434
+;
+
+-- 16.04.2009 17:58:02 EEST
+-- 
+UPDATE AD_Column SET DefaultValue='@#AD_Client_ID@',Updated=TO_DATE('2009-04-16 17:58:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=755
+;
+
