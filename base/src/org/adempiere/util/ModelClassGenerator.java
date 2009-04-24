cca647ddd2099a9a7e80953dec675e5ac46959be
globalqss <devnull@localhost>
2009-04-24 17:59:33 +0000

    Small refactoring for BF [ 2780468 ] ModelClassGenerator: not generating methods for Created* https://sourceforge.net/tracker/?func=detail&aid=2780468&group_id=176962&atid=879332

--- base/src/org/adempiere/util/ModelClassGenerator.java
+++ base/src/org/adempiere/util/ModelClassGenerator.java
@@ -276,10 +276,7 @@ public class ModelClassGenerator
 			+ " c.Name, c.Description, c.ColumnSQL, c.IsEncrypted, c.IsKey, c.IsIdentifier "  // 13..18
 			+ "FROM AD_Column c "
 			+ "WHERE c.AD_Table_ID=?"
-			+ " AND c.ColumnName <> 'AD_Client_ID'"
-			+ " AND c.ColumnName <> 'AD_Org_ID'"
-			+ " AND c.ColumnName <> 'IsActive'"
-			+ " AND c.ColumnName NOT IN ('Created', 'CreatedBy', 'Updated', 'UpdatedBy')"
+			+ " AND c.ColumnName NOT IN ('AD_Client_ID', 'AD_Org_ID', 'IsActive', 'Created', 'CreatedBy', 'Updated', 'UpdatedBy')"
 			+ " AND c.IsActive='Y'"
 			+ " ORDER BY c.ColumnName";
 		boolean isKeyNamePairCreated = false; // true if the method "getKeyNamePair" is already generated
