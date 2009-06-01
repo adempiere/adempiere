55c004783a2917c1b07e622b3c9d8e3d70d8bf76
hengsin <devnull@localhost>
2009-06-01 08:40:44 +0000

    https://sourceforge.net/tracker/index.php?func=detail&aid=1756793&group_id=176962&atid=879334 - Hide vendor return doctype from purchase order - Hide customer return doctype from sales order

---
+++ migration/353a-trunk/postgresql/474_FR1756793.sql
@@ -0,0 +1,8 @@
+-- Jun 1, 2009 4:30:09 PM MYT
+-- RMA Feature - ID: 1756793
+UPDATE AD_Val_Rule SET Code='C_DocType.DocBaseType IN (''SOO'', ''POO'') AND C_DocType.IsSOTrx=''@IsSOTrx@'' AND COALESCE(C_DocType.DocSubTypeSO,'' '')<>''RM''',Updated=TO_TIMESTAMP('2009-06-01 16:30:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=133
+;
+
+COMMIT;
+
+
