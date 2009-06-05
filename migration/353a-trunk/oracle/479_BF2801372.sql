72b039f2c1dace104ba4acfd2397236f0f42acd9
tspc <devnull@localhost>
2009-06-05 22:39:05 +0000

    BF2801372 NO Data found for C_BPartner_ID=50006
    https://sourceforge.net/tracker/?func=detail&aid=2801372&group_id=176962&atid=879332

---
+++ migration/353a-trunk/oracle/479_BF2801372.sql
@@ -0,0 +1,15 @@
+-- Jun 6, 2009 7:13:11 AM EST
+-- BF2801372 NO Data found for C_BPartner_ID=50006
+DELETE FROM AD_WF_EventAudit WHERE AD_WF_EventAudit_ID=50006
+;
+
+-- Jun 6, 2009 7:13:11 AM EST
+-- BF2801372 NO Data found for C_BPartner_ID=50006
+DELETE FROM AD_WF_Activity WHERE AD_WF_Activity_ID=50006
+;
+
+-- Jun 6, 2009 7:13:11 AM EST
+-- BF2801372 NO Data found for C_BPartner_ID=50006
+DELETE FROM AD_WF_Process WHERE AD_WF_Process_ID=50006
+;
+
