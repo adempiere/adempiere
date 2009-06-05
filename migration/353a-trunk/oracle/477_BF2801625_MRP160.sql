f65bb6814badddb040eb36b99461bc5a81922c16
teo_sarca <devnull@localhost>
2009-06-05 10:12:40 +0000

    MRP-160 Document creation error https://sourceforge.net/tracker/?func=detail&aid=2801625&group_id=176962&atid=934929

---
+++ migration/353a-trunk/oracle/477_BF2801625_MRP160.sql
@@ -0,0 +1,15 @@
+-- 05.06.2009 13:07:43 EEST
+-- MRP-160 Document creation error
+INSERT INTO AD_Message (MsgType,MsgText,CreatedBy,IsActive,Created,Updated,UpdatedBy,MsgTip,AD_Client_ID,AD_Org_ID,EntityType,Value,AD_Message_ID) VALUES ('I','Cannot Create Document',0,'Y',TO_DATE('2009-06-05 13:07:42','YYYY-MM-DD HH24:MI:SS'),TO_DATE('2009-06-05 13:07:42','YYYY-MM-DD HH24:MI:SS'),0,'Indicates that there was an error durring document creation',0,0,'EE01','MRP-160',1500019)
+;
+
+-- 05.06.2009 13:07:43 EEST
+-- MRP-160 Document creation error
+INSERT INTO AD_Message_Trl (AD_Language,AD_Message_ID, MsgText,MsgTip, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Message_ID, t.MsgText,t.MsgTip, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Message t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Message_ID=1500019 AND EXISTS (SELECT * FROM AD_Message_Trl tt WHERE tt.AD_Language!=l.AD_Language OR tt.AD_Message_ID!=t.AD_Message_ID)
+;
+
+-- 05.06.2009 13:08:40 EEST
+-- MRP-160 Document creation error
+UPDATE AD_Message_Trl SET IsTranslated='Y',MsgText='Nu se poate crea document',MsgTip='S-au intampinat erori la crearea documentului de supply',Updated=TO_DATE('2009-06-05 13:08:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Message_ID=1500019 AND AD_Language='ro_RO'
+;
+
