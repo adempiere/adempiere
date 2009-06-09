380d5f08bbaeef898ed0e742f66aa910af78ee48
mjudd <devnull@localhost>
2009-06-09 18:52:00 +0000

    BF [2791095] - EmailUserPW too short for encrypted passwords https://sourceforge.net/tracker/index.php?func=detail&aid=2791095&group_id=176962&atid=879332

--- migration/353a-trunk/oracle/480_BF2791095.sql
+++ migration/353a-trunk/oracle/480_BF2791095.sql
@@ -1,5 +1,4 @@
 --- https://sourceforge.net/tracker/index.php?func=detail&aid=2791095&group_id=176962&atid=879332
 --- increase size of pw field to cater for encrypted passwords
-ALTER TABLE ad_user ALTER COLUMN emailuserpw TYPE varchar(255)
+ALTER TABLE AD_User MODIFY emailuserpw NVARCHAR2(255) 
 ;
-
