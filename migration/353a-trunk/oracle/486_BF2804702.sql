03ab546bc6fb9e6c73aefa3c9ad5bf7a7a708a11
teo_sarca <devnull@localhost>
2009-06-11 09:09:49 +0000

    BF [ 2804702 ] ClassNotFoundException: org.eevolution.process.PayrollProces https://sourceforge.net/tracker/?func=detail&aid=2804702&group_id=176962&atid=934929

---
+++ migration/353a-trunk/oracle/486_BF2804702.sql
@@ -0,0 +1,25 @@
+-- 11.06.2009 12:02:13 EEST
+-- -
+UPDATE AD_Process SET Name='Payroll Processing', Classname='org.eevolution.process.PayrollProcessing', Value='PayrollProcessing',Updated=TO_DATE('2009-06-11 12:02:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Process_ID=53082
+;
+
+-- 11.06.2009 12:02:13 EEST
+-- -
+UPDATE AD_Process_Trl SET IsTranslated='N' WHERE AD_Process_ID=53082
+;
+
+-- 11.06.2009 12:02:15 EEST
+-- -
+UPDATE AD_Menu SET Name='Payroll Processing', IsActive='Y', Description='The Payroll Processing is used to processing a Payroll, you can calculate for a Employee or All Employees',Updated=TO_DATE('2009-06-11 12:02:14','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Menu_ID=53116
+;
+
+-- 11.06.2009 12:02:15 EEST
+-- -
+UPDATE AD_Menu_Trl SET IsTranslated='N' WHERE AD_Menu_ID=53116
+;
+
+-- 11.06.2009 12:02:20 EEST
+-- -
+UPDATE AD_Process SET Value='HR_PayrollProcessing',Updated=TO_DATE('2009-06-11 12:02:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Process_ID=53082
+;
+
