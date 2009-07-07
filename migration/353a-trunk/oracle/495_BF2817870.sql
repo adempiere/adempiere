cfb0fd3d20598efd153c03b3a56f3acdfdb39e96
teo_sarca <devnull@localhost>
2009-07-07 09:02:58 +0000

    [ 2817870 ] MO should use only validated BOMs and WFs https://sourceforge.net/tracker/?func=detail&aid=2817870&group_id=176962&atid=934929
    Product Planning window, Product tab (first) - Verify BOM button should be editable

---
+++ migration/353a-trunk/oracle/495_BF2817870.sql
@@ -0,0 +1,5 @@
+-- 07.07.2009 11:45:51 EEST
+-- 
+UPDATE AD_Field SET DisplayLength=10, IsReadOnly='N',Updated=TO_DATE('2009-07-07 11:45:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=53594
+;
+
