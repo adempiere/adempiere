24a087b5e406b4c953b52c018e5b66b38ccf3e35
trifonnt <devnull@localhost>
2009-07-13 18:09:50 +0000

    BF: [2820886] Wrong Reference type of Manufacturer field https://sourceforge.net/tracker/?func=detail&aid=2820886&group_id=176962&atid=879332

---
+++ migration/353a-trunk/oracle/501_BF2820886.sql
@@ -0,0 +1,5 @@
+-- Jul 13, 2009 6:48:55 PM CEST
+-- [2820886] Wrong Reference type of Manufacturer field
+UPDATE AD_Column SET AD_Reference_ID=10,Updated=TO_DATE('2009-07-13 18:48:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=7965
+;
+
