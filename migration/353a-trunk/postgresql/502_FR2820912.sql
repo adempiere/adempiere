426bf3eccae7f9a7cc6454e2ad5c14ebf06982d5
trifonnt <devnull@localhost>
2009-07-13 19:32:38 +0000

    FR: [2820912] Disable Import Processor https://sourceforge.net/tracker/?func=detail&aid=2820912&group_id=176962&atid=879335

---
+++ migration/353a-trunk/postgresql/502_FR2820912.sql
@@ -0,0 +1,10 @@
+-- Jul 13, 2009 9:11:02 PM CEST
+-- [2820912] Disable Import Processor
+UPDATE IMP_Processor SET IsActive='N',Updated=TO_TIMESTAMP('2009-07-13 21:11:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE IMP_Processor_ID=50000
+;
+
+-- Jul 13, 2009 9:11:05 PM CEST
+-- [2820912] Disable Import Processor
+UPDATE IMP_Processor SET IsActive='N',Updated=TO_TIMESTAMP('2009-07-13 21:11:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE IMP_Processor_ID=50001
+;
+
