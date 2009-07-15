c890acc35c671c9bd7722b721b6aa2c853bc9a69
teo_sarca <devnull@localhost>
2009-07-15 07:10:19 +0000

    BF [ 2821679 ] Display Verified check box https://sourceforge.net/tracker/?func=detail&atid=934929&aid=2821679&group_id=176962

---
+++ migration/353a-trunk/oracle/504_BF2821679.sql
@@ -0,0 +1,10 @@
+-- 15.07.2009 10:04:45 EEST
+-- 
+UPDATE AD_Field SET SeqNo=340,IsDisplayed='Y' WHERE AD_Field_ID=56684
+;
+
+-- 15.07.2009 10:05:13 EEST
+-- 
+UPDATE AD_Field SET IsSameLine='Y', IsReadOnly='Y',Updated=TO_DATE('2009-07-15 10:05:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=56684
+;
+
