00c38c55dfb4b7b51cb7df604fca945c567072a6
globalqss <devnull@localhost>
2009-07-17 19:03:25 +0000

    Fix [2823254] - Minor dictionary problems https://sourceforge.net/tracker/?func=detail&aid=2823254&group_id=176962&atid=934929

---
+++ migration/353a-trunk/oracle/505_BF2823254.sql
@@ -0,0 +1,15 @@
+-- Jul 17, 2009 1:44:55 PM COT
+-- Fix dictionary payroll problems
+UPDATE AD_Field SET SeqNo=70,Updated=TO_DATE('2009-07-17 13:44:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=56307
+;
+
+-- Jul 17, 2009 1:47:09 PM COT
+-- Fix dictionary payroll problems
+UPDATE AD_Tab SET Name='Concept Category',Updated=TO_DATE('2009-07-17 13:47:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53119
+;
+
+-- Jul 17, 2009 1:47:09 PM COT
+-- Fix dictionary payroll problems
+UPDATE AD_Tab_Trl SET IsTranslated='N' WHERE AD_Tab_ID=53119
+;
+
