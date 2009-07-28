4ec6c05be8ec498239fc7b021c77c2feee5dea4c
tspc <devnull@localhost>
2009-07-28 11:31:22 +0000

    BF2828321-Error inserting Forecast Lines
    https://sourceforge.net/tracker/?func=detail&aid=2828321&group_id=176962&atid=934929

---
+++ migration/353a-trunk/oracle/509_BF2828321.sql
@@ -0,0 +1,4 @@
+-- Jul 28, 2009 8:51:07 PM EST
+-- BF2828321 Error inserting Forecast Lines
+UPDATE AD_Column SET DefaultValue='@M_Warehouse_ID@', ReadOnlyLogic=NULL,Updated=TO_DATE('2009-07-28 20:51:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53411
+;
