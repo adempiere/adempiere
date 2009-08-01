f10de4fc4fa221a3e8dcc3d9bf508d318befc179
trifonnt <devnull@localhost>
2009-08-01 12:51:03 +0000

    FR-2830806 [IsAllowLogging human readable] https://sourceforge.net/tracker/?func=detail&aid=2830806&group_id=176962&atid=883808

---
+++ migration/353a-trunk/oracle/514_FR2830806_IsAllowLogging.sql
@@ -0,0 +1,35 @@
+-- Aug 1, 2009 3:01:46 PM EEST
+-- FR2830806 - IsAllowLogging human readable
+UPDATE AD_Element SET Name='Allow Logging', PrintName='Allow Logging',Updated=TO_DATE('2009-08-01 15:01:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=53669
+;
+
+-- Aug 1, 2009 3:01:46 PM EEST
+-- FR2830806 - IsAllowLogging human readable
+UPDATE AD_Element_Trl SET IsTranslated='N' WHERE AD_Element_ID=53669
+;
+
+-- Aug 1, 2009 3:01:46 PM EEST
+-- FR2830806 - IsAllowLogging human readable
+UPDATE AD_Column SET ColumnName='IsAllowLogging', Name='Allow Logging', Description='Determine if a column must be recorded into the change log', Help=NULL WHERE AD_Element_ID=53669
+;
+
+-- Aug 1, 2009 3:01:46 PM EEST
+-- FR2830806 - IsAllowLogging human readable
+UPDATE AD_Process_Para SET ColumnName='IsAllowLogging', Name='Allow Logging', Description='Determine if a column must be recorded into the change log', Help=NULL, AD_Element_ID=53669 WHERE UPPER(ColumnName)='ISALLOWLOGGING' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- Aug 1, 2009 3:01:46 PM EEST
+-- FR2830806 - IsAllowLogging human readable
+UPDATE AD_Process_Para SET ColumnName='IsAllowLogging', Name='Allow Logging', Description='Determine if a column must be recorded into the change log', Help=NULL WHERE AD_Element_ID=53669 AND IsCentrallyMaintained='Y'
+;
+
+-- Aug 1, 2009 3:01:46 PM EEST
+-- FR2830806 - IsAllowLogging human readable
+UPDATE AD_Field SET Name='Allow Logging', Description='Determine if a column must be recorded into the change log', Help=NULL WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=53669) AND IsCentrallyMaintained='Y'
+;
+
+-- Aug 1, 2009 3:01:46 PM EEST
+-- FR2830806 - IsAllowLogging human readable
+UPDATE AD_PrintFormatItem pi SET PrintName='Allow Logging', Name='Allow Logging' WHERE IsCentrallyMaintained='Y' AND EXISTS (SELECT * FROM AD_Column c WHERE c.AD_Column_ID=pi.AD_Column_ID AND c.AD_Element_ID=53669)
+;
+
