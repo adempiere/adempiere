a61062f9fc43cd7bf3af413a16a38f4de71ccef1
trifonnt <devnull@localhost>
2009-08-04 20:49:47 +0000

    Add Two missing JP Prefectures. As per sf.net post: https://sourceforge.net/forum/message.php?msg_id=7544012

---
+++ migration/353a-trunk/postgresql/518_Add_Two_JP_Prefectures.sql
@@ -0,0 +1,15 @@
+-- Aug 4, 2009 11:39:02 PM EEST
+-- Add missing Janap regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,216,TO_TIMESTAMP('2009-08-04 23:39:00','YYYY-MM-DD HH24:MI:SS'),100,50000,'Y','N','栃木県(Tochigi)',TO_TIMESTAMP('2009-08-04 23:39:00','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Aug 4, 2009 11:39:24 PM EEST
+-- Add missing Janap regions
+UPDATE C_Region SET Description='栃木県(Tochigi)', Name='栃木県',Updated=TO_TIMESTAMP('2009-08-04 23:39:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Region_ID=50000
+;
+
+-- Aug 4, 2009 11:40:01 PM EEST
+-- Add missing Janap regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,Description,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,216,TO_TIMESTAMP('2009-08-04 23:40:00','YYYY-MM-DD HH24:MI:SS'),100,50001,'長野県(Nagano)','Y','N','長野県',TO_TIMESTAMP('2009-08-04 23:40:00','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
