fbbd22f3b2f2b9653bcf09298ed296b86154809e
globalqss <devnull@localhost>
2009-08-04 23:46:03 +0000

    Fix [ adempiere-Bugs-2832352 ] InOutCreateConfirm running out of trx https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2832352&group_id=176962

--- base/src/org/compiere/process/InOutCreateConfirm.java
+++ base/src/org/compiere/process/InOutCreateConfirm.java
@@ -62,7 +62,7 @@ public class InOutCreateConfirm extends SvrProcess
 	protected String doIt () throws Exception
 	{
 		log.info("M_InOut_ID=" + p_M_InOut_ID + ", Type=" + p_ConfirmType);
-		MInOut shipment = new MInOut (getCtx(), p_M_InOut_ID, null);
+		MInOut shipment = new MInOut (getCtx(), p_M_InOut_ID, get_TrxName());
 		if (shipment.get_ID() == 0)
 			throw new IllegalArgumentException("Not found M_InOut_ID=" + p_M_InOut_ID);
 		//
