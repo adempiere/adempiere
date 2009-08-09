675e46399185411d51ca941e28eef976656e9b6b
trifonnt <devnull@localhost>
2009-08-09 09:10:01 +0000

    BF [2834402] - Wrong Reference type for Partner Category column https://sourceforge.net/tracker/?func=detail&aid=2834402&group_id=176962&atid=879332

---
+++ migration/353a-trunk/postgresql/520_BF2834402_PartnerCategory.sql
@@ -0,0 +1,5 @@
+-- Aug 9, 2009 11:49:40 AM EEST
+-- BF [2834402] - Wrong Reference type for Partner Category column
+UPDATE AD_Column SET AD_Reference_ID=10,Updated=TO_TIMESTAMP('2009-08-09 11:49:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2710
+;
+
