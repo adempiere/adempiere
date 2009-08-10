2e6e7888718f416a7e6a7c03b8d942bf8a35d06b
globalqss <devnull@localhost>
2009-08-10 21:33:13 +0000

    FR [2835168] - Minor problem found in dictionary https://sourceforge.net/tracker/?func=detail&aid=2835168&group_id=176962&atid=934929

---
+++ migration/353a-trunk/postgresql/523_payroll_dict_minor_fix.sql
@@ -0,0 +1,5 @@
+-- Aug 10, 2009 4:18:22 PM COT
+-- Minor payroll dictionary problem
+UPDATE AD_Field SET IsCentrallyMaintained='Y',Updated=TO_TIMESTAMP('2009-08-10 16:18:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=54838
+;
+
