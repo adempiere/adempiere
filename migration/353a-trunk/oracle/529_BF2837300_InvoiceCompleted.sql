83ec16575b901e7a3a4ade29eefc808233629539
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-08-14 03:27:38 +0000

    http://sourceforge.net/tracker/?func=detail&atid=879332&aid=2837300&group_id=176962  In payment selection you can select draft invoice

---
+++ migration/353a-trunk/oracle/529_BF2837300_InvoiceCompleted.sql
@@ -0,0 +1,5 @@
+-- Aug 13, 2009 10:03:11 PM ECT
+-- In payment selection you can select draft invoice http://sourceforge.net/tracker/?func=detail&atid=879332&aid=2837300&group_id=176962
+UPDATE AD_Column SET AD_Val_Rule_ID=220,Updated=TO_DATE('2009-08-13 22:03:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Column_ID=5639
+;
+
