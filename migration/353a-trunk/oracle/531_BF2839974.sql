01cd4ebf8f4270a97026291869b1d51d735458e8
globalqss <devnull@localhost>
2009-08-19 00:24:27 +0000

    Fix [2839974] - Not zoom for payroll period https://sourceforge.net/tracker/?func=detail&aid=2839974&group_id=176962&atid=934929

---
+++ migration/353a-trunk/oracle/531_BF2839974.sql
@@ -0,0 +1,5 @@
+-- Aug 18, 2009 7:17:15 PM COT
+-- [ adempiere-Libero-2839974 ] Not zoom for payroll period
+UPDATE AD_Table SET AD_Window_ID=53038,Updated=TO_DATE('2009-08-18 19:17:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=53094
+;
+
