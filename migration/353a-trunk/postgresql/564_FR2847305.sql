9e9de75a7a7f4eed6a0415c8e5c0acff099e9de6
teo_sarca <devnull@localhost>
2009-09-06 06:42:13 +0000

    FR [ 2846869 ] Info class - add more helper methods https://sourceforge.net/tracker/?func=detail&atid=879335&aid=2846869&group_id=176962 FR [ 2847305 ] Info class improvements https://sourceforge.net/tracker/?func=detail&aid=2847305&group_id=176962&atid=879335
    added SysConfig parameters

---
+++ migration/353a-trunk/postgresql/564_FR2847305.sql
@@ -0,0 +1,12 @@
+-- Sep 6, 2009 9:20:34 AM EEST
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50032,'C',TO_TIMESTAMP('2009-09-06 09:20:32','YYYY-MM-DD HH24:MI:SS'),0,'Info windows - Specify if the records should be checked(selected) by default (multi selection mode only)','D','Y','INFO_DEFAULTSELECTED',TO_TIMESTAMP('2009-09-06 09:20:32','YYYY-MM-DD HH24:MI:SS'),0,'N')
+;
+
+-- Sep 6, 2009 9:21:13 AM EEST
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50033,'C',TO_TIMESTAMP('2009-09-06 09:21:12','YYYY-MM-DD HH24:MI:SS'),0,'Info Window - True if double click on a row toggles if row is selected (multi selection mode only)','D','Y','INFO_DOUBLECLICKTOGGLESSELECTION',TO_TIMESTAMP('2009-09-06 09:21:12','YYYY-MM-DD HH24:MI:SS'),0,'50000')
+;
+
+-- Sep 6, 2009 9:21:22 AM EEST
+UPDATE AD_SysConfig SET Value='N',Updated=TO_TIMESTAMP('2009-09-06 09:21:22','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_SysConfig_ID=50033
+;
+
