c18a34a2b692e92f03440038af4f2f7a54e9e218
globalqss <devnull@localhost>
2009-09-11 22:25:28 +0000

    Fix [2852100] - PP_Order_Head_v has columns wrongly defined https://sourceforge.net/tracker/index.php?func=detail&aid=2852100&group_id=176962&atid=934929 Deleted definition of view from dictionary

---
+++ migration/353a-trunk/postgresql/568_BF2852100DeletePP_Order_Head_v.sql
@@ -0,0 +1,8 @@
+-- Sep 11, 2009 5:18:43 PM COT
+-- BF2852100-PP_Order_Head_v has columns wrongly defined
+DELETE FROM AD_Table_Trl WHERE AD_Table_ID=53196
+;
+
+-- Sep 11, 2009 5:18:44 PM COT
+DELETE FROM AD_Table WHERE AD_Table_ID=53196
+;
