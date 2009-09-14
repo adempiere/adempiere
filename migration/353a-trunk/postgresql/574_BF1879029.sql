57618ae016d59be73c51207d10d24c06f166a87f
globalqss <devnull@localhost>
2009-09-14 01:12:17 +0000

    FR [1879029] - Added IsInternalUseInventory flag to M_Inventory table https://sourceforge.net/tracker/index.php?func=detail&aid=1879029&group_id=176962&atid=879332 Temporary measure - where clause in tab

---
+++ migration/353a-trunk/postgresql/574_BF1879029.sql
@@ -0,0 +1,8 @@
+-- Oct 8, 2008 6:48:17 PM COT
+-- [ 1879029 ] Added IsInternalUseInventory flag to M_Inventory table
+UPDATE AD_Tab SET WhereClause='(SELECT COUNT(*) FROM M_InventoryLine WHERE M_InventoryLine.M_Inventory_ID = M_Inventory.M_Inventory_ID) = 0 OR (SELECT COUNT(*) FROM M_InventoryLine WHERE M_InventoryLine.M_Inventory_ID = M_Inventory.M_Inventory_ID AND M_InventoryLine.QtyInternalUse <> 0) > 0',Updated=TO_TIMESTAMP('2008-10-08 18:48:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=682
+;
+
+UPDATE AD_Tab SET WhereClause='(SELECT COUNT(*) FROM M_InventoryLine WHERE M_InventoryLine.M_Inventory_ID = M_Inventory.M_Inventory_ID) = 0 OR (SELECT COUNT(*) FROM M_InventoryLine WHERE M_InventoryLine.M_Inventory_ID = M_Inventory.M_Inventory_ID AND M_InventoryLine.QtyInternalUse <> 0) <= 0',Updated=TO_TIMESTAMP('2008-10-08 18:50:44','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=255
+;
+
