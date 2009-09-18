51377dda69b2fc57542fa3896a13f3b658abd0de
tspc <devnull@localhost>
2009-09-18 11:08:21 +0000

    BF2861302 fix Order By clause http://sourceforge.net/tracker/?func=detail&aid=2861302&group_id=176962&atid=879332

---
+++ migration/354a-trunk/postgresql/580_BF2861302.sql
@@ -0,0 +1,5 @@
+-- Sep 18, 2009 8:46:56 PM EST
+-- BF2861302 Order By Clause of ad_reference_id 240
+UPDATE AD_Ref_Table SET OrderByClause='PA_ReportLine.SeqNo',Updated=TO_TIMESTAMP('2009-09-18 20:46:56','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=240
+;
+
