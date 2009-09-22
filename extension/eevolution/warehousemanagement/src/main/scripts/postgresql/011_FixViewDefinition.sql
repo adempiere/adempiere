90070a5997a90c3696263077fecf1521299aa4cf
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-09-22 04:03:42 +0000

    fix view definition
    --HG--
    branch : WarehouseManagement
    extra : convert_revision : svn%3Af8025ce2-341d-0410-ac50-9b82273f5115/contributions/e-Evolution/WarehouseManagement%4010309

---
+++ extension/eevolution/warehousemanagement/src/main/scripts/postgresql/011_FixViewDefinition.sql
@@ -0,0 +1,45 @@
+-- Sep 14, 2009 10:27:34 AM ECT
+-- Warehouse Management
+UPDATE AD_View_Definition SET JoinClause='INNER JOIN C_BPartner bp ON (bp.C_BPartner_ID=ol.C_BPartner_ID)',Updated=TO_TIMESTAMP('2009-09-14 10:27:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_View_Definition_ID=50009
+;
+
+-- Sep 14, 2009 10:28:33 AM ECT
+-- Warehouse Management
+UPDATE AD_Browse SET WhereClause='iob.IsSOTrx=''Y'' AND iob.DocStatus=''DR''',Updated=TO_TIMESTAMP('2009-09-14 10:28:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Browse_ID=50001
+;
+
+-- Sep 15, 2009 7:52:34 AM ECT
+-- Warehouse Management
+DELETE FROM AD_Table_Trl WHERE AD_Table_ID=53042
+;
+
+-- Sep 15, 2009 7:52:34 AM ECT
+-- Warehouse Management
+DELETE FROM AD_Table WHERE AD_Table_ID=53042
+;
+
+-- Sep 15, 2009 7:54:42 AM ECT
+-- Warehouse Management
+DELETE FROM AD_Table_Trl WHERE AD_Table_ID=53033
+;
+
+-- Sep 15, 2009 7:54:42 AM ECT
+-- Warehouse Management
+DELETE FROM AD_Table WHERE AD_Table_ID=53033
+;
+
+-- Sep 15, 2009 7:55:56 AM ECT
+-- Warehouse Management
+DELETE FROM AD_Table_Trl WHERE AD_Table_ID=53033
+;
+
+-- Sep 15, 2009 7:55:56 AM ECT
+-- Warehouse Management
+DELETE FROM AD_Table WHERE AD_Table_ID=53033
+;
+
+-- Sep 15, 2009 8:00:29 AM ECT
+-- Warehouse Management
+UPDATE AD_Client SET IsUseBetaFunctions='Y',Updated=TO_TIMESTAMP('2009-09-15 08:00:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Client_ID=0
+;
+
