6f9634a23ceeb6dcd2a28f6491e46efe1ac48b9a
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-09-22 04:09:33 +0000

    fix migration script delete in necessary statement
    --HG--
    branch : WarehouseManagement
    extra : convert_revision : svn%3Af8025ce2-341d-0410-ac50-9b82273f5115/contributions/e-Evolution/WarehouseManagement%4010310

--- extension/eevolution/warehousemanagement/src/main/scripts/oracle/011_FixViewDefinition.sql
+++ extension/eevolution/warehousemanagement/src/main/scripts/oracle/011_FixViewDefinition.sql
@@ -11,36 +11,6 @@ UPDATE AD_View_Definition SET JoinClause='INNER JOIN C_BPartner bp ON (bp.C_BPar
 UPDATE AD_Browse SET WhereClause='iob.IsSOTrx=''Y'' AND iob.DocStatus=''DR''',Updated=TO_DATE('2009-09-14 10:28:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Browse_ID=50001
 ;
 
--- Sep 15, 2009 7:52:34 AM ECT
--- Warehouse Management
-DELETE  FROM  AD_Table_Trl WHERE AD_Table_ID=53042
-;
-
--- Sep 15, 2009 7:52:34 AM ECT
--- Warehouse Management
-DELETE FROM AD_Table WHERE AD_Table_ID=53042
-;
-
--- Sep 15, 2009 7:54:42 AM ECT
--- Warehouse Management
-DELETE  FROM  AD_Table_Trl WHERE AD_Table_ID=53033
-;
-
--- Sep 15, 2009 7:54:42 AM ECT
--- Warehouse Management
-DELETE FROM AD_Table WHERE AD_Table_ID=53033
-;
-
--- Sep 15, 2009 7:55:56 AM ECT
--- Warehouse Management
-DELETE  FROM  AD_Table_Trl WHERE AD_Table_ID=53033
-;
-
--- Sep 15, 2009 7:55:56 AM ECT
--- Warehouse Management
-DELETE FROM AD_Table WHERE AD_Table_ID=53033
-;
-
 -- Sep 15, 2009 8:00:29 AM ECT
 -- Warehouse Management
 UPDATE AD_Client SET IsUseBetaFunctions='Y',Updated=TO_DATE('2009-09-15 08:00:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Client_ID=0
