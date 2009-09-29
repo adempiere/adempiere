f4aa9c65b36d62b4d5fd00a94f41a00213e20c55
trifonnt <devnull@localhost>
2009-09-29 13:53:23 +0000

    BF[2869715] dba_free_space does not exists on postgres https://sourceforge.net/tracker/?func=detail&aid=2869715&group_id=176962&atid=879332

---
+++ migration/354a-trunk/oracle/587_BF2869715_dba_free_space.sql
@@ -0,0 +1,6 @@
+-- Sep 29, 2009 3:42:58 PM CEST
+-- For Oracle this BUG do not applay as this view exists on Oracle Server!
+-- BF[2869715] - dba_free_space does not exists on postgres
+-- https://sourceforge.net/tracker/?func=detail&aid=2869715&group_id=176962&atid=879332
+--UPDATE AD_AlertRule SET IsActive='N',Updated=TO_TIMESTAMP('2009-09-29 15:42:58','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_AlertRule_ID=100
+--;
