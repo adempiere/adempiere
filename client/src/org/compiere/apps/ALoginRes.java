f2afcbec65ebfc40bfe29e655bcdcaf93abbad43
felixmoeller <devnull@localhost>
2009-09-29 15:03:43 +0000

    the D is capitalized. I forgot the major window: https://sourceforge.net/projects/adempiere/forums/forum/610546/topic/3399028

--- client/src/org/compiere/apps/ALoginRes.java
+++ client/src/org/compiere/apps/ALoginRes.java
@@ -36,7 +36,7 @@ public final class ALoginRes extends ListResourceBundle
 	{
 	{ "Connection",         "Connection" },
 	{ "Defaults",           "Defaults" },
-	{ "Login",              "Adempiere Login" },
+	{ "Login",              "ADempiere Login" },
 	{ "File",               "&File" },
 	{ "Exit",               "Exit" },
 	{ "Help",               "&Help" },
