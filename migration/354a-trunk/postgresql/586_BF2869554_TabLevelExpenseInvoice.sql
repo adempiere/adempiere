06813883b687df4633f9aceb5b99ed214ceeccd7
trifonnt <devnull@localhost>
2009-09-29 08:23:43 +0000

    BF[2869554] - Wrong tab level in "Expense Invoice" window https://sourceforge.net/tracker/?func=detail&aid=2869554&group_id=176962&atid=879332

---
+++ migration/354a-trunk/postgresql/586_BF2869554_TabLevelExpenseInvoice.sql
@@ -0,0 +1,6 @@
+-- Sep 29, 2009 9:53:34 AM CEST
+-- BF[2869554]-Wrong tab level in "Expense Invoice" window
+-- https://sourceforge.net/tracker/?func=detail&aid=2869554&group_id=176962&atid=879332
+UPDATE AD_Tab SET TabLevel=1,Updated=TO_TIMESTAMP('2009-09-29 09:53:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=471
+;
+
