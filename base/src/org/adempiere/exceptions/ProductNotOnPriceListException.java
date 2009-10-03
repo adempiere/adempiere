d898d2ddf191d0c140c0d75c912a412785e42403
teo_sarca <devnull@localhost>
2009-10-03 15:51:20 +0000

    FR [ 2872255 ] Introduce ProductNotOnPriceListException https://sourceforge.net/tracker/?func=detail&aid=2872255&group_id=176962&atid=879335
    Added GPL Header. Thanks Carlos for noticing this.

--- base/src/org/adempiere/exceptions/ProductNotOnPriceListException.java
+++ base/src/org/adempiere/exceptions/ProductNotOnPriceListException.java
@@ -1,6 +1,16 @@
-/**
- * 
- */
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 2009 Teo Sarca. All Rights Reserved.                         *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
 package org.adempiere.exceptions;
 
 import java.text.DateFormat;
