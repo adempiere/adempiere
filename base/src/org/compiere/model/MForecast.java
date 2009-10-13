bc92f49531bf60ee64b8c1ce0e90b29fa8db09f1
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-10-13 02:42:23 +0000

    Forecast Line not validated the warehouse with Organization https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2877525&group_id=176962
    kind regards
    Victor Perez
    www.e-evolution.com

--- base/src/org/compiere/model/MForecast.java
+++ base/src/org/compiere/model/MForecast.java
@@ -17,34 +17,30 @@ package org.compiere.model;
 
 import java.sql.ResultSet;
 import java.util.Properties;
-import org.adempiere.exceptions.WarehouseInvalidForOrgException;
 
 
 /**
- * Forecast Line Model
+ * Forecast Model
  * 
- * @author Victor Perez www.e-evolution.com     
- * @version $Id: MForecastLine.java,v 1.11 2005/05/17 05:29:52 vpj-cd Exp $
+ * @author victor.perez@e-evolution.com, www.e-evolution.com     
  */
-public class MForecastLine extends  X_M_ForecastLine
+public class MForecast extends  X_M_Forecast
 {
 
-	
+
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -3420900505079279058L;
-	/** Parent					*/
-	private MForecast			m_parent = null;
+	private static final long serialVersionUID = 66771328316032322L;
 
 	/**
 	 * Standard Constructor
 	 * @param ctx context
 	 * @param M_ForecastLine_ID id
 	 */
-	public MForecastLine (Properties ctx, int M_ForecastLine_ID, String trxName)
+	public MForecast (Properties ctx, int M_Forecast_ID, String trxName)
 	{
-		super (ctx, M_ForecastLine_ID, trxName);
+		super (ctx, M_Forecast_ID, trxName);
 	}	//	MForecastLine
 
 	/**
@@ -52,39 +48,8 @@ public class MForecastLine extends  X_M_ForecastLine
 	 * @param ctx context
 	 * @param rs result set
 	 */
-	public MForecastLine (Properties ctx, ResultSet rs, String trxName)
+	public MForecast (Properties ctx, ResultSet rs, String trxName)
 	{
 		super(ctx, rs, trxName);
-	}	//	MForecastLine
-	
-	/**************************************************************************
-	 * 	Before Save
-	 *	@param newRecord
-	 *	@return true if it can be saved
-	 */
-	protected boolean beforeSave (boolean newRecord)
-	{
-		if (newRecord 
-			|| is_ValueChanged("AD_Org_ID") || is_ValueChanged("M_Warehouse_ID"))
-		{	
-			MWarehouse wh = MWarehouse.get(getCtx(), getM_Warehouse_ID());
-			if (wh.getAD_Org_ID() != getAD_Org_ID())
-			{
-				throw new WarehouseInvalidForOrgException(wh.getName(), MOrg.get(getCtx(), getAD_Org_ID()).getName());
-			}
-		}
-		return true;
-	}
-	
-	/**
-	 * 	Get Parent
-	 *	@return parent
-	 */
-	public MForecast getParent()
-	{
-		if (m_parent == null)
-			m_parent = new MForecast(getCtx(), getM_Forecast_ID(), get_TrxName());
-		return m_parent;
-	}	//	getParent
-	
-}	//	MForecastLine
+	}	//	MForecast
+}	//	MForecast
---
+++ migration/354a-trunk/postgresql/596_BF2877525_FixForecast.sql
@@ -0,0 +1,5 @@
+-- Oct 12, 2009 9:06:07 PM CDT
+-- Forecast
+UPDATE AD_Column SET AD_Val_Rule_ID=189,Updated=TO_TIMESTAMP('2009-10-12 21:06:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53411
+;
+
