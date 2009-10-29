6b4853df27575874be85665d4c911aace0a7adc9
Kthiemann <devnull@localhost>
2009-10-29 11:05:34 +0000

    BF (1901326) - New euro member countries http://sourceforge.net/tracker/?func=detail&aid=1901326&group_id=176962&atid=879332
    Submitted by Matjaž Godec

---
+++ migration/354a-trunk/oracle/601_BF1901326_NewEuroMemberCountries.sql
@@ -0,0 +1,15 @@
+-- Feb 25, 2008 1:28:06 PM CET
+-- Default comment for updating dictionary
+UPDATE C_Country SET C_Currency_ID=102,Updated=TO_DATE('2008-02-25 13:28:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=302
+;
+
+-- Feb 25, 2008 1:28:26 PM CET
+-- Default comment for updating dictionary
+UPDATE C_Country SET C_Currency_ID=102,Updated=TO_DATE('2008-02-25 13:28:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=165
+;
+
+-- Feb 25, 2008 1:28:46 PM CET
+-- Default comment for updating dictionary
+UPDATE C_Country SET C_Currency_ID=102,Updated=TO_DATE('2008-02-25 13:28:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=241
+;
+
