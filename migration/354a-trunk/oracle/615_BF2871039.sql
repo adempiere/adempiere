3d8927e8847887d7f038e595a6a428362d923403
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-11-30 17:02:40 +0000

    Tax Rate Parent window not working https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2871039&group_id=176962

---
+++ migration/354a-trunk/oracle/615_BF2871039.sql
@@ -0,0 +1,10 @@
+-- Nov 30, 2009 10:54:15 AM CST
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET WhereClause='M_Product_ID=@0|M_Product_ID@',Updated=TO_DATE('2009-11-30 10:54:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53286
+;
+
+-- Nov 30, 2009 10:54:36 AM CST
+-- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
+UPDATE AD_Tab SET WhereClause='IsSummary=''Y'' AND C_Tax.Parent_Tax_ID=@0|C_Tax_ID@',Updated=TO_DATE('2009-11-30 10:54:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53079
+;
+
