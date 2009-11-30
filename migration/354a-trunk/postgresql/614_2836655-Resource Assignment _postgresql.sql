28bbb3549a6647634bed0d47b5c964048bbd5bfb
red1 <devnull@localhost>
2009-11-30 15:27:51 +0000

    2836655-Resource Assignment always return Qty 1 Fixed SQL script to add in Callouts to OrderLine Resource Assignment - thanks to iCyLand, Bangkok's help.

---
+++ migration/354a-trunk/postgresql/614_2836655-Resource Assignment _postgresql.sql
@@ -0,0 +1,5 @@
+-- Nov 29, 2009 4:56:03 PM MYT
+-- 2836655-Resource Assignment always return Qty 1
+UPDATE AD_Column SET Callout='org.compiere.model.CalloutAssignment.product; org.compiere.model.CalloutOrder.amt; org.compiere.model.CalloutOrder.qty',Updated=TO_TIMESTAMP('2009-11-29 16:56:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=6775
+;
+
