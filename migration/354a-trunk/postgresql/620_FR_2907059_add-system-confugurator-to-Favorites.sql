d9282629f3381355c9aadf3dd5f254a708836c29
trifonnt <devnull@localhost>
2009-12-01 19:09:44 +0000

    FR[2907059] - Add System Configurator to Favourites https://sourceforge.net/tracker/?func=detail&aid=2907059&group_id=176962&atid=883808

---
+++ migration/354a-trunk/postgresql/620_FR_2907059_add-system-confugurator-to-Favorites.sql
@@ -0,0 +1,6 @@
+-- Dec 1, 2009 7:55:39 PM CET
+-- FR[2907059] - Add System Configurator to Favourites
+-- https://sourceforge.net/tracker/?func=detail&aid=2907059&group_id=176962&atid=883808
+INSERT INTO AD_TreeBar (AD_Tree_ID,AD_User_ID,Node_ID, AD_Client_ID,AD_Org_ID, IsActive,Created,CreatedBy,Updated,UpdatedBy)VALUES (10,100,50008,0,0,'Y',CURRENT_TIMESTAMP,100,CURRENT_TIMESTAMP,100)
+;
+
