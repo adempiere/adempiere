1e33d16e70d4462c726ebc7c02ff63a6d0277e05
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-12-07 23:19:10 +0000

    fix BOM back compatibility https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2904737&group_id=176962 Tax Rate Parent window not working https://sourceforge.net/tracker/index.php?func=detail&aid=2871039&group_id=176962&atid=879332

--- migration/354a-trunk/oracle/615_BF2871039.sql
+++ migration/354a-trunk/oracle/615_BF2871039.sql
@@ -1,10 +1,24 @@
--- Nov 30, 2009 10:54:15 AM CST
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Tab SET WhereClause='M_Product_ID=@0|M_Product_ID@',Updated=TO_DATE('2009-11-30 10:54:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53286
+-- Dec 3, 2009 12:09:29 AM CST
+-- Fix BOM and Parent Tax Rate
+UPDATE AD_Tab SET TabLevel=1,Updated=TO_DATE('2009-12-03 00:09:29','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53078
 ;
 
--- Nov 30, 2009 10:54:36 AM CST
--- I forgot to set the DICTIONARY_ID_COMMENTS System Configurator
-UPDATE AD_Tab SET WhereClause='IsSummary=''Y'' AND C_Tax.Parent_Tax_ID=@0|C_Tax_ID@',Updated=TO_DATE('2009-11-30 10:54:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53079
+-- Dec 3, 2009 12:10:10 AM CST
+-- Fix BOM and Parent Tax Rate
+UPDATE AD_Field SET DefaultValue='@C_Tax_ID@',Updated=TO_DATE('2009-12-03 00:10:10','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=54489
 ;
 
+-- Dec 3, 2009 12:12:51 AM CST
+-- Fix BOM and Parent Tax Rate
+UPDATE AD_Tab SET Parent_Column_ID=2240,Updated=TO_DATE('2009-12-03 00:12:51','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53078
+;
+
+-- Dec 3, 2009 12:14:48 AM CST
+-- Fix BOM and Parent Tax Rate
+UPDATE AD_Field SET DefaultValue='@Parent_Tax_ID@',Updated=TO_DATE('2009-12-03 00:14:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=54489
+;
+
+-- Dec 3, 2009 12:19:07 AM CST
+-- Fix BOM and Parent Tax Rate
+UPDATE AD_Field SET DefaultValue='@0|C_Tax_ID@',Updated=TO_DATE('2009-12-03 00:19:07','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=54489
+;
