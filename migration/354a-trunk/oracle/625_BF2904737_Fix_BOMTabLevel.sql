da7c760ef90c289bd7aa09122bc32faf7ffcf5b8
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2009-12-08 01:18:07 +0000

    fix BOM back compatibility https://sourceforge.net/tracker/index.php?func=detail&aid=2904737&group_id=176962&atid=879332
    Fixed the tab level wrong

---
+++ migration/354a-trunk/oracle/625_BF2904737_Fix_BOMTabLevel.sql
@@ -0,0 +1,14 @@
+-- Dec 7, 2009 6:53:06 PM CST
+-- Fix Tab Level in BOM Tab
+UPDATE AD_Tab SET TabLevel=1,Updated=TO_TIMESTAMP('2009-12-07 18:53:06','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53286
+;
+
+-- Dec 7, 2009 6:53:13 PM CST
+-- Fix Tab Level in BOM Tab
+UPDATE AD_Tab SET TabLevel=2,Updated=TO_TIMESTAMP('2009-12-07 18:53:13','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Tab_ID=53287
+;
+
+-- Dec 7, 2009 6:37:03 PM CST
+-- Fix Tab Level in BOM Tab
+UPDATE AD_Tab SET Parent_Column_ID=1402,Updated=TO_DATE('2009-12-07 18:37:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53286
+;
