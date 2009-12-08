dc5b9a605feeb0bcd76ab64e9311dbc451461358
globalqss <devnull@localhost>
2009-12-08 00:35:48 +0000

    Temporal solution for [2904737] - fix BOM back compatibility https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2904737&group_id=176962 Intended to keep product window clean and working

---
+++ migration/354a-trunk/oracle/624_BF2904737.sql
@@ -0,0 +1,115 @@
+-- Dec 7, 2009 7:30:30 PM COT
+-- fix BOM back compatibility
+UPDATE AD_Tab SET IsActive='N',Updated=TO_DATE('2009-12-07 19:30:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53286
+;
+
+-- Dec 7, 2009 7:30:32 PM COT
+-- fix BOM back compatibility
+UPDATE AD_Tab SET IsActive='N',Updated=TO_DATE('2009-12-07 19:30:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53287
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=0, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=268
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=1, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=125
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=2, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=422
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=3, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=107
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=4, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=130
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=5, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=188
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=6, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=227
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=7, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=381
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=8, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=126
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=9, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53024
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=10, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=421
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=11, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=534
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=12, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=267
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=13, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=490
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=14, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=132
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=15, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=310
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=16, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=128
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=17, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=585
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=18, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=187
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=19, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53210
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=20, Updated=SysDate WHERE AD_Tree_ID=10 AND Node_ID=53211
+;
+
---
+++ migration/354a-trunk/postgresql/624_BF2904737.sql
@@ -0,0 +1,115 @@
+-- Dec 7, 2009 7:30:30 PM COT
+-- fix BOM back compatibility
+UPDATE AD_Tab SET IsActive='N',Updated=TO_TIMESTAMP('2009-12-07 19:30:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53286
+;
+
+-- Dec 7, 2009 7:30:32 PM COT
+-- fix BOM back compatibility
+UPDATE AD_Tab SET IsActive='N',Updated=TO_TIMESTAMP('2009-12-07 19:30:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53287
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=0, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=268
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=1, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=125
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=2, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=422
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=3, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=107
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=4, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=130
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=5, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=188
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=6, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=227
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=7, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=381
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=8, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=126
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=9, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=53024
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=10, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=421
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=11, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=534
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=12, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=267
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=13, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=490
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=14, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=132
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=15, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=310
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=16, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=128
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=17, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=585
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=18, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=187
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=19, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=53210
+;
+
+-- Dec 7, 2009 7:31:12 PM COT
+-- fix BOM back compatibility
+UPDATE AD_TreeNodeMM SET Parent_ID=167, SeqNo=20, Updated=CURRENT_TIMESTAMP WHERE AD_Tree_ID=10 AND Node_ID=53211
+;
+
