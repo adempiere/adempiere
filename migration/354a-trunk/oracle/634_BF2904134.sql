0d270dacbba5743ae2377063c478b1a24048be8d
globalqss <devnull@localhost>
2009-12-11 02:10:08 +0000

    BF [2904134] - Cannot define the print format for a check https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2904134&group_id=176962

---
+++ migration/354a-trunk/oracle/634_BF2904134.sql
@@ -0,0 +1,21 @@
+-- Dec 10, 2009 9:05:19 PM COT
+-- 2904134_Cannot define the print format for a check
+UPDATE AD_Ref_List SET IsActive='Y',Updated=TO_DATE('2009-12-10 21:05:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=332
+;
+
+-- Dec 10, 2009 9:05:37 PM COT
+UPDATE AD_Ref_List SET IsActive='Y',Updated=TO_DATE('2009-12-10 21:05:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=652
+;
+
+-- Dec 10, 2009 9:05:48 PM COT
+UPDATE AD_Ref_List SET IsActive='Y',Updated=TO_DATE('2009-12-10 21:05:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=333
+;
+
+-- Dec 10, 2009 9:06:15 PM COT
+UPDATE AD_Ref_List SET IsActive='Y',Updated=TO_DATE('2009-12-10 21:06:15','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=337
+;
+
+-- Dec 10, 2009 9:06:27 PM COT
+UPDATE AD_Ref_List SET IsActive='Y',Updated=TO_DATE('2009-12-10 21:06:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Ref_List_ID=335
+;
+
