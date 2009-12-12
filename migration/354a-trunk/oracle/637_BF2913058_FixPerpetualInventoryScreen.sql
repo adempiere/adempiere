722887efb68d0a99a108098c6b321c13d0961216
trifonnt <devnull@localhost>
2009-12-12 01:07:23 +0000

    BF [2913058] - Fix Perpetual Inventory window https://sourceforge.net/tracker/?func=detail&aid=2913058&group_id=176962&atid=879332

---
+++ migration/354a-trunk/oracle/637_BF2913058_FixPerpetualInventoryScreen.sql
@@ -0,0 +1,6 @@
+-- Dec 11, 2009 10:03:25 PM CET
+-- BF [2913058] - FixPerpetualInventoryScreen
+-- https://sourceforge.net/tracker/?func=detail&aid=2913058&group_id=176962&atid=879332
+UPDATE AD_Field SET IsReadOnly='N',Updated=TO_DATE('2009-12-11 22:03:25','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=3036
+;
+
