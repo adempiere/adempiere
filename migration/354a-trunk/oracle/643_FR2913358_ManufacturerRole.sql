5ba2ff6b80efd8bd98660283f51023419abf86ae
trifonnt <devnull@localhost>
2009-12-12 22:22:45 +0000

    -- FR [2913358] BPartner role - Manufacturer -- https://sourceforge.net/tracker/?func=detail&aid=2913358&group_id=176962&atid=883808 Fix position of tab. Thank's to Carlos for the report!

---
+++ migration/354a-trunk/oracle/643_FR2913358_ManufacturerRole.sql
@@ -0,0 +1,6 @@
+-- Dec 12, 2009 11:17:21 PM CET
+-- FR [2913358] BPartner role - Manufacturer
+-- https://sourceforge.net/tracker/?func=detail&aid=2913358&group_id=176962&atid=883808
+UPDATE AD_Tab SET SeqNo=75,Updated=TO_DATE('2009-12-12 23:17:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53288
+;
+
