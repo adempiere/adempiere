dda92868821b5c0350d16a7eeb94645968522fd9
globalqss <devnull@localhost>
2009-12-14 22:32:04 +0000

    Fix [2897751] - PackIn -> folder target must be mandatory https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2897751&group_id=176962

---
+++ migration/354a-trunk/oracle/644_BF2897751.sql
@@ -0,0 +1,5 @@
+-- Dec 14, 2009 5:29:08 PM COT
+-- 2897751_PackIn -> folder target must be mandatory
+UPDATE AD_Column SET IsMandatory='Y',Updated=TO_DATE('2009-12-14 17:29:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=50170
+;
+
