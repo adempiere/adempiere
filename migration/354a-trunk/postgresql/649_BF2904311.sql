751a05e50bd91311ee7af1ec2349bdbd212a0de4
globalqss <devnull@localhost>
2009-12-15 23:34:43 +0000

    Fix [2904311] - Wrong default for parameter Org in Statement of Accounts https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2904311&group_id=176962

---
+++ migration/354a-trunk/postgresql/649_BF2904311.sql
@@ -0,0 +1,5 @@
+-- Dec 15, 2009 6:32:41 PM COT
+-- 2904311_Wrong default for parameter Org in Statement of Accounts
+UPDATE AD_Process_Para SET DefaultValue='-1',Updated=TO_TIMESTAMP('2009-12-15 18:32:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Process_Para_ID=459
+;
+
