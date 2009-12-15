132ff5066392bfeba4de4ed8fe15586922041086
globalqss <devnull@localhost>
2009-12-15 22:49:06 +0000

    Fix [2914836] - Correct Reference for AmountTendered & AmountRefunded https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2914836&group_id=176962

---
+++ migration/354a-trunk/oracle/647_BF2914836.sql
@@ -0,0 +1,9 @@
+-- Dec 15, 2009 5:46:57 PM COT
+-- 2914836_Correct Reference for AmountTendered AmountRefunded
+UPDATE AD_Column SET AD_Reference_ID=12,Updated=TO_DATE('2009-12-15 17:46:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=52065
+;
+
+-- Dec 15, 2009 5:47:03 PM COT
+UPDATE AD_Column SET AD_Reference_ID=12,Updated=TO_DATE('2009-12-15 17:47:03','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=52064
+;
+
