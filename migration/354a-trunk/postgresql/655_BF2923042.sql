e753b858ce8ab4fa6d61134ec81c1969f51ec0b0
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-01-05 07:33:25 +0000

    Integrate revision 11222 Problems in Requisition window - fix dictionary to avoid making the second tab read-write after navigating to third tab
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2923042

---
+++ migration/354a-trunk/postgresql/655_BF2923042.sql
@@ -0,0 +1,5 @@
+-- Jan 4, 2010 5:36:54 PM COT
+-- 2923042_Problems in Requisition window
+UPDATE AD_Field SET IsActive='N', IsDisplayed='N', SeqNo=0,Updated=TO_TIMESTAMP('2010-01-04 17:36:54','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Field_ID=12463
+;
+
