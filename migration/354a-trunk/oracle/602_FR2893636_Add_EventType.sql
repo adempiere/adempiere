7d6f3b78b82a80600e2b00f075ae634bcc7e4c1a
trifonnt <devnull@localhost>
2010-01-16 07:55:22 +0000

    BF [2933248] - Oracle script 602_FR2893636_Add_EventType.sql https://sourceforge.net/tracker/?func=detail&atid=879332&aid=2933248&group_id=176962

--- migration/354a-trunk/oracle/602_FR2893636_Add_EventType.sql
+++ migration/354a-trunk/oracle/602_FR2893636_Add_EventType.sql
@@ -1,3 +1,5 @@
+SET DEFINE OFF
+
 -- Nov 6, 2009 5:47:56 PM CST
 -- Human Resource & Payroll EventType
 INSERT INTO AD_Ref_List (AD_Client_ID,AD_Org_ID,AD_Ref_List_ID,AD_Reference_ID,Created,CreatedBy,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,53551,53236,TO_DATE('2009-11-06 17:47:56','YYYY-MM-DD HH24:MI:SS'),0,'EE02','Y','Human Resource & Payroll',TO_DATE('2009-11-06 17:47:56','YYYY-MM-DD HH24:MI:SS'),0,'H')
