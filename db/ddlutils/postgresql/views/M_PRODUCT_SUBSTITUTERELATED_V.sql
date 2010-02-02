6e3b9777c9408faa1d7d3c60dadc313cec57f879
mjudd <devnull@localhost>
2010-02-02 21:28:17 +0000

    [ BF 2944056 ] - Fix quantities for Substitute & Related products in InfoProduct https://sourceforge.net/tracker/index.php?func=detail&aid=2944056&group_id=176962&atid=879332 Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2944056

--- db/ddlutils/postgresql/views/M_PRODUCT_SUBSTITUTERELATED_V.sql
+++ db/ddlutils/postgresql/views/M_PRODUCT_SUBSTITUTERELATED_V.sql
@@ -1,55 +1,78 @@
-CREATE OR 
-REPLACE VIEW m_product_substituterelated_v AS 
-SELECT s.ad_client_id,
-    s.ad_org_id,
-    s.isactive,
-    s.created,
-    s.createdby,
-    s.updated,
-    s.updatedby,
-    s.m_product_id,
-    s.substitute_id,
-    s.description,
-    'S'                             AS ROWTYPE,
-    (ms.qtyonhand - ms.qtyreserved) AS qtyavailable,
-    mp.NAME,
-    ms.qtyonhand,
-    ms.qtyreserved,
-    mpr.pricestd,
-    mpr.m_pricelist_version_id,
-    mw.m_warehouse_id,
-    org.NAME AS orgname
-FROM M_SUBSTITUTE s 
-    JOIN M_STORAGE ms ON ms.m_product_id = s.substitute_id
-    JOIN M_PRODUCT mp ON ms.m_product_id = mp.m_product_id
-    JOIN M_LOCATOR ml ON ms.m_locator_id = ml.m_locator_id
-    JOIN M_WAREHOUSE mw ON ml.m_warehouse_id = mw.m_warehouse_id
-    JOIN M_PRODUCTPRICE mpr ON ms.m_product_id = mpr.m_product_id
-    JOIN AD_ORG org ON org.ad_org_id = mw.ad_org_id
-UNION 
-SELECT r.ad_client_id,
-    r.ad_org_id,
-    r.isactive,
-    r.created,
-    r.createdby,
-    r.updated,
-    r.updatedby,
-    r.m_product_id,
-    r.relatedproduct_id             AS substitute_id,
-    r.description,
-    'R'                             AS ROWTYPE,
-    (ms.qtyonhand - ms.qtyreserved) AS qtyavailable,
-    mp.NAME,
-    ms.qtyonhand,
-    ms.qtyreserved,
-    mpr.pricestd,
-    mpr.m_pricelist_version_id,
-    mw.m_warehouse_id,
-    org.NAME AS orgname
-FROM M_RELATEDPRODUCT r 
-    JOIN M_STORAGE ms ON ms.m_product_id = r.relatedproduct_id
-    JOIN M_PRODUCT mp ON ms.m_product_id = mp.m_product_id
-    JOIN M_LOCATOR ml ON ms.m_locator_id = ml.m_locator_id 
-    JOIN M_WAREHOUSE mw ON ml.m_warehouse_id = mw.m_warehouse_id
-    JOIN M_PRODUCTPRICE mpr ON ms.m_product_id = mpr.m_product_id
-    JOIN AD_ORG org ON org.ad_org_id = mw.ad_org_id;
+CREATE OR REPLACE VIEW M_PRODUCT_SUBSTITUTERELATED_V AS
+SELECT s.ad_client_id, 
+		s.ad_org_id, 
+		s.isactive, 
+		s.created, 
+		s.createdby, 
+		s.updated, 
+		s.updatedby,
+		s.m_product_id, 
+		s.substitute_id, 
+		'S' AS rowtype,
+		mp.name, 
+		sum(ms.qtyonhand - ms.qtyreserved) AS qtyavailable, 
+		sum(ms.qtyonhand) AS qtyonhand, 
+		sum(ms.qtyreserved) AS qtyreserved,
+		ROUND(MAX(mpr.pricestd),0) AS pricestd, 
+		mpr.m_pricelist_version_id, 
+		mw.m_warehouse_id, 
+		org.name AS orgname
+           FROM m_substitute s
+      JOIN m_storage ms ON ms.m_product_id = s.substitute_id
+   JOIN m_product mp ON ms.m_product_id = mp.m_product_id
+   JOIN m_locator ml ON ms.m_locator_id = ml.m_locator_id
+   JOIN m_warehouse mw ON ml.m_warehouse_id = mw.m_warehouse_id
+   JOIN m_productprice mpr ON ms.m_product_id = mpr.m_product_id
+   JOIN ad_org org ON org.ad_org_id = mw.ad_org_id
+   GROUP BY s.ad_client_id, 
+   			s.ad_org_id, 
+   			s.isactive, 
+   			s.created, 
+   			s.createdby, 
+   			s.updated, 
+   			s.updatedby, 
+   			s.m_product_id, 
+   			s.substitute_id, 
+   			mw.m_warehouse_id, 
+   			mpr.m_pricelist_version_id, 
+   			org.name,  
+   			mp.name
+	UNION 
+         SELECT r.ad_client_id, 
+         	r.ad_org_id, 
+         	r.isactive, 
+         	r.created, 
+         	r.createdby, 
+         	r.updated, 
+         	r.updatedby,
+			r.m_product_id, 
+			r.relatedproduct_id AS substitute_id, 
+			'R' AS rowtype,  
+			mp.name, 
+			sum(ms.qtyonhand - ms.qtyreserved) AS qtyavailable, 
+			sum(ms.qtyonhand) AS qtyonhand, 
+			sum(ms.qtyreserved) AS qtyreserved, 
+			ROUND(MAX(mpr.pricestd),0) AS pricestd, 
+			mpr.m_pricelist_version_id, 
+			mw.m_warehouse_id, 
+			org.name AS orgname
+           FROM m_relatedproduct r
+      JOIN m_storage ms ON ms.m_product_id = r.relatedproduct_id
+   JOIN m_product mp ON ms.m_product_id = mp.m_product_id
+   JOIN m_locator ml ON ms.m_locator_id = ml.m_locator_id
+   JOIN m_warehouse mw ON ml.m_warehouse_id = mw.m_warehouse_id
+   JOIN m_productprice mpr ON ms.m_product_id = mpr.m_product_id
+   JOIN ad_org org ON org.ad_org_id = mw.ad_org_id 
+   GROUP BY r.ad_client_id, 
+   			r.ad_org_id, 
+   			r.isactive, 
+   			r.created, 
+   			r.createdby, 
+   			r.updated, 
+   			r.updatedby, 
+   			r.m_product_id, 
+   			r.relatedproduct_id, 
+   			mw.m_warehouse_id, 
+   			mpr.m_pricelist_version_id, 
+   			org.name,  
+   			mp.name;
