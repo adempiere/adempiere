f885fcc2ba7b8ba642a6ac7920276065bb5907b3
phib <devnull@localhost>
2010-02-08 05:10:43 +0000

    BT2944388 "No SQL" error when deleting report cube Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2944388

---
+++ migration/354a-trunk/postgresql/662_BT_2944388.sql
@@ -0,0 +1,5 @@
+-- Feb 8, 2010 4:05:08 PM EST
+-- BF2944388
+UPDATE AD_Column SET IsIdentifier='Y', SeqNo=1,Updated=TO_TIMESTAMP('2010-02-08 16:05:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=57563
+;
+
