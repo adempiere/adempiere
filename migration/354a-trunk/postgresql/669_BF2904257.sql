e27a52c2ed60df5c6819980b634338fe90ade9f6
globalqss <devnull@localhost>
2010-03-01 17:30:14 +0000

    Found the NOT EXISTS part of the validation is not needed as it's controlled in MRMALine.beforeSave - better drop the NOT EXISTS part for backporting compatibility with 343 and 355 Fix [2904257] wrong validation sql for "M_InOutShipment/Receipt (RMA)" Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2904257

---
+++ migration/354a-trunk/postgresql/669_BF2904257.sql
@@ -0,0 +1,5 @@
+-- 01-mar-2010 12:10:16 COT
+-- Bug_2904257_wrong validation sql for M_InOutShipment/Receipt (RMA)
+UPDATE AD_Val_Rule SET Code='M_InOutLine.M_InOut_ID=@InOut_ID@',Updated=TO_TIMESTAMP('2010-03-01 12:10:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Val_Rule_ID=52001
+;
+
