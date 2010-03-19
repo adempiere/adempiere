e50c201e189585c1de23702ab0c697c07dae652d
globalqss <devnull@localhost>
2010-03-19 00:21:42 +0000

    BF [2493555] Language Table, missing record for ms_MY - synchronized country, currency and language with compiere 3.2.0 Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2493555

---
+++ migration/354a-trunk/oracle/684_BF2493555.sql
@@ -0,0 +1,117 @@
+-- Mar 18, 2010 6:51:36 PM COT
+-- 2493555_Language Table, missing record for  ms_MY
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'ms_MY',50003,0,'MY',TO_DATE('2010-03-18 18:51:35','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','ms','Malay (Malaysia)','N',TO_DATE('2010-03-18 18:51:35','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:51:52 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'el_CY',50004,0,'CY',TO_DATE('2010-03-18 18:51:52','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','el','Greek (Cyprus)','N',TO_DATE('2010-03-18 18:51:52','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:52:14 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'en_MT',50005,0,'MT',TO_DATE('2010-03-18 18:52:14','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','en','English (Malta)','N',TO_DATE('2010-03-18 18:52:14','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:52:33 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'en_PH',50006,0,'PH',TO_DATE('2010-03-18 18:52:33','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','en','English (Philippines)','N',TO_DATE('2010-03-18 18:52:33','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:52:48 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'en_SG',50007,0,'SG',TO_DATE('2010-03-18 18:52:48','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','en','English (Singapore)','N',TO_DATE('2010-03-18 18:52:48','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:53:06 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'es_US',50008,0,'US',TO_DATE('2010-03-18 18:53:06','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','es','Spanish (USA)','N',TO_DATE('2010-03-18 18:53:06','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:53:22 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'ga_IE',50009,0,'IE',TO_DATE('2010-03-18 18:53:22','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','ga','Irish (Ireland)','N',TO_DATE('2010-03-18 18:53:22','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:53:36 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'in_ID',50010,0,'ID',TO_DATE('2010-03-18 18:53:36','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','in','Indonesian (Indonesia)','N',TO_DATE('2010-03-18 18:53:36','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:53:50 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'mt_MT',50011,0,'MT',TO_DATE('2010-03-18 18:53:50','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','mt','Maltese (Malta)','N',TO_DATE('2010-03-18 18:53:50','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:55:52 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'sr_BA',50012,0,'BA',TO_DATE('2010-03-18 18:55:52','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','sr','Serbian (Bosnia and Herzegovina)','N',TO_DATE('2010-03-18 18:55:52','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:56:05 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'sr_CS',50013,0,'CS',TO_DATE('2010-03-18 18:56:05','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','sr','Serbian (Serbia and Montenegro)','N',TO_DATE('2010-03-18 18:56:05','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:56:16 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'sr_ME',50014,0,'ME',TO_DATE('2010-03-18 18:56:16','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','sr','Serbian (Montenegro)','N',TO_DATE('2010-03-18 18:56:16','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:56:29 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'sr_RS',50015,0,'RS',TO_DATE('2010-03-18 18:56:29','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','sr','Serbian (Serbia)','N',TO_DATE('2010-03-18 18:56:29','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 6:56:41 PM COT
+INSERT INTO AD_Language (AD_Client_ID,AD_Language,AD_Language_ID,AD_Org_ID,CountryCode,Created,CreatedBy,IsActive,IsBaseLanguage,IsDecimalPoint,IsSystemLanguage,LanguageISO,Name,Processing,Updated,UpdatedBy) VALUES (0,'zh_SG',50016,0,'SG',TO_DATE('2010-03-18 18:56:41','YYYY-MM-DD HH24:MI:SS'),100,'Y','N','Y','N','zh','Chinese (Singapore)','N',TO_DATE('2010-03-18 18:56:41','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 7:08:42 PM COT
+UPDATE C_Country SET AD_Language='in_ID',Updated=TO_DATE('2010-03-18 19:08:42','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=209
+;
+
+-- Mar 18, 2010 7:14:36 PM COT
+INSERT INTO C_Country (AD_Client_ID,AD_Language,AD_Org_ID,AllowCitiesOutOfList,C_Country_ID,CountryCode,Created,CreatedBy,Description,DisplaySequence,HasPostal_Add,HasRegion,IsActive,IsAddressLinesLocalReverse,IsAddressLinesReverse,IsPostcodeLookup,Name,RegionName,Updated,UpdatedBy) VALUES (0,NULL,0,'Y',50000,'AX',TO_DATE('2010-03-18 19:14:35','YYYY-MM-DD HH24:MI:SS'),100,'Åland Islands','@C@,  @P@','N','N','Y','N','N','N','Åland Islands','State',TO_DATE('2010-03-18 19:14:35','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 7:14:36 PM COT
+INSERT INTO C_Country_Trl (AD_Language,C_Country_ID, Description,Name,RegionName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.C_Country_ID, t.Description,t.Name,t.RegionName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, C_Country t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.C_Country_ID=50000 AND NOT EXISTS (SELECT * FROM C_Country_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.C_Country_ID=t.C_Country_ID)
+;
+
+-- Mar 18, 2010 7:15:45 PM COT
+UPDATE C_Country SET CaptureSequence='@A1@ @A2@ @A3@ @A4@ @C@ @P@ @CO@',Updated=TO_DATE('2010-03-18 19:15:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=50000
+;
+
+-- Mar 18, 2010 7:16:02 PM COT
+INSERT INTO C_Country (AD_Client_ID,AD_Org_ID,AllowCitiesOutOfList,CaptureSequence,C_Country_ID,CountryCode,Created,CreatedBy,Description,DisplaySequence,HasPostal_Add,HasRegion,IsActive,IsAddressLinesLocalReverse,IsAddressLinesReverse,IsPostcodeLookup,Name,RegionName,Updated,UpdatedBy) VALUES (0,0,'Y','@A1@ @A2@ @A3@ @A4@ @C@ @P@ @CO@',50001,'BL',TO_DATE('2010-03-18 19:16:02','YYYY-MM-DD HH24:MI:SS'),100,'Saint Barthélemy','@C@,  @P@','N','N','Y','N','N','N','Saint Barthélemy','State',TO_DATE('2010-03-18 19:16:02','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 7:16:03 PM COT
+INSERT INTO C_Country_Trl (AD_Language,C_Country_ID, Description,Name,RegionName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.C_Country_ID, t.Description,t.Name,t.RegionName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, C_Country t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.C_Country_ID=50001 AND NOT EXISTS (SELECT * FROM C_Country_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.C_Country_ID=t.C_Country_ID)
+;
+
+-- Mar 18, 2010 7:16:26 PM COT
+INSERT INTO C_Country (AD_Client_ID,AD_Language,AD_Org_ID,AllowCitiesOutOfList,CaptureSequence,C_Country_ID,CountryCode,Created,CreatedBy,Description,DisplaySequence,HasPostal_Add,HasRegion,IsActive,IsAddressLinesLocalReverse,IsAddressLinesReverse,IsPostcodeLookup,Name,RegionName,Updated,UpdatedBy) VALUES (0,'en_US',0,'Y','@A1@ @A2@ @A3@ @A4@ @C@ @P@ @CO@',50002,'GG',TO_DATE('2010-03-18 19:16:25','YYYY-MM-DD HH24:MI:SS'),100,NULL,'@C@,  @P@','N','N','Y','N','N','N','Guernsey','State',TO_DATE('2010-03-18 19:16:25','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 7:16:26 PM COT
+INSERT INTO C_Country_Trl (AD_Language,C_Country_ID, Description,Name,RegionName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.C_Country_ID, t.Description,t.Name,t.RegionName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, C_Country t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.C_Country_ID=50002 AND NOT EXISTS (SELECT * FROM C_Country_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.C_Country_ID=t.C_Country_ID)
+;
+
+-- Mar 18, 2010 7:16:48 PM COT
+INSERT INTO C_Country (AD_Client_ID,AD_Org_ID,AllowCitiesOutOfList,CaptureSequence,C_Country_ID,CountryCode,Created,CreatedBy,Description,DisplaySequence,HasPostal_Add,HasRegion,IsActive,IsAddressLinesLocalReverse,IsAddressLinesReverse,IsPostcodeLookup,Name,RegionName,Updated,UpdatedBy) VALUES (0,0,'Y','@A1@ @A2@ @A3@ @A4@ @C@ @P@ @CO@',50003,'IM',TO_DATE('2010-03-18 19:16:46','YYYY-MM-DD HH24:MI:SS'),100,'Isle of Man','@C@,  @P@','N','N','Y','N','N','N','Isle of Man','State',TO_DATE('2010-03-18 19:16:46','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 7:16:48 PM COT
+INSERT INTO C_Country_Trl (AD_Language,C_Country_ID, Description,Name,RegionName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.C_Country_ID, t.Description,t.Name,t.RegionName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, C_Country t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.C_Country_ID=50003 AND NOT EXISTS (SELECT * FROM C_Country_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.C_Country_ID=t.C_Country_ID)
+;
+
+-- Mar 18, 2010 7:17:04 PM COT
+INSERT INTO C_Country (AD_Client_ID,AD_Org_ID,AllowCitiesOutOfList,CaptureSequence,C_Country_ID,CountryCode,Created,CreatedBy,Description,DisplaySequence,HasPostal_Add,HasRegion,IsActive,IsAddressLinesLocalReverse,IsAddressLinesReverse,IsPostcodeLookup,Name,RegionName,Updated,UpdatedBy) VALUES (0,0,'Y','@A1@ @A2@ @A3@ @A4@ @C@ @P@ @CO@',50004,'JE',TO_DATE('2010-03-18 19:17:04','YYYY-MM-DD HH24:MI:SS'),100,'Jersey','@C@,  @P@','N','N','Y','N','N','N','Jersey','State',TO_DATE('2010-03-18 19:17:04','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 7:17:04 PM COT
+INSERT INTO C_Country_Trl (AD_Language,C_Country_ID, Description,Name,RegionName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.C_Country_ID, t.Description,t.Name,t.RegionName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, C_Country t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.C_Country_ID=50004 AND NOT EXISTS (SELECT * FROM C_Country_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.C_Country_ID=t.C_Country_ID)
+;
+
+-- Mar 18, 2010 7:17:21 PM COT
+INSERT INTO C_Country (AD_Client_ID,AD_Org_ID,AllowCitiesOutOfList,CaptureSequence,C_Country_ID,CountryCode,Created,CreatedBy,Description,DisplaySequence,HasPostal_Add,HasRegion,IsActive,IsAddressLinesLocalReverse,IsAddressLinesReverse,IsPostcodeLookup,Name,RegionName,Updated,UpdatedBy) VALUES (0,0,'Y','@A1@ @A2@ @A3@ @A4@ @C@ @P@ @CO@',50005,'MF',TO_DATE('2010-03-18 19:17:19','YYYY-MM-DD HH24:MI:SS'),100,'Saint Martin','@C@,  @P@','N','N','Y','N','N','N','Saint Martin','State',TO_DATE('2010-03-18 19:17:19','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 18, 2010 7:17:21 PM COT
+INSERT INTO C_Country_Trl (AD_Language,C_Country_ID, Description,Name,RegionName, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.C_Country_ID, t.Description,t.Name,t.RegionName, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, C_Country t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.C_Country_ID=50005 AND NOT EXISTS (SELECT * FROM C_Country_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.C_Country_ID=t.C_Country_ID)
+;
+
+-- Mar 18, 2010 7:17:36 PM COT
+UPDATE C_Country SET AD_Language='ms_MY',Updated=TO_DATE('2010-03-18 19:17:36','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=238
+;
+
