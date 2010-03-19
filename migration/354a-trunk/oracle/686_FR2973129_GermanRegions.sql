faf4cdfd2f7819bde5966ffb1ade8176a6998939
trifonnt <devnull@localhost>
2010-03-19 12:16:52 +0000

    -- FR[2973129]-Add German Regions https://sourceforge.net/tracker/?func=detail&aid=2973129&group_id=176962&atid=883808

---
+++ migration/354a-trunk/oracle/686_FR2973129_GermanRegions.sql
@@ -0,0 +1,95 @@
+-- Mar 19, 2010 12:28:52 PM CET
+-- FR[2973129]-Add German Regions
+UPDATE C_Country SET HasRegion='Y',Updated=TO_DATE('2010-03-19 12:28:52','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=101
+;
+
+-- Mar 19, 2010 12:29:35 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:29:34','YYYY-MM-DD HH24:MI:SS'),100,50002,'Y','N','Baden-Württemberg',TO_DATE('2010-03-19 12:29:34','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:29:51 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:29:49','YYYY-MM-DD HH24:MI:SS'),100,50003,'Y','N','Bayern',TO_DATE('2010-03-19 12:29:49','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:30:02 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:30:01','YYYY-MM-DD HH24:MI:SS'),100,50004,'Y','N','Berlin',TO_DATE('2010-03-19 12:30:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:31:07 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:31:07','YYYY-MM-DD HH24:MI:SS'),100,50005,'Y','N','Brandenburg',TO_DATE('2010-03-19 12:31:07','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:31:20 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:31:19','YYYY-MM-DD HH24:MI:SS'),100,50006,'Y','N','Bremen',TO_DATE('2010-03-19 12:31:19','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:31:32 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:31:30','YYYY-MM-DD HH24:MI:SS'),100,50007,'Y','N','Hamburg',TO_DATE('2010-03-19 12:31:30','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:31:46 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:31:45','YYYY-MM-DD HH24:MI:SS'),100,50008,'Y','N','Hessen',TO_DATE('2010-03-19 12:31:45','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:32:31 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:32:29','YYYY-MM-DD HH24:MI:SS'),100,50009,'Y','N','Mecklenburg-Vorpommern',TO_DATE('2010-03-19 12:32:29','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:32:49 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:32:49','YYYY-MM-DD HH24:MI:SS'),100,50010,'Y','N','Niedersachsen',TO_DATE('2010-03-19 12:32:49','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:33:02 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:33:01','YYYY-MM-DD HH24:MI:SS'),100,50011,'Y','N','Nordrhein-Westfalen',TO_DATE('2010-03-19 12:33:01','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:33:14 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:33:13','YYYY-MM-DD HH24:MI:SS'),100,50012,'Y','N','Rheinland-Pfalz',TO_DATE('2010-03-19 12:33:13','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:33:26 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:33:25','YYYY-MM-DD HH24:MI:SS'),100,50013,'Y','N','Saarland',TO_DATE('2010-03-19 12:33:25','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:33:37 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:33:36','YYYY-MM-DD HH24:MI:SS'),100,50014,'Y','N','Sachsen',TO_DATE('2010-03-19 12:33:36','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:33:48 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:33:47','YYYY-MM-DD HH24:MI:SS'),100,50015,'Y','N','Sachsen-Anhalt',TO_DATE('2010-03-19 12:33:47','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:34:00 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:33:59','YYYY-MM-DD HH24:MI:SS'),100,50016,'Y','N','Schleswig-Holstein',TO_DATE('2010-03-19 12:33:59','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:34:10 PM CET
+-- FR[2973129]-Add German Regions
+INSERT INTO C_Region (AD_Client_ID,AD_Org_ID,C_Country_ID,Created,CreatedBy,C_Region_ID,IsActive,IsDefault,Name,Updated,UpdatedBy) VALUES (0,0,101,TO_DATE('2010-03-19 12:34:10','YYYY-MM-DD HH24:MI:SS'),100,50017,'Y','N','Thüringen',TO_DATE('2010-03-19 12:34:10','YYYY-MM-DD HH24:MI:SS'),100)
+;
+
+-- Mar 19, 2010 12:34:43 PM CET
+-- FR[2973129]-Add German Regions
+UPDATE C_Country SET RegionName='Bundesland',Updated=TO_DATE('2010-03-19 12:34:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=101
+;
+
+-- Mar 19, 2010 12:34:43 PM CET
+-- FR[2973129]-Add German Regions
+UPDATE C_Country_Trl SET IsTranslated='N' WHERE C_Country_ID=101
+;
+
