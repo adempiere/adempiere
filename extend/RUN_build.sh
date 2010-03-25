fe165f3a676140e9f48f66f77e57c9148158525d
trifonnt <devnull@localhost>
2010-03-25 16:00:41 +0000

    Ident xml file. Remove not used packages.txt and Libero xml. Fix typo mistakes in .sh script

--- extend/RUN_build.sh
+++ extend/RUN_build.sh
@@ -4,13 +4,13 @@
 
 SAVED_DIR=`pwd`			#save current dir
 cd `dirname $0`/../utils_dev	#change dir to place where script resides - doesn not work with sym links
-UTILS_DEV=`pwd`			#this is adempiere source
+UTILS_DEV=`pwd`			#this is ADempiere source
 cd $SAVED_DIR			#back to the saved directory
 
 .  $UTILS_DEV/myDevEnv.sh	#call environment
 echo done
 if [ ! $ADEMPIERE_ENV==Y ] ; then
-    echo "Can't set developemeent environemnt - check myDevEnv.sh"
+    echo "Can't set development environment - check myDevEnv.sh"
     exit 1
 fi
 
