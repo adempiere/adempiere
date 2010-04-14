ff78fa5bfb94e59e6aca7fc12d6dbe1021aefea2
trifonnt <devnull@localhost>
2010-04-14 12:33:28 +0000

    -- BF [2987097] - Wrong column size: VendorProductNo -- https://sourceforge.net/tracker/?func=detail&aid=2987097&group_id=176962&atid=879332

---
+++ migration/354a-trunk/postgresql/709_BF2987097_VendorProductNo.sql
@@ -0,0 +1,24 @@
+-- Apr 14, 2010 2:13:17 PM CEST
+-- BF [2987097] - Wrong column size: VendorProductNo
+-- https://sourceforge.net/tracker/?func=detail&aid=2987097&group_id=176962&atid=879332
+UPDATE AD_Column SET FieldLength=40,Updated=TO_TIMESTAMP('2010-04-14 14:13:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=7818
+;
+
+-- Apr 14, 2010 2:13:22 PM CEST
+-- BF [2987097] - Wrong column size: VendorProductNo
+-- https://sourceforge.net/tracker/?func=detail&aid=2987097&group_id=176962&atid=879332
+INSERT INTO t_alter_column values('i_product','VendorProductNo','VARCHAR(40)',null,'NULL')
+;
+
+-- Apr 14, 2010 2:14:09 PM CEST
+-- BF [2987097] - Wrong column size: VendorProductNo
+-- https://sourceforge.net/tracker/?func=detail&aid=2987097&group_id=176962&atid=879332
+UPDATE AD_Column SET FieldLength=40,Updated=TO_TIMESTAMP('2010-04-14 14:14:09','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2709
+;
+
+-- Apr 14, 2010 2:14:15 PM CEST
+-- BF [2987097] - Wrong column size: VendorProductNo
+-- https://sourceforge.net/tracker/?func=detail&aid=2987097&group_id=176962&atid=879332
+INSERT INTO t_alter_column values('m_product_po','VendorProductNo','VARCHAR(40)',null,null)
+;
+
