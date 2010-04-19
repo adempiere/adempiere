a226e54ab01f95e6bda1ec8882b4303bf7e42c74
trifonnt <devnull@localhost>
2010-04-19 15:51:08 +0000

    -- BF [2989431] - Country: Germany, add Region -- https://sourceforge.net/tracker/?func=detail&aid=2989431&group_id=176962&atid=879332

---
+++ migration/354a-trunk/postgresql/715_BF2989431.sql
@@ -0,0 +1,6 @@
+-- Apr 19, 2010 5:30:18 PM CEST
+-- BF [2989431] - Country: Germany, add Region
+-- https://sourceforge.net/tracker/?func=detail&aid=2989431&group_id=176962&atid=879332
+UPDATE C_Country SET CaptureSequence='@A1@ @A2@ @A3@ @A4@ D-@P@ @R@ @C@ @CO@',Updated=TO_TIMESTAMP('2010-04-19 17:30:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE C_Country_ID=101
+;
+
