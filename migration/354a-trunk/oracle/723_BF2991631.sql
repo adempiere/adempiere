c8f8cdb05ae9f3129a86ba5bdf4a67d2975df93d
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-04-24 04:02:37 +0000

    apply the Carlos's suggestion, Thank a lot
    kind regards
    Victor Perez
    www.e-evolution.com
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2991631

--- migration/354a-trunk/oracle/723_BF2991631.sql
+++ migration/354a-trunk/oracle/723_BF2991631.sql
@@ -1,5 +1,31 @@
--- 23-abr-2010 16:37:17 CDT
--- Default null to QtyInTransit
-UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_DATE('2010-04-23 16:37:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53927
+-- 23-abr-2010 22:55:57 CDT
+-- fix default value
+UPDATE AD_Column SET DefaultValue=NULL, ValueMax=NULL,Updated=TO_DATE('2010-04-23 22:55:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53954
 ;
+
+-- 23-abr-2010 22:56:00 CDT
+-- fix default value
+ALTER TABLE DD_OrderLine MODIFY QtyEntered NUMBER DEFAULT NULL 
+;
+
+-- 23-abr-2010 22:56:19 CDT
+-- fix default value
+UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_DATE('2010-04-23 22:56:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53927
+;
+
+-- 23-abr-2010 22:56:23 CDT
+-- fix default value
+ALTER TABLE DD_OrderLine MODIFY QtyInTransit NUMBER DEFAULT NULL 
+;
+
+-- 23-abr-2010 22:56:34 CDT
+-- fix default value
+UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_DATE('2010-04-23 22:56:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53928
+;
+
+-- 23-abr-2010 22:56:37 CDT
+-- fix default value
+ALTER TABLE DD_OrderLine MODIFY QtyOrdered NUMBER DEFAULT NULL 
+;
+
 
--- migration/354a-trunk/postgresql/723_BF2991631.sql
+++ migration/354a-trunk/postgresql/723_BF2991631.sql
@@ -1,5 +1,29 @@
--- 23-abr-2010 16:37:17 CDT
--- Default null to QtyInTransit
-UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_TIMESTAMP('2010-04-23 16:37:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53927
+-- 23-abr-2010 22:55:57 CDT
+-- fix default value
+UPDATE AD_Column SET DefaultValue=NULL, ValueMax=NULL,Updated=TO_TIMESTAMP('2010-04-23 22:55:57','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53954
 ;
 
+-- 23-abr-2010 22:56:00 CDT
+-- fix default value
+INSERT INTO t_alter_column values('dd_orderline','QtyEntered','NUMERIC',null,'NULL')
+;
+
+-- 23-abr-2010 22:56:19 CDT
+-- fix default value
+UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_TIMESTAMP('2010-04-23 22:56:19','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53927
+;
+
+-- 23-abr-2010 22:56:23 CDT
+-- fix default value
+INSERT INTO t_alter_column values('dd_orderline','QtyInTransit','NUMERIC',null,'NULL')
+;
+
+-- 23-abr-2010 22:56:34 CDT
+-- fix default value
+UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_TIMESTAMP('2010-04-23 22:56:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53928
+;
+
+-- 23-abr-2010 22:56:37 CDT
+-- fix default value
+INSERT INTO t_alter_column values('dd_orderline','QtyOrdered','NUMERIC',null,'NULL')
+;
