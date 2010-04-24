2b17df2fe1ca6a09e323e53df004757b1c844d00
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-04-24 03:36:34 +0000

    Error Qty In transit have default 1 and should be null Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2991631

---
+++ migration/354a-trunk/oracle/723_BF2991631.sql
@@ -0,0 +1,5 @@
+-- 23-abr-2010 16:37:17 CDT
+-- Default null to QtyInTransit
+UPDATE AD_Column SET DefaultValue=NULL,Updated=TO_DATE('2010-04-23 16:37:17','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53927
+;
+
