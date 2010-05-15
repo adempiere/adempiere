01dd122cc005bfb27533f2d5785b0daa6e5df8bb
trifonnt <devnull@localhost>
2010-05-15 16:24:04 +0000

    -- BF [3002076] -  Merge entities not working ! -- https://sourceforge.net/tracker/?func=detail&atid=879332&aid=3002076&group_id=176962

---
+++ migration/354a-trunk/oracle/727_BF3002076.sql
@@ -0,0 +1,12 @@
+-- May 15, 2010 6:16:55 PM CEST
+-- BF [3002076] -  Merge entities not working !
+-- https://sourceforge.net/tracker/?func=detail&atid=879332&aid=3002076&group_id=176962
+UPDATE AD_Table SET IsDeleteable='N', IsView='Y',Updated=TO_DATE('2010-05-15 18:16:55','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=53247
+;
+
+-- May 15, 2010 6:17:43 PM CEST
+-- BF [3002076] -  Merge entities not working !
+-- https://sourceforge.net/tracker/?func=detail&atid=879332&aid=3002076&group_id=176962
+UPDATE AD_Table SET IsView='Y',Updated=TO_DATE('2010-05-15 18:17:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Table_ID=53033
+;
+
