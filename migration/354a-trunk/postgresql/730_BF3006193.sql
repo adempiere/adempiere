c2d21bcae4ef3f5c95f8feffdb8d2142b9183b21
trifonnt <devnull@localhost>
2010-05-24 15:53:04 +0000

    BF [3006193] - Only Manufacturing Resources in Manufacturing WFs https://sourceforge.net/tracker/?func=detail&atid=879332&aid=3006193&group_id=176962

---
+++ migration/354a-trunk/postgresql/730_BF3006193.sql
@@ -0,0 +1,11 @@
+-- 05-23-2010 04:48:37 PM CST
+-- BF [3006193] - Only Manufacturing Resources in Manufacturing WFs
+-- https://sourceforge.net/tracker/?func=detail&atid=879332&aid=3006193&group_id=176962
+UPDATE AD_Column SET AD_Val_Rule_ID=52059,Updated=TO_TIMESTAMP('2010-05-23 16:48:37','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53311
+;
+
+-- 05-23-2010 05:03:34 PM CST
+-- BF [3006193] - Only Manufacturing Resources in Manufacturing WFs
+-- https://sourceforge.net/tracker/?func=detail&atid=879332&aid=3006193&group_id=176962
+UPDATE AD_Column SET AD_Val_Rule_ID=52059,Updated=TO_TIMESTAMP('2010-05-23 17:03:34','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53314
+;
