c4d5899c440e85241f31d037504c7c842e4c06e3
globalqss <devnull@localhost>
2010-06-14 18:52:59 +0000

    [3013401] IsCreatePlan in table PP_Product_Planning default=Y - Peer review -> sync database default Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3013401

--- migration/354a-trunk/oracle/734_FR3013401_IsPlan.sql
+++ migration/354a-trunk/oracle/734_FR3013401_IsPlan.sql
@@ -3,3 +3,13 @@
 UPDATE AD_Column SET DefaultValue='Y',Updated=TO_DATE('2010-06-08 12:47:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53382
 ;
 
+-- Jun 14, 2010 1:50:44 PM COT
+-- FR3013401_IsCreatePlan default value=Y
+ALTER TABLE PP_Product_Planning MODIFY IsCreatePlan CHAR(1) DEFAULT 'Y'
+;
+
+-- Jun 14, 2010 1:50:45 PM COT
+-- FR3013401_IsCreatePlan default value=Y
+UPDATE PP_Product_Planning SET IsCreatePlan='Y' WHERE IsCreatePlan IS NULL
+;
+
--- migration/354a-trunk/postgresql/734_FR3013401_IsPlan.sql
+++ migration/354a-trunk/postgresql/734_FR3013401_IsPlan.sql
@@ -3,3 +3,13 @@
 UPDATE AD_Column SET DefaultValue='Y',Updated=TO_TIMESTAMP('2010-06-08 12:47:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53382
 ;
 
+-- Jun 14, 2010 1:50:44 PM COT
+-- FR3013401_IsCreatePlan default value=Y
+INSERT INTO t_alter_column values('pp_product_planning','IsCreatePlan','CHAR(1)',null,'Y')
+;
+
+-- Jun 14, 2010 1:50:45 PM COT
+-- FR3013401_IsCreatePlan default value=Y
+UPDATE PP_Product_Planning SET IsCreatePlan='Y' WHERE IsCreatePlan IS NULL
+;
+
