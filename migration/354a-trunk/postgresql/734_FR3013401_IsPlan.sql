c9d82e6a2b27e0a54991bcfc9a845a2aebea4289
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-06-14 01:54:25 +0000

    IsCreatePlan in table PP_Product_Planning default=Y
    Thank you Mario for your improve
    
    kind regards
    Victor Perez
    www.e-evolution.com
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3013401

---
+++ migration/354a-trunk/postgresql/734_FR3013401_IsPlan.sql
@@ -0,0 +1,5 @@
+-- 06-08-2010 12:47:16 PM CST
+-- IsCreatePlan default value=Y
+UPDATE AD_Column SET DefaultValue='Y',Updated=TO_TIMESTAMP('2010-06-08 12:47:16','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=53382
+;
+
