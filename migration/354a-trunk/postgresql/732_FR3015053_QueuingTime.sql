5e785f3e0be448306447e3b40afce2b114db4164
globalqss <devnull@localhost>
2010-06-14 01:21:28 +0000

    Peer review Revision 12517 fix typo modification must be done at element / it replicates to all other levels Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3015053

--- migration/354a-trunk/postgresql/732_FR3015053_QueuingTime.sql
+++ migration/354a-trunk/postgresql/732_FR3015053_QueuingTime.sql
@@ -7,3 +7,34 @@ UPDATE AD_Field SET Description='Queue time is the time a ob waits at a work cen
 -- queuing time_explanation
 UPDATE AD_Field SET Name='Queuing Time', Description='Queue time is the time a ob waits at a work center before begin handled.', Help='Queuing time has no implication on costs, but on Capacity Requirement Planning (CRP) to calculate the total time needed to manufacture a product.' WHERE AD_Column_ID=53310 AND IsCentrallyMaintained='Y'
 ;
+
+-- Jun 13, 2010 8:18:43 PM COT
+-- FR3015053_queuing time_explanation
+UPDATE AD_Element SET Description='Queue time is the time a job waits at a work center before begin handled.', Help='Queuing time has no implication on costs, but on Capacity Requirement Planning (CRP) to calculate the total time needed to manufacture a product.',Updated=TO_TIMESTAMP('2010-06-13 20:18:43','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Element_ID=53234
+;
+
+-- Jun 13, 2010 8:18:43 PM COT
+-- FR3015053_queuing time_explanation
+UPDATE AD_Element_Trl SET IsTranslated='N' WHERE AD_Element_ID=53234
+;
+
+-- Jun 13, 2010 8:18:43 PM COT
+-- FR3015053_queuing time_explanation
+UPDATE AD_Column SET ColumnName='QueuingTime', Name='Queuing Time', Description='Queue time is the time a job waits at a work center before begin handled.', Help='Queuing time has no implication on costs, but on Capacity Requirement Planning (CRP) to calculate the total time needed to manufacture a product.' WHERE AD_Element_ID=53234
+;
+
+-- Jun 13, 2010 8:18:43 PM COT
+-- FR3015053_queuing time_explanation
+UPDATE AD_Process_Para SET ColumnName='QueuingTime', Name='Queuing Time', Description='Queue time is the time a job waits at a work center before begin handled.', Help='Queuing time has no implication on costs, but on Capacity Requirement Planning (CRP) to calculate the total time needed to manufacture a product.', AD_Element_ID=53234 WHERE UPPER(ColumnName)='QUEUINGTIME' AND IsCentrallyMaintained='Y' AND AD_Element_ID IS NULL
+;
+
+-- Jun 13, 2010 8:18:43 PM COT
+-- FR3015053_queuing time_explanation
+UPDATE AD_Process_Para SET ColumnName='QueuingTime', Name='Queuing Time', Description='Queue time is the time a job waits at a work center before begin handled.', Help='Queuing time has no implication on costs, but on Capacity Requirement Planning (CRP) to calculate the total time needed to manufacture a product.' WHERE AD_Element_ID=53234 AND IsCentrallyMaintained='Y'
+;
+
+-- Jun 13, 2010 8:18:43 PM COT
+-- FR3015053_queuing time_explanation
+UPDATE AD_Field SET Name='Queuing Time', Description='Queue time is the time a job waits at a work center before begin handled.', Help='Queuing time has no implication on costs, but on Capacity Requirement Planning (CRP) to calculate the total time needed to manufacture a product.' WHERE AD_Column_ID IN (SELECT AD_Column_ID FROM AD_Column WHERE AD_Element_ID=53234) AND IsCentrallyMaintained='Y'
+;
+
