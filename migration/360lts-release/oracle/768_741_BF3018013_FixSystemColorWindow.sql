c789228c8cfee565b1ce50f34c492b3cfa696dc6
teo_sarca <devnull@localhost>
2010-06-18 12:34:41 +0000

    Fix System Color Window Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3018013 (transplanted from af890ca435cb90f8faceefce29e4d352342394e9)

---
+++ migration/360lts-release/oracle/768_741_BF3018013_FixSystemColorWindow.sql
@@ -0,0 +1,145 @@
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=0,IsDisplayed='N' WHERE AD_Field_ID=4957
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=10,IsDisplayed='Y' WHERE AD_Field_ID=4958
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=20,IsDisplayed='Y' WHERE AD_Field_ID=4959
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=30,IsDisplayed='Y' WHERE AD_Field_ID=4961
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=40,IsDisplayed='Y' WHERE AD_Field_ID=4960
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=50,IsDisplayed='Y' WHERE AD_Field_ID=4962
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=60,IsDisplayed='Y' WHERE AD_Field_ID=4963
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=70,IsDisplayed='Y' WHERE AD_Field_ID=4964
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=80,IsDisplayed='Y' WHERE AD_Field_ID=4969
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=90,IsDisplayed='Y' WHERE AD_Field_ID=4965
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=100,IsDisplayed='Y' WHERE AD_Field_ID=4970
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=110,IsDisplayed='Y' WHERE AD_Field_ID=4966
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=120,IsDisplayed='Y' WHERE AD_Field_ID=4971
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=130,IsDisplayed='Y' WHERE AD_Field_ID=4967
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=140,IsDisplayed='Y' WHERE AD_Field_ID=4972
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=150,IsDisplayed='Y' WHERE AD_Field_ID=5329
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=160,IsDisplayed='Y' WHERE AD_Field_ID=5328
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=170,IsDisplayed='Y' WHERE AD_Field_ID=4973
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=180,IsDisplayed='Y' WHERE AD_Field_ID=4974
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=190,IsDisplayed='Y' WHERE AD_Field_ID=4968
+;
+
+-- Jun 18, 2010 3:27:29 PM EEST
+-- -
+UPDATE AD_Field SET SeqNo=200,IsDisplayed='Y' WHERE AD_Field_ID=5327
+;
+
+-- Jun 18, 2010 3:28:50 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic='@ColorType@=F | @ColorType@=G | @ColorType@=L',Updated=TO_DATE('2010-06-18 15:28:50','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=4964
+;
+
+-- Jun 18, 2010 3:28:59 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic='@ColorType@=F | @ColorType@=G | @ColorType@=L',Updated=TO_DATE('2010-06-18 15:28:59','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=4965
+;
+
+-- Jun 18, 2010 3:29:05 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic='@ColorType@=F | @ColorType@=G | @ColorType@=L',Updated=TO_DATE('2010-06-18 15:29:05','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=4966
+;
+
+-- Jun 18, 2010 3:29:27 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_DATE('2010-06-18 15:29:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=4964
+;
+
+-- Jun 18, 2010 3:29:33 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_DATE('2010-06-18 15:29:33','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=4965
+;
+
+-- Jun 18, 2010 3:29:40 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic=NULL,Updated=TO_DATE('2010-06-18 15:29:40','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=4966
+;
+
+-- Jun 18, 2010 3:30:48 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic='@ColorType@=T',Updated=TO_DATE('2010-06-18 15:30:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=5327
+;
+
+-- Jun 18, 2010 3:30:53 PM EEST
+-- -
+UPDATE AD_Field SET DisplayLogic='@ColorType@=T',Updated=TO_DATE('2010-06-18 15:30:53','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=4968
+;
+
