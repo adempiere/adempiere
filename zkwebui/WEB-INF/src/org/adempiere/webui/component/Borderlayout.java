fb12546b11b76d7c26ca9d5e5b1c75fd34e91ed8
teo_sarca <devnull@localhost>
2010-06-19 08:10:08 +0000

    Borderlayout helper class Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3018339

---
+++ zkwebui/WEB-INF/src/org/adempiere/webui/component/Borderlayout.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package org.adempiere.webui.component;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zkex.zul.Center;
+import org.zkoss.zkex.zul.East;
+import org.zkoss.zkex.zul.North;
+import org.zkoss.zkex.zul.South;
+import org.zkoss.zkex.zul.West;
+
+/**
+ * @author teo_sarca
+ *
+ */
+public class Borderlayout extends org.zkoss.zkex.zul.Borderlayout
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6976820221945897268L;
+	
+	public Borderlayout()
+	{
+		super();
+	}
+	
+	public Borderlayout appendNorth(Component c)
+	{
+		North north = getNorth();
+		if (north == null)
+		{
+			north = new North();
+			this.appendChild(north);
+		}
+		north.appendChild(c);
+		return this;
+	}
+	public Borderlayout appendWest(Component c)
+	{
+		West west = getWest();
+		if (west == null)
+		{
+			west = new West();
+			this.appendChild(west);
+		}
+		west.appendChild(c);
+		return this;
+	}
+	public Borderlayout appendSouth(Component c)
+	{
+		South south = getSouth();
+		if (south == null)
+		{
+			south = new South();
+			this.appendChild(south);
+		}
+		south.appendChild(c);
+		return this;
+	}
+	public Borderlayout appendEast(Component c)
+	{
+		East east = getEast();
+		if (east == null)
+		{
+			east = new East();
+			this.appendChild(east);
+		}
+		east.appendChild(c);
+		return this;
+	}
+	public Borderlayout appendCenter(Component c)
+	{
+		Center center = getCenter();
+		if (center == null)
+		{
+			center = new Center();
+			this.appendChild(center);
+		}
+		center.appendChild(c);
+		return this;
+	}
+}
