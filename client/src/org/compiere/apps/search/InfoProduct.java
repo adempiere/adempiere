893f97e6b7701caf30a5dbdf5ad11e5160db1da9
phib <devnull@localhost>
2010-07-16 04:54:39 +0000

    Product Info can display data from another client Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3030365

--- client/src/org/compiere/apps/search/InfoProduct.java
+++ client/src/org/compiere/apps/search/InfoProduct.java
@@ -408,22 +408,8 @@ public class InfoProduct extends Info implements ActionListener, ChangeListener
 			DB.close(rs, pstmt);
 			rs = null; pstmt = null;
 		}
-				
-		try {
-			sql = "SELECT M_Product_ID FROM M_Product WHERE Value = ?";
-			pstmt = DB.prepareStatement(sql, null);
-			pstmt.setString(1, (String)obj);
-			rs = pstmt.executeQuery();
-			if(rs.next())
-				m_M_Product_ID = rs.getInt(1);
-		} catch (Exception e) {
-			log.log(Level.WARNING, sql, e);
-		}
-		finally
-		{
-			DB.close(rs, pstmt);
-			rs = null; pstmt = null;
-		}
+		
+		m_M_Product_ID = getSelectedRowKey();
 		
 		sql = m_sqlSubstitute;
 		log.finest(sql);
