04358696fec0cf59fa5a3a36eea0ae014fb1ef20
usrdno <devnull@localhost>
2010-08-27 18:45:18 +0000

    Reverting back to previous version since bug fix has side effects. Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3030394

--- base/src/org/compiere/db/DB_PostgreSQL.java
+++ base/src/org/compiere/db/DB_PostgreSQL.java
@@ -37,7 +37,6 @@ import org.compiere.util.CLogger;
 import org.compiere.util.DB;
 import org.compiere.util.DisplayType;
 import org.compiere.util.Ini;
-import org.compiere.util.Language;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
@@ -399,24 +398,20 @@ public class DB_PostgreSQL implements AdempiereDatabase
 	 **/
 	public String TO_CHAR (String columnName, int displayType, String AD_Language)
 	{
-		StringBuffer retValue = new StringBuffer("TRIM(TO_CHAR(");
-        retValue.append(columnName);
-        
-        char d = '.';
-        char g = ',';
-        if ( !Language.isDecimalPoint(AD_Language))
-        {
-        	d = ',';
-        	g = '.';
-        }
-        
-		//  Numbers
+		StringBuffer retValue = new StringBuffer("CAST (");
+		retValue.append(columnName);
+		retValue.append(" AS Text)");
+
+		//  Numbers 
+		/*
 		if (DisplayType.isNumeric(displayType))
 		{
 			if (displayType == DisplayType.Amount)
-				retValue.append(",'999" + g + "999" + g + "999" + g + "990" + d + "00'");
+				retValue.append(" AS TEXT");
 			else
-				retValue.append(",'999" + g + "999" + g + "999" + g + "990" + d + "9999999999'");
+				retValue.append(" AS TEXT");			
+			//if (!Language.isDecimalPoint(AD_Language))      //  reversed
+			//retValue.append(",'NLS_NUMERIC_CHARACTERS='',.'''");
 		}
 		else if (DisplayType.isDate(displayType))
 		{
@@ -424,7 +419,8 @@ public class DB_PostgreSQL implements AdempiereDatabase
 				.append(Language.getLanguage(AD_Language).getDBdatePattern())
 				.append("'");
 		}
-		retValue.append("))");
+		retValue.append(")");
+		//*/
 		return retValue.toString();
 	}   //  TO_CHAR
 
