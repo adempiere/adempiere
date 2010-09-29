6fdac8a479a1e31bfb60d024d85ad58d26a44c38
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-09-29 21:53:08 +0000

    Fix the print format for Manufacturing Order.
    You need setting the default print format into the Print From window.
    I you want get a preview Manufacturing Order you can print from Manufacturing Order window after you setting the print form
    
    kind regards
    Victor Pérez
    www.e-evolution.com
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3016214

---
+++ migration/360lts-release/postgresql/750_BF3016214_PrintMO.sql
@@ -0,0 +1,5 @@
+-- Sep 29, 2010 4:27:48 PM CDT
+-- Fix Manufacturing Order Print
+UPDATE AD_Ref_Table SET WhereClause='AD_PrintFormat.AD_Table_ID=53197',Updated=TO_TIMESTAMP('2010-09-29 16:27:48','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Reference_ID=53281
+;
+
