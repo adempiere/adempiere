f0f149c4063e0d40b1eaff7b020237089be394c3
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-10-19 00:03:48 +0000

    New import I_HR_Movement class model
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=2878276

---
+++ base/src/org/eevolution/model/I_I_HR_Movement.java
@@ -0,0 +1,332 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.eevolution.model;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Interface for I_HR_Movement
+ *  @author Adempiere (generated) 
+ *  @version Release 3.6.0LTS
+ */
+public interface I_I_HR_Movement 
+{
+
+    /** TableName=I_HR_Movement */
+    public static final String Table_Name = "I_HR_Movement";
+
+    /** AD_Table_ID=53259 */
+    public static final int Table_ID = MTable.getTable_ID(Table_Name);
+
+    KeyNamePair Model = new KeyNamePair(Table_ID, Table_Name);
+
+    /** AccessLevel = 3 - Client - Org 
+     */
+    BigDecimal accessLevel = BigDecimal.valueOf(3);
+
+    /** Load Meta Data */
+
+    /** Column name AD_Client_ID */
+    public static final String COLUMNNAME_AD_Client_ID = "AD_Client_ID";
+
+	/** Get Client.
+	  * Client/Tenant for this installation.
+	  */
+	public int getAD_Client_ID();
+
+    /** Column name AD_Org_ID */
+    public static final String COLUMNNAME_AD_Org_ID = "AD_Org_ID";
+
+	/** Set Organization.
+	  * Organizational entity within client
+	  */
+	public void setAD_Org_ID (int AD_Org_ID);
+
+	/** Get Organization.
+	  * Organizational entity within client
+	  */
+	public int getAD_Org_ID();
+
+    /** Column name Amount */
+    public static final String COLUMNNAME_Amount = "Amount";
+
+	/** Set Amount.
+	  * Amount in a defined currency
+	  */
+	public void setAmount (BigDecimal Amount);
+
+	/** Get Amount.
+	  * Amount in a defined currency
+	  */
+	public BigDecimal getAmount();
+
+    /** Column name BPartner_Value */
+    public static final String COLUMNNAME_BPartner_Value = "BPartner_Value";
+
+	/** Set Business Partner Key.
+	  * The Key of the Business Partner
+	  */
+	public void setBPartner_Value (String BPartner_Value);
+
+	/** Get Business Partner Key.
+	  * The Key of the Business Partner
+	  */
+	public String getBPartner_Value();
+
+    /** Column name C_BPartner_ID */
+    public static final String COLUMNNAME_C_BPartner_ID = "C_BPartner_ID";
+
+	/** Set Business Partner .
+	  * Identifies a Business Partner
+	  */
+	public void setC_BPartner_ID (int C_BPartner_ID);
+
+	/** Get Business Partner .
+	  * Identifies a Business Partner
+	  */
+	public int getC_BPartner_ID();
+
+	public org.compiere.model.I_C_BPartner getC_BPartner() throws RuntimeException;
+
+    /** Column name ConceptValue */
+    public static final String COLUMNNAME_ConceptValue = "ConceptValue";
+
+	/** Set Concept Value.
+	  * Value of the Concept
+	  */
+	public void setConceptValue (String ConceptValue);
+
+	/** Get Concept Value.
+	  * Value of the Concept
+	  */
+	public String getConceptValue();
+
+    /** Column name Created */
+    public static final String COLUMNNAME_Created = "Created";
+
+	/** Get Created.
+	  * Date this record was created
+	  */
+	public Timestamp getCreated();
+
+    /** Column name CreatedBy */
+    public static final String COLUMNNAME_CreatedBy = "CreatedBy";
+
+	/** Get Created By.
+	  * User who created this records
+	  */
+	public int getCreatedBy();
+
+    /** Column name Description */
+    public static final String COLUMNNAME_Description = "Description";
+
+	/** Set Description.
+	  * Optional short description of the record
+	  */
+	public void setDescription (String Description);
+
+	/** Get Description.
+	  * Optional short description of the record
+	  */
+	public String getDescription();
+
+    /** Column name HR_Concept_ID */
+    public static final String COLUMNNAME_HR_Concept_ID = "HR_Concept_ID";
+
+	/** Set Payroll Concept	  */
+	public void setHR_Concept_ID (int HR_Concept_ID);
+
+	/** Get Payroll Concept	  */
+	public int getHR_Concept_ID();
+
+	public org.eevolution.model.I_HR_Concept getHR_Concept() throws RuntimeException;
+
+    /** Column name HR_Movement_ID */
+    public static final String COLUMNNAME_HR_Movement_ID = "HR_Movement_ID";
+
+	/** Set Payroll Movement	  */
+	public void setHR_Movement_ID (int HR_Movement_ID);
+
+	/** Get Payroll Movement	  */
+	public int getHR_Movement_ID();
+
+	public org.eevolution.model.I_HR_Movement getHR_Movement() throws RuntimeException;
+
+    /** Column name HR_Process_ID */
+    public static final String COLUMNNAME_HR_Process_ID = "HR_Process_ID";
+
+	/** Set Payroll Process	  */
+	public void setHR_Process_ID (int HR_Process_ID);
+
+	/** Get Payroll Process	  */
+	public int getHR_Process_ID();
+
+	public org.eevolution.model.I_HR_Process getHR_Process() throws RuntimeException;
+
+    /** Column name I_ErrorMsg */
+    public static final String COLUMNNAME_I_ErrorMsg = "I_ErrorMsg";
+
+	/** Set Import Error Message.
+	  * Messages generated from import process
+	  */
+	public void setI_ErrorMsg (String I_ErrorMsg);
+
+	/** Get Import Error Message.
+	  * Messages generated from import process
+	  */
+	public String getI_ErrorMsg();
+
+    /** Column name I_HR_Movement_ID */
+    public static final String COLUMNNAME_I_HR_Movement_ID = "I_HR_Movement_ID";
+
+	/** Set Payroll Movement Import	  */
+	public void setI_HR_Movement_ID (int I_HR_Movement_ID);
+
+	/** Get Payroll Movement Import	  */
+	public int getI_HR_Movement_ID();
+
+    /** Column name I_IsImported */
+    public static final String COLUMNNAME_I_IsImported = "I_IsImported";
+
+	/** Set Imported.
+	  * Has this import been processed
+	  */
+	public void setI_IsImported (String I_IsImported);
+
+	/** Get Imported.
+	  * Has this import been processed
+	  */
+	public String getI_IsImported();
+
+    /** Column name IsActive */
+    public static final String COLUMNNAME_IsActive = "IsActive";
+
+	/** Set Active.
+	  * The record is active in the system
+	  */
+	public void setIsActive (boolean IsActive);
+
+	/** Get Active.
+	  * The record is active in the system
+	  */
+	public boolean isActive();
+
+    /** Column name Processed */
+    public static final String COLUMNNAME_Processed = "Processed";
+
+	/** Set Processed.
+	  * The document has been processed
+	  */
+	public void setProcessed (boolean Processed);
+
+	/** Get Processed.
+	  * The document has been processed
+	  */
+	public boolean isProcessed();
+
+    /** Column name Processing */
+    public static final String COLUMNNAME_Processing = "Processing";
+
+	/** Set Process Now	  */
+	public void setProcessing (boolean Processing);
+
+	/** Get Process Now	  */
+	public boolean isProcessing();
+
+    /** Column name ProcessName */
+    public static final String COLUMNNAME_ProcessName = "ProcessName";
+
+	/** Set Process Name.
+	  * Name of the Process
+	  */
+	public void setProcessName (String ProcessName);
+
+	/** Get Process Name.
+	  * Name of the Process
+	  */
+	public String getProcessName();
+
+    /** Column name Qty */
+    public static final String COLUMNNAME_Qty = "Qty";
+
+	/** Set Quantity.
+	  * Quantity
+	  */
+	public void setQty (BigDecimal Qty);
+
+	/** Get Quantity.
+	  * Quantity
+	  */
+	public BigDecimal getQty();
+
+    /** Column name ServiceDate */
+    public static final String COLUMNNAME_ServiceDate = "ServiceDate";
+
+	/** Set Service date.
+	  * Date service was provided
+	  */
+	public void setServiceDate (Timestamp ServiceDate);
+
+	/** Get Service date.
+	  * Date service was provided
+	  */
+	public Timestamp getServiceDate();
+
+    /** Column name TextMsg */
+    public static final String COLUMNNAME_TextMsg = "TextMsg";
+
+	/** Set Text Message.
+	  * Text Message
+	  */
+	public void setTextMsg (String TextMsg);
+
+	/** Get Text Message.
+	  * Text Message
+	  */
+	public String getTextMsg();
+
+    /** Column name Updated */
+    public static final String COLUMNNAME_Updated = "Updated";
+
+	/** Get Updated.
+	  * Date this record was updated
+	  */
+	public Timestamp getUpdated();
+
+    /** Column name UpdatedBy */
+    public static final String COLUMNNAME_UpdatedBy = "UpdatedBy";
+
+	/** Get Updated By.
+	  * User who updated this records
+	  */
+	public int getUpdatedBy();
+
+    /** Column name ValidFrom */
+    public static final String COLUMNNAME_ValidFrom = "ValidFrom";
+
+	/** Set Valid from.
+	  * Valid from including this date (first day)
+	  */
+	public void setValidFrom (Timestamp ValidFrom);
+
+	/** Get Valid from.
+	  * Valid from including this date (first day)
+	  */
+	public Timestamp getValidFrom();
+}
---
+++ base/src/org/eevolution/model/X_I_HR_Movement.java
@@ -0,0 +1,447 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.eevolution.model;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Properties;
+import org.compiere.model.*;
+import org.compiere.util.Env;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Model for I_HR_Movement
+ *  @author Adempiere (generated) 
+ *  @version Release 3.6.0LTS - $Id$ */
+public class X_I_HR_Movement extends PO implements I_I_HR_Movement, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20101013L;
+
+    /** Standard Constructor */
+    public X_I_HR_Movement (Properties ctx, int I_HR_Movement_ID, String trxName)
+    {
+      super (ctx, I_HR_Movement_ID, trxName);
+      /** if (I_HR_Movement_ID == 0)
+        {
+			setI_HR_Movement_ID (0);
+			setI_IsImported (null);
+// N
+        } */
+    }
+
+    /** Load Constructor */
+    public X_I_HR_Movement (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 3 - Client - Org 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_I_HR_Movement[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Amount.
+		@param Amount 
+		Amount in a defined currency
+	  */
+	public void setAmount (BigDecimal Amount)
+	{
+		set_Value (COLUMNNAME_Amount, Amount);
+	}
+
+	/** Get Amount.
+		@return Amount in a defined currency
+	  */
+	public BigDecimal getAmount () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_Amount);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
+	/** Set Business Partner Key.
+		@param BPartner_Value 
+		The Key of the Business Partner
+	  */
+	public void setBPartner_Value (String BPartner_Value)
+	{
+		set_Value (COLUMNNAME_BPartner_Value, BPartner_Value);
+	}
+
+	/** Get Business Partner Key.
+		@return The Key of the Business Partner
+	  */
+	public String getBPartner_Value () 
+	{
+		return (String)get_Value(COLUMNNAME_BPartner_Value);
+	}
+
+	public org.compiere.model.I_C_BPartner getC_BPartner() throws RuntimeException
+    {
+		return (org.compiere.model.I_C_BPartner)MTable.get(getCtx(), org.compiere.model.I_C_BPartner.Table_Name)
+			.getPO(getC_BPartner_ID(), get_TrxName());	}
+
+	/** Set Business Partner .
+		@param C_BPartner_ID 
+		Identifies a Business Partner
+	  */
+	public void setC_BPartner_ID (int C_BPartner_ID)
+	{
+		if (C_BPartner_ID < 1) 
+			set_Value (COLUMNNAME_C_BPartner_ID, null);
+		else 
+			set_Value (COLUMNNAME_C_BPartner_ID, Integer.valueOf(C_BPartner_ID));
+	}
+
+	/** Get Business Partner .
+		@return Identifies a Business Partner
+	  */
+	public int getC_BPartner_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_C_BPartner_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Concept Value.
+		@param ConceptValue 
+		Value of the Concept
+	  */
+	public void setConceptValue (String ConceptValue)
+	{
+		set_Value (COLUMNNAME_ConceptValue, ConceptValue);
+	}
+
+	/** Get Concept Value.
+		@return Value of the Concept
+	  */
+	public String getConceptValue () 
+	{
+		return (String)get_Value(COLUMNNAME_ConceptValue);
+	}
+
+	/** Set Description.
+		@param Description 
+		Optional short description of the record
+	  */
+	public void setDescription (String Description)
+	{
+		set_Value (COLUMNNAME_Description, Description);
+	}
+
+	/** Get Description.
+		@return Optional short description of the record
+	  */
+	public String getDescription () 
+	{
+		return (String)get_Value(COLUMNNAME_Description);
+	}
+
+	public org.eevolution.model.I_HR_Concept getHR_Concept() throws RuntimeException
+    {
+		return (org.eevolution.model.I_HR_Concept)MTable.get(getCtx(), org.eevolution.model.I_HR_Concept.Table_Name)
+			.getPO(getHR_Concept_ID(), get_TrxName());	}
+
+	/** Set Payroll Concept.
+		@param HR_Concept_ID Payroll Concept	  */
+	public void setHR_Concept_ID (int HR_Concept_ID)
+	{
+		if (HR_Concept_ID < 1) 
+			set_Value (COLUMNNAME_HR_Concept_ID, null);
+		else 
+			set_Value (COLUMNNAME_HR_Concept_ID, Integer.valueOf(HR_Concept_ID));
+	}
+
+	/** Get Payroll Concept.
+		@return Payroll Concept	  */
+	public int getHR_Concept_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_HR_Concept_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public org.eevolution.model.I_HR_Movement getHR_Movement() throws RuntimeException
+    {
+		return (org.eevolution.model.I_HR_Movement)MTable.get(getCtx(), org.eevolution.model.I_HR_Movement.Table_Name)
+			.getPO(getHR_Movement_ID(), get_TrxName());	}
+
+	/** Set Payroll Movement.
+		@param HR_Movement_ID Payroll Movement	  */
+	public void setHR_Movement_ID (int HR_Movement_ID)
+	{
+		if (HR_Movement_ID < 1) 
+			set_Value (COLUMNNAME_HR_Movement_ID, null);
+		else 
+			set_Value (COLUMNNAME_HR_Movement_ID, Integer.valueOf(HR_Movement_ID));
+	}
+
+	/** Get Payroll Movement.
+		@return Payroll Movement	  */
+	public int getHR_Movement_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_HR_Movement_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public org.eevolution.model.I_HR_Process getHR_Process() throws RuntimeException
+    {
+		return (org.eevolution.model.I_HR_Process)MTable.get(getCtx(), org.eevolution.model.I_HR_Process.Table_Name)
+			.getPO(getHR_Process_ID(), get_TrxName());	}
+
+	/** Set Payroll Process.
+		@param HR_Process_ID Payroll Process	  */
+	public void setHR_Process_ID (int HR_Process_ID)
+	{
+		if (HR_Process_ID < 1) 
+			set_Value (COLUMNNAME_HR_Process_ID, null);
+		else 
+			set_Value (COLUMNNAME_HR_Process_ID, Integer.valueOf(HR_Process_ID));
+	}
+
+	/** Get Payroll Process.
+		@return Payroll Process	  */
+	public int getHR_Process_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_HR_Process_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+    /** Get Record ID/ColumnName
+        @return ID/ColumnName pair
+      */
+    public KeyNamePair getKeyNamePair() 
+    {
+        return new KeyNamePair(get_ID(), String.valueOf(getHR_Process_ID()));
+    }
+
+	/** Set Import Error Message.
+		@param I_ErrorMsg 
+		Messages generated from import process
+	  */
+	public void setI_ErrorMsg (String I_ErrorMsg)
+	{
+		set_Value (COLUMNNAME_I_ErrorMsg, I_ErrorMsg);
+	}
+
+	/** Get Import Error Message.
+		@return Messages generated from import process
+	  */
+	public String getI_ErrorMsg () 
+	{
+		return (String)get_Value(COLUMNNAME_I_ErrorMsg);
+	}
+
+	/** Set Payroll Movement Import.
+		@param I_HR_Movement_ID Payroll Movement Import	  */
+	public void setI_HR_Movement_ID (int I_HR_Movement_ID)
+	{
+		if (I_HR_Movement_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_I_HR_Movement_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_I_HR_Movement_ID, Integer.valueOf(I_HR_Movement_ID));
+	}
+
+	/** Get Payroll Movement Import.
+		@return Payroll Movement Import	  */
+	public int getI_HR_Movement_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_I_HR_Movement_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Imported.
+		@param I_IsImported 
+		Has this import been processed
+	  */
+	public void setI_IsImported (String I_IsImported)
+	{
+		set_Value (COLUMNNAME_I_IsImported, I_IsImported);
+	}
+
+	/** Get Imported.
+		@return Has this import been processed
+	  */
+	public String getI_IsImported () 
+	{
+		return (String)get_Value(COLUMNNAME_I_IsImported);
+	}
+
+	/** Set Processed.
+		@param Processed 
+		The document has been processed
+	  */
+	public void setProcessed (boolean Processed)
+	{
+		set_Value (COLUMNNAME_Processed, Boolean.valueOf(Processed));
+	}
+
+	/** Get Processed.
+		@return The document has been processed
+	  */
+	public boolean isProcessed () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processed);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Process Now.
+		@param Processing Process Now	  */
+	public void setProcessing (boolean Processing)
+	{
+		set_Value (COLUMNNAME_Processing, Boolean.valueOf(Processing));
+	}
+
+	/** Get Process Now.
+		@return Process Now	  */
+	public boolean isProcessing () 
+	{
+		Object oo = get_Value(COLUMNNAME_Processing);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Process Name.
+		@param ProcessName 
+		Name of the Process
+	  */
+	public void setProcessName (String ProcessName)
+	{
+		set_Value (COLUMNNAME_ProcessName, ProcessName);
+	}
+
+	/** Get Process Name.
+		@return Name of the Process
+	  */
+	public String getProcessName () 
+	{
+		return (String)get_Value(COLUMNNAME_ProcessName);
+	}
+
+	/** Set Quantity.
+		@param Qty 
+		Quantity
+	  */
+	public void setQty (BigDecimal Qty)
+	{
+		set_Value (COLUMNNAME_Qty, Qty);
+	}
+
+	/** Get Quantity.
+		@return Quantity
+	  */
+	public BigDecimal getQty () 
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_Qty);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
+	/** Set Service date.
+		@param ServiceDate 
+		Date service was provided
+	  */
+	public void setServiceDate (Timestamp ServiceDate)
+	{
+		set_Value (COLUMNNAME_ServiceDate, ServiceDate);
+	}
+
+	/** Get Service date.
+		@return Date service was provided
+	  */
+	public Timestamp getServiceDate () 
+	{
+		return (Timestamp)get_Value(COLUMNNAME_ServiceDate);
+	}
+
+	/** Set Text Message.
+		@param TextMsg 
+		Text Message
+	  */
+	public void setTextMsg (String TextMsg)
+	{
+		set_Value (COLUMNNAME_TextMsg, TextMsg);
+	}
+
+	/** Get Text Message.
+		@return Text Message
+	  */
+	public String getTextMsg () 
+	{
+		return (String)get_Value(COLUMNNAME_TextMsg);
+	}
+
+	/** Set Valid from.
+		@param ValidFrom 
+		Valid from including this date (first day)
+	  */
+	public void setValidFrom (Timestamp ValidFrom)
+	{
+		set_Value (COLUMNNAME_ValidFrom, ValidFrom);
+	}
+
+	/** Get Valid from.
+		@return Valid from including this date (first day)
+	  */
+	public Timestamp getValidFrom () 
+	{
+		return (Timestamp)get_Value(COLUMNNAME_ValidFrom);
+	}
+}
