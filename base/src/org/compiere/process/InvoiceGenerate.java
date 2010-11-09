c3eee1d682678c6526a1dddb55240dadcc340ac8
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-11-09 07:58:31 +0000

    error process, is consolidating by order bill locatio
    integration of Revision: 14434 http://adempiere.svn.sourceforge.net/adempiere/?rev=14434&view=rev
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3105800

--- base/src/org/compiere/process/InvoiceGenerate.java
+++ base/src/org/compiere/process/InvoiceGenerate.java
@@ -131,7 +131,7 @@ public class InvoiceGenerate extends SvrProcess
 				+ "WHERE C_Order.DocStatus='CO' AND C_Order.IsSOTrx='Y' "
 				+ "AND C_Order.C_Order_ID = T_Selection.T_Selection_ID "
 				+ "AND T_Selection.AD_PInstance_ID=? "
-				+ "ORDER BY C_Order.M_Warehouse_ID, C_Order.PriorityRule, C_Order.C_BPartner_ID, C_Order.C_Order_ID";
+				+ "ORDER BY C_Order.M_Warehouse_ID, C_Order.PriorityRule, C_Order.C_BPartner_ID, C_Order.Bill_Location_ID, C_Order.C_Order_ID";
 		}
 		else
 		{
