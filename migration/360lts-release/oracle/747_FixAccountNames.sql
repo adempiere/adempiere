5d76a16b7ba399f1b3bd6762ab538cf224e6e8f5
globalqss <devnull@localhost>
2010-11-15 23:16:57 -0600

    Integrate Revision: 14479 http://adempiere.svn.sourceforge.net/adempiere/?rev=14479&view=rev Author:   trifonnt Date:     2010-11-14 21:17:08 +0000 (Sun, 14 Nov 2010) Typo corrections in USA CoA.
    Added some improvements (fix on the spreadsheet and migration scripts to fix the current GW data)
    (transplanted from 7b1c7c13008b9865b26367cf6c456f5107695d4f)

---
+++ migration/360lts-release/oracle/747_FixAccountNames.sql
@@ -0,0 +1,105 @@
+-- Nov 15, 2010 10:51:28 PM CST
+-- Fix Typos in Account Names
+UPDATE C_ElementValue SET AD_Org_ID=0, Description='Tax to be reimbursed - before tax declaration',Updated=TO_DATE('2010-11-15 22:51:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=534
+;
+
+-- Nov 15, 2010 10:51:28 PM CST
+UPDATE C_ElementValue_Trl SET Description='Tax to be reimbursed - before tax declaration',Name='Tax credit A/R',IsTranslated='Y' WHERE C_ElementValue_ID=534
+;
+
+-- Nov 15, 2010 10:51:41 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Accumulated Amortization',Updated=TO_DATE('2010-11-15 22:51:41','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=582
+;
+
+-- Nov 15, 2010 10:51:41 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Accumulated Amortization',IsTranslated='Y' WHERE C_ElementValue_ID=582
+;
+
+-- Nov 15, 2010 10:52:08 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Federal Unemployment Employers Contribution',Updated=TO_DATE('2010-11-15 22:52:08','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=613
+;
+
+-- Nov 15, 2010 10:52:08 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Federal Unemployment Employers Contribution',IsTranslated='Y' WHERE C_ElementValue_ID=613
+;
+
+-- Nov 15, 2010 10:52:21 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Dividends',Updated=TO_DATE('2010-11-15 22:52:21','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=757
+;
+
+-- Nov 15, 2010 10:52:21 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Dividends',IsTranslated='Y' WHERE C_ElementValue_ID=757
+;
+
+-- Nov 15, 2010 10:52:38 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Retirement (Pension, Profit Sharing, 401k)',Updated=TO_DATE('2010-11-15 22:52:38','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=455
+;
+
+-- Nov 15, 2010 10:52:38 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Retirement (Pension, Profit Sharing, 401k)',IsTranslated='Y' WHERE C_ElementValue_ID=455
+;
+
+-- Nov 15, 2010 10:53:02 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Data Processing Other Expense',Updated=TO_DATE('2010-11-15 22:53:02','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=645
+;
+
+-- Nov 15, 2010 10:53:02 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Data Processing Other Expense',IsTranslated='Y' WHERE C_ElementValue_ID=645
+;
+
+-- Nov 15, 2010 10:54:20 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Building Repairs & Maintenance',Updated=TO_DATE('2010-11-15 22:54:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=683
+;
+
+-- Nov 15, 2010 10:54:20 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Building Repairs & Maintenance',IsTranslated='Y' WHERE C_ElementValue_ID=683
+;
+
+-- Nov 15, 2010 10:54:26 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Furniture, Equipment Repairs & Maintenance',Updated=TO_DATE('2010-11-15 22:54:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=684
+;
+
+-- Nov 15, 2010 10:54:26 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Furniture, Equipment Repairs & Maintenance',IsTranslated='Y' WHERE C_ElementValue_ID=684
+;
+
+-- Nov 15, 2010 10:54:30 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Data Processing Repairs & Maintenance',Updated=TO_DATE('2010-11-15 22:54:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=764
+;
+
+-- Nov 15, 2010 10:54:30 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Data Processing Repairs & Maintenance',IsTranslated='Y' WHERE C_ElementValue_ID=764
+;
+
+-- Nov 15, 2010 10:54:45 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Software Maintenance',Updated=TO_DATE('2010-11-15 22:54:45','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=685
+;
+
+-- Nov 15, 2010 10:54:45 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Software Maintenance',IsTranslated='Y' WHERE C_ElementValue_ID=685
+;
+
+-- Nov 15, 2010 10:54:49 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Vehicle Repairs & Maintenance',Updated=TO_DATE('2010-11-15 22:54:49','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=686
+;
+
+-- Nov 15, 2010 10:54:49 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Vehicle Repairs & Maintenance',IsTranslated='Y' WHERE C_ElementValue_ID=686
+;
+
+-- Nov 15, 2010 10:55:11 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Dividend Income',Updated=TO_DATE('2010-11-15 22:55:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=706
+;
+
+-- Nov 15, 2010 10:55:11 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Dividend Income',IsTranslated='Y' WHERE C_ElementValue_ID=706
+;
+
+-- Nov 15, 2010 10:55:27 PM CST
+UPDATE C_ElementValue SET AD_Org_ID=0, Name='Mortgage Interest Expense',Updated=TO_DATE('2010-11-15 22:55:27','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=101 WHERE C_ElementValue_ID=718
+;
+
+-- Nov 15, 2010 10:55:27 PM CST
+UPDATE C_ElementValue_Trl SET Description=NULL,Name='Mortgage Interest Expense',IsTranslated='Y' WHERE C_ElementValue_ID=718
+;
+
