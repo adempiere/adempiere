f51956e9b9ad4721527c481918205919975f8566
trifonnt <devnull@localhost>
2010-11-18 15:46:59 +0000

    Added System Configuration Variable: ZK_ROOT_FOLDER_BROWSER Related to SVN Commit:  - Revision: 14459 http://adempiere.svn.sourceforge.net/adempiere/?rev=14459&view=rev

---
+++ migration/360lts-release/oracle/767_ZK_RootFolder_oracle.sql
@@ -0,0 +1,3 @@
+-- Nov 18, 2010 4:42:20 PM CET
+INSERT INTO AD_SysConfig (AD_Client_ID,AD_Org_ID,AD_SysConfig_ID,ConfigurationLevel,Created,CreatedBy,Description,EntityType,IsActive,Name,Updated,UpdatedBy,Value) VALUES (0,0,50050,'S',TO_DATE('2010-11-18 16:42:19','YYYY-MM-DD HH24:MI:SS'),100,'Root Folder to be used when opening server files.','D','Y','ZK_ROOT_FOLDER_BROWSER',TO_DATE('2010-11-18 16:42:19','YYYY-MM-DD HH24:MI:SS'),100,'/opt/adempiere/current/data')
+;
