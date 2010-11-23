5474dfb10a155f609da4be930f28da297277400a
globalqss <devnull@localhost>
2010-11-22 23:12:36 -0600

    Fix 3116131 - Packout details can not be saved - empty Package Build ID http://sourceforge.net/support/tracker.php?aid=3116131 (transplanted from 27169650fb2d546df2182d11e2a828108c10cdc9)

---
+++ migration/360lts-release/oracle/749_BF3116131.sql
@@ -0,0 +1,13 @@
+-- Nov 22, 2010 11:04:35 PM CST
+-- BF3116131-Packout details can not be saved - empty Package Build ID
+UPDATE AD_Column SET AD_Reference_ID=19, IsUpdateable='N',Updated=TO_DATE('2010-11-22 23:04:35','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=50132
+;
+
+-- Nov 22, 2010 11:06:28 PM CST
+UPDATE AD_Column SET IsIdentifier='N', IsUpdateable='N', SeqNo=0,Updated=TO_DATE('2010-11-22 23:06:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=50080
+;
+
+-- Nov 22, 2010 11:06:30 PM CST
+UPDATE AD_Column SET SeqNo=1,Updated=TO_DATE('2010-11-22 23:06:30','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=50086
+;
+
