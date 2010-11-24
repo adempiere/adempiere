96fccbe494272b3c2b62248799795c85a46603e4
trifonnt <devnull@localhost>
2010-11-24 11:02:34 +0000

    Based on commit by Carlos: http://adempiere.hg.sourceforge.net/hgweb/adempiere/adempiere361/rev/f7098adfc433 Fix problem caused by revision 14423: http://adempiere.svn.sourceforge.net/viewvc/adempiere?view=revision&revision=14423 Problem reported here: https://sourceforge.net/projects/adempiere/forums/forum/610548/topic/3958171 * Fix zkwebui problem with mandatory read-only fields. * Now it behaves like swing * And lists are not filled for read-only fields (better performance)

--- zkwebui/WEB-INF/src/org/adempiere/webui/editor/WTableDirEditor.java
+++ zkwebui/WEB-INF/src/org/adempiere/webui/editor/WTableDirEditor.java
@@ -188,7 +188,7 @@ ContextMenuListener, IZoomableEditor
             getComponent().setValue(value);            
             if (!getComponent().isSelected(value))
             {
-            	if (lookup != null)
+            	if (isReadWrite() && lookup != null)
             		lookup.refresh();
             	Object curValue = oldValue;
                 oldValue = value;
@@ -197,7 +197,7 @@ ContextMenuListener, IZoomableEditor
                 //still not in list, reset to zero
                 if (!getComponent().isSelected(value))
                 {
-                	if (value instanceof Integer)
+                	if (value instanceof Integer && gridField.getDisplayType() != DisplayType.ID) // for IDs is ok to be out of the list
                 	{
                 		getComponent().setValue(null);
                 		if (curValue == null)
