4757831b798e4430aa94c1eaf69c015312eba625
globalqss <devnull@localhost>
2010-12-05 09:47:47 -0500

    Typo in demo data - replace Whater with Water (transplanted from 0fd6e1d2448249ca7a6a24e3af86c2e799f56b01)

---
+++ migration/360lts-release/postgresql/753_FixTypoInDemoData.sql
@@ -0,0 +1,9 @@
+-- Dec 5, 2010 9:43:01 AM COT
+-- Typo in demo data
+UPDATE M_Product SET Name='Water', Value='Water',Updated=TO_TIMESTAMP('2010-12-05 09:43:01','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE M_Product_ID=50017
+;
+
+-- Dec 5, 2010 9:43:32 AM COT
+UPDATE AD_WF_Node SET Help='Mixed the 17% of Nitrogen, 17% of Phosphorus , 17% of Potassium and Water',Updated=TO_TIMESTAMP('2010-12-05 09:43:32','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_WF_Node_ID=50090
+;
+
