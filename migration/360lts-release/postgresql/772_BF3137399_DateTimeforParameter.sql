8a57eed62a2eb968dd53c65c451dbfd3d275a83e
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2010-12-15 17:15:20 +0000

    Currently when you send a date time parameter it is truncate
    kind regards
    Victor Perez
    www.e-evolution.com
    Link to SF Tracker: http://sourceforge.net/support/tracker.php?aid=3137399

---
+++ migration/360lts-release/postgresql/772_BF3137399_DateTimeforParameter.sql
@@ -0,0 +1,10 @@
+-- Dec 15, 2010 11:02:18 AM CST
+-- Fix Application Dictionary
+UPDATE AD_Column SET AD_Reference_ID=16,Updated=TO_TIMESTAMP('2010-12-15 11:02:18','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2797
+;
+
+-- Dec 15, 2010 11:02:24 AM CST
+-- Fix Application Dictionary
+UPDATE AD_Column SET AD_Reference_ID=16,Updated=TO_TIMESTAMP('2010-12-15 11:02:24','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Column_ID=2798
+;
+
