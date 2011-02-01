af38d3f81e2390abcbc6ebca416d59afaff68f61
globalqss <devnull@localhost>
2011-02-01 12:52:17 -0500

    Fix [2946164] - Report headers are not translated in ZK Client http://sourceforge.net/support/tracker.php?aid=2946164 (transplanted from 728d671086cb9c61e20249cc93d70ff8eb63ed3f)

--- base/src/org/compiere/util/Language.java
+++ base/src/org/compiere/util/Language.java
@@ -406,7 +406,8 @@ public class Language implements Serializable
 	 */
 	public static Language getLoginLanguage ()
 	{
-		return s_loginLanguage;
+		// return s_loginLanguage; // See bug [2946164]
+		return Env.getLanguage(Env.getCtx());
 	}   //  getLanguage
 
 	/**
