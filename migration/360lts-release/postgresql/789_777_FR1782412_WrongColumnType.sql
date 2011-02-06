86fdc6f8b5bdc055a15763c0004d585278cc3ace
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2011-02-05 19:58:53 -0600

    fix script sequence move from 777 to 789

---
+++ migration/360lts-release/postgresql/789_777_FR1782412_WrongColumnType.sql
@@ -0,0 +1,9 @@
+-- 15-ene-2011 11:04:15 COT
+-- FR 1782412 - Add Document Action Access Functionality - wrong column type
+INSERT INTO t_alter_column values('ad_document_action_access','Updated','TIMESTAMP',null,null)
+;
+
+-- 15-ene-2011 11:04:36 COT
+INSERT INTO t_alter_column values('ad_document_action_access','Created','TIMESTAMP',null,null)
+;
+
