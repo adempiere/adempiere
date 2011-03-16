84ff37c8ef81c2e67d10b93931ac92d198103a75
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2011-03-15 22:31:38 -0600

    The Cost Collector window should not allow create new record https://sourceforge.net/tracker/?func=detail&aid=3215137&group_id=176962&atid=934929

---
+++ migration/360lts-release/oracle/793_BF3215137_FixCostCollector.sql
@@ -0,0 +1,10 @@
+-- Mar 15, 2011 10:25:07 PM CST
+-- Fix Collector Cost Window
+UPDATE AD_Window SET IsSOTrx='N',Updated=TO_DATE('2011-03-15 22:24:46','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Window_ID=53014
+;
+
+-- Mar 15, 2011 10:25:23 PM CST
+-- Fix Collector Cost Window
+UPDATE AD_Tab SET IsInsertRecord='N',Updated=TO_DATE('2011-03-15 22:25:20','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=100 WHERE AD_Tab_ID=53053
+;
+
