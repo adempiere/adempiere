1d3aae6d5740d9c04f65bbbca99cca1136e65388
Paul Bowden <devnull@localhost>
2011-07-26 15:33:25 +1000

    Read migrations from ADEMPIERE_HOME/migration

--- base/src/org/adempiere/process/MigrationLoader.java
+++ base/src/org/adempiere/process/MigrationLoader.java
@@ -1,21 +1,29 @@
 package org.adempiere.process;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Level;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.compiere.Adempiere;
 import org.compiere.model.MMigration;
+import org.compiere.util.CLogger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class MigrationLoader {
+	
+
+	/**	Logger	*/
+	private CLogger	log	= CLogger.getCLogger (MigrationLoader.class);
 	
 	public void load(Properties ctx)
 	{
@@ -26,21 +34,44 @@ public class MigrationLoader {
 		DocumentBuilder builder;
 		try {
 			builder = dbf.newDocumentBuilder();
-		Class klass = getClass();
-		InputSource is1 = new InputSource(klass.getResourceAsStream("/org/adempiere/migration/Migration.xml"));
-		Document doc = builder.parse(is1);
-
-		NodeList migrations = doc.getDocumentElement().getElementsByTagName("Migration");
-		for ( int i = 0; i < migrations.getLength(); i++ ) {
-			int migrationId = MMigration.fromXmlNode(ctx, (Element) migrations.item(i), null);  //TODO trx
-			if ( migrationId > 0 )
+			
+			
+			File home = new File (Adempiere.getAdempiereHome() + File.separator + "migration");
+			if ( !home.exists() && !home.isDirectory() )
 			{
-				MMigration migration = new MMigration(ctx, migrationId, null);
-				migration.setFailOnError(true);
-				migration.apply();
+				log.log(Level.WARNING, "No migration directory");
+				return;
 			}
-		
-		}
+			else
+			{
+				log.log(Level.FINE, "Processing migration files in directory: " + home.getAbsolutePath() );
+			}
+			
+			File[] migrationFiles = home.listFiles(new FilenameFilter() {
+				
+				@Override
+				public boolean accept(File dir, String name) {
+					
+					return name.endsWith(".xml");
+				}
+			});
+			
+			for (File file : migrationFiles )
+			{
+				Document doc = builder.parse(file);
+
+				NodeList migrations = doc.getDocumentElement().getElementsByTagName("Migration");
+				for ( int i = 0; i < migrations.getLength(); i++ ) {
+					int migrationId = MMigration.fromXmlNode(ctx, (Element) migrations.item(i), null);  //TODO trx
+					if ( migrationId > 0 )
+					{
+						MMigration migration = new MMigration(ctx, migrationId, null);
+						migration.setFailOnError(true);
+						migration.apply();
+					}
+
+				}
+			}
 
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
