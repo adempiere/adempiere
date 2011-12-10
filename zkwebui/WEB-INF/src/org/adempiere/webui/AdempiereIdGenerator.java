e2ea92f45755d62afbd902c5360f115fb69decdc
teo_sarca <devnull@localhost>
2011-12-10 22:47:00 +0200

    FR3456545 - Pluggable IdGenerator

--- zkwebui/WEB-INF/src/org/adempiere/webui/AdempiereIdGenerator.java
+++ zkwebui/WEB-INF/src/org/adempiere/webui/AdempiereIdGenerator.java
@@ -1,6 +1,6 @@
 /******************************************************************************
  * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) Potix Corporation All Rights Reserved.                       *
+ * Copyright (C) metas All Rights Reserved.                                   *
  * This program is free software; you can redistribute it and/or modify it    *
  * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
@@ -19,6 +19,9 @@
 
 package org.adempiere.webui;
 
+import org.adempiere.exceptions.AdempiereException;
+import org.compiere.model.MSysConfig;
+import org.compiere.util.Env;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Desktop;
 import org.zkoss.zk.ui.Page;
@@ -26,29 +29,56 @@ import org.zkoss.zk.ui.sys.IdGenerator;
 
 /**
  * 
- * @author: Ryan Wu, Engineer, Potix Corporation
- * @author: Carlos Ruiz
- * @author: Victor Perez
- * 
- * @see http://docs.zkoss.org/wiki/How_to_Test_ZK_Application_with_Selenium
+ * @author: Teo Sarca
  */
-public class AdempiereIdGenerator implements IdGenerator {
-	public String nextComponentUuid(Desktop desktop, Component comp) {
-        int i = Integer.parseInt(desktop.getAttribute("Id_Num").toString());
-        i++;// Start from 1
-        desktop.setAttribute("Id_Num", String.valueOf(i));
-        return "zk_comp_" + i;
-    }
- 
-    public String nextDesktopId(Desktop desktop) {
-        if (desktop.getAttribute("Id_Num") == null) {
-            String number = "0";
-            desktop.setAttribute("Id_Num", number);
-        }
-        return null;
-    }
- 
-    public String nextPageUuid(Page page) {
-        return null;
-    }
+public class AdempiereIdGenerator implements IdGenerator
+{
+	public static final String SYSCONFIG_IdGenerator = "org.adempiere.webui.IdGenerator";
+	public static final String SYSCONFIG_IdGenerator_Default = org.adempiere.webui.SahiIdGenerator.class.getCanonicalName();
+
+	private static IdGenerator idGenerator = null;
+
+	public static void setIdGenerator(IdGenerator generator)
+	{
+		idGenerator = generator;
+	}
+
+	public static IdGenerator getIdGenerator()
+	{
+		if (idGenerator == null)
+		{
+			String classname = MSysConfig.getValue(SYSCONFIG_IdGenerator, SYSCONFIG_IdGenerator_Default);
+			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			if (cl == null)
+				cl = AdempiereIdGenerator.class.getClassLoader();
+
+			try
+			{
+				idGenerator = (IdGenerator)cl.loadClass(classname).newInstance();
+			}
+			catch (Exception e)
+			{
+				throw new AdempiereException(e);
+			}
+		}
+		return idGenerator;
+	}
+
+	@Override
+	public String nextComponentUuid(Desktop desktop, Component comp)
+	{
+		return getIdGenerator().nextComponentUuid(desktop, comp);
+	}
+
+	@Override
+	public String nextDesktopId(Desktop desktop)
+	{
+		return getIdGenerator().nextDesktopId(desktop);
+	}
+
+	@Override
+	public String nextPageUuid(Page page)
+	{
+		return getIdGenerator().nextPageUuid(page);
+	}
 }
