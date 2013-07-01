d1fca4937c068b9a9c6dfe1e22b309bfe818796b
al <devnull@localhost>
2013-07-01 17:37:50 +0300

    added interface (at this point it only contains tagging related to dunning writeOff)

---
+++ base/src/org/adempiere/invoice/service/IInvoiceBL.java
@@ -0,0 +1,17 @@
+package org.adempiere.invoice.service;
+
+import java.math.BigDecimal;
+
+import org.adempiere.util.ISingletonService;
+
+public interface IInvoiceBL extends ISingletonService
+{
+	/**
+	 * Writes off the given openAmt from the given invoice.
+	 * 
+	 * @param invoice
+	 * @param openAmt open amount (not absolute, the value is relative to IsSOTrx sign)
+	 * @param description
+	 */
+	void writeOffInvoice(org.compiere.model.I_C_Invoice invoice, BigDecimal openAmt, String description);
+}
