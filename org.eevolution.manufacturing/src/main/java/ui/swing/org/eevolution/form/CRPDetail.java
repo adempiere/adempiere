fc6aeadd904ae2a48b1afc2d5629538091e1c318
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2013-09-18 02:46:50 -0500

    Syncronize manufacturing repository

--- org.eevolution.manufacturing/src/main/java/ui/swing/org/eevolution/form/CRPDetail.java
+++ org.eevolution.manufacturing/src/main/java/ui/swing/org/eevolution/form/CRPDetail.java
@@ -10,7 +10,7 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
  * For the text or an alternative of this public license, you may reach us    *
- * Copyright (C) 2003-2010 e-Evolution,SC. All Rights Reserved.              *
+ * Copyright (C) 2003-2010 e-Evolution,SC. All Rights Reserved.               *
  * Contributor(s): Victor Perez www.e-evolution.com                           *
  *****************************************************************************/
 package org.eevolution.form;
