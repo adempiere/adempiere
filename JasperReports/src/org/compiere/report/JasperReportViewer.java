e310dbbfe1d40dd470b66efc65f3306b5298c082
adempiererm <adempiere@walkingtree.in>
2013-10-04 10:48:29 +0530

    Checking in XLS option for jasper reports

--- JasperReports/src/org/compiere/report/JasperReportViewer.java
+++ JasperReports/src/org/compiere/report/JasperReportViewer.java
@@ -6,6 +6,8 @@ package org.compiere.report;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.logging.Level;
 
 import javax.swing.JButton;
@@ -20,6 +22,7 @@ import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 import net.sf.jasperreports.view.JRViewer;
 
 import org.compiere.apps.EMailDialog;
@@ -68,6 +71,7 @@ public class JasperReportViewer extends JRViewer {
         comboBox.setPreferredSize(new java.awt.Dimension(80, 23));
         comboBox.setMaximumSize(new java.awt.Dimension(80, 23));
         comboBox.setMinimumSize(new java.awt.Dimension(80, 23));
+        comboBox.addActionListener(new ReportView(this , comboBox ));
         tlbToolBar.add(comboBox);
 		
 		// Set default viewer zoom level
@@ -82,6 +86,59 @@ public class JasperReportViewer extends JRViewer {
     public String getFormat() {
         return (String) comboBox.getSelectedItem();
     }
+
+}
+
+class ReportView implements ActionListener {
+	
+	private JasperReportViewer viewer;
+	private JComboBox comboBox;
+	
+	public ReportView(JasperReportViewer viewer, JComboBox comboBox) {
+		this.viewer = viewer;
+		this.comboBox = comboBox;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+		JFileChooser fileChooser = new JFileChooser();
+ 		fileChooser.setSelectedFile(new File(viewer.getJasperPrint().getName() +
+ 				"." + viewer.getFormat().toLowerCase()));
+ 		if (fileChooser.showSaveDialog( viewer)==JFileChooser.APPROVE_OPTION) {
+ 			
+ 			File file = fileChooser.getSelectedFile();
+		
+	        try {
+	        	
+	        	if (viewer.getFormat().equals("PDF")) {
+                    JasperExportManager.exportReportToPdfFile( viewer.getJasperPrint(), file.getAbsolutePath());
+	        	} else if (comboBox.getSelectedItem().equals("HTML")) {
+	        		JasperExportManager.exportReportToHtmlFile( viewer.getJasperPrint(), file.getAbsolutePath());
+	        	} else if (comboBox.getSelectedItem().equals("XLS")) {
+	        		
+	        		FileOutputStream fos = new FileOutputStream(file);
+		        	JRXlsExporter exporterXLS = new JRXlsExporter();
+		        	exporterXLS.setParameter(JRXlsExporterParameter.JASPER_PRINT, viewer.getJasperPrint());
+		        	exporterXLS.setParameter(JRXlsExporterParameter.OUTPUT_STREAM, fos);
+		        	exporterXLS.setParameter(JRXlsExporterParameter.OUTPUT_FILE, file.getAbsolutePath());
+		        	exporterXLS.setParameter(JRXlsExporterParameter.IS_DETECT_CELL_TYPE, Boolean.TRUE);
+		        	exporterXLS.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND, Boolean.FALSE);
+		        	exporterXLS.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS, Boolean.TRUE );
+		        	exporterXLS.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_COLUMNS, Boolean.TRUE );
+		        	exporterXLS.setParameter(JRXlsExporterParameter.IS_COLLAPSE_ROW_SPAN, Boolean.TRUE);
+		        	exporterXLS.setParameter(JRXlsExporterParameter.IS_IGNORE_GRAPHICS, Boolean.FALSE);
+		        	exporterXLS.exportReport();
+		        }
+	        } catch (JRException e) {
+	                e.printStackTrace();
+	            } catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			}
+ 		}
+	}
 }
 
 class ExportListener implements ActionListener {
