67f3e6d0fc75902754fb765accedd06ca4e475a2
adempiererm <adempiere@walkingtree.in>
2013-10-30 21:57:46 +0530

    Reverting back the unnecessary commit of RV_C_BankAccount View and rectified the duplicate error in Create Shipment View

--- migration/380lts-release/oracle/XXX_12__CreateShipment_Button_OrderLine.sql
+++ migration/380lts-release/oracle/XXX_12__CreateShipment_Button_OrderLine.sql
@@ -21,11 +21,6 @@ INSERT INTO AD_Process_Para (AD_Client_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,
 INSERT INTO AD_Process_Para_Trl (AD_Language,AD_Process_Para_ID, Description,Help,Name, IsTranslated,AD_Client_ID,AD_Org_ID,Created,Createdby,Updated,UpdatedBy) SELECT l.AD_Language,t.AD_Process_Para_ID, t.Description,t.Help,t.Name, 'N',t.AD_Client_ID,t.AD_Org_ID,t.Created,t.Createdby,t.Updated,t.UpdatedBy FROM AD_Language l, AD_Process_Para t WHERE l.IsActive='Y' AND l.IsSystemLanguage='Y' AND l.IsBaseLanguage='N' AND t.AD_Process_Para_ID=54176 AND NOT EXISTS (SELECT * FROM AD_Process_Para_Trl tt WHERE tt.AD_Language=l.AD_Language AND tt.AD_Process_Para_ID=t.AD_Process_Para_ID)
 ;
 
--- Oct 19, 2013 5:36:41 PM IST
--- Create Shipment Process is Created
-INSERT INTO AD_Column (AD_Client_ID,AD_Column_ID,AD_Element_ID,AD_Org_ID,AD_Process_ID,AD_Reference_ID,AD_Table_ID,ColumnName,Created,CreatedBy,Description,EntityType,FieldLength,Help,IsActive,IsAllowLogging,IsAlwaysUpdateable,IsAutocomplete,IsEncrypted,IsIdentifier,IsKey,IsMandatory,IsParent,IsRange,IsSelectionColumn,IsSyncDatabase,IsTranslated,IsUpdateable,Name,SeqNo,Updated,UpdatedBy,Version) VALUES (0,69034,1490,0,53501,28,260,'CreateFrom',TO_DATE('2013-10-19 17:36:40','YYYY-MM-DD HH24:MI:SS'),0,'Process which will generate a new document lines based on an existing document','D',1,'The Create From process will create a new document based on information in an existing document selected by the user.','Y','Y','Y','N','N','N','N','N','N','N','N','N','N','Y','Create lines from',0,TO_DATE('2013-10-19 17:36:40','YYYY-MM-DD HH24:MI:SS'),0,0)
-;
-
 -- Oct 19, 2013 5:38:07 PM IST
 -- Create Shipment Process is Created
 INSERT INTO AD_Element (AD_Client_ID,AD_Element_ID,AD_Org_ID,ColumnName,Created,CreatedBy,EntityType,IsActive,Name,PrintName,Updated,UpdatedBy) VALUES (0,56518,0,'createshipment',TO_DATE('2013-10-19 17:38:07','YYYY-MM-DD HH24:MI:SS'),0,'D','Y','createshipment','createshipment',TO_DATE('2013-10-19 17:38:07','YYYY-MM-DD HH24:MI:SS'),0)
