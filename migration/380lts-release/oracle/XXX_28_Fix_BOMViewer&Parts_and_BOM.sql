c41953945e42a9c6158e693f3fd92da25177ad15
adempiererm <adempiere@walkingtree.in>
2013-12-23 20:52:57 +0530

    Modified the Manufacturing Light BOM Viewer to Libero BOM viewer and inactivated the Parts and BOM Window

---
+++ migration/380lts-release/oracle/XXX_28_Fix_BOMViewer&Parts_and_BOM.sql
@@ -0,0 +1,15 @@
+-- Dec 23, 2013 10:36:28 AM IST
+-- Changed the Manufacturing Light BOM Viewer to Libero BOM Viewer.
+UPDATE AD_Form SET Classname='org.eevolution.form.VTreeBOM',Updated=TO_DATE('2013-12-23 10:36:28','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Form_ID=53017
+;
+
+-- Dec 23, 2013 10:37:11 AM IST
+-- Changed the Manufacturing Light BOM Viewer to Libero BOM Viewer.
+UPDATE AD_Window SET IsActive='N',Updated=TO_DATE('2013-12-23 10:37:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Window_ID=53128
+;
+
+-- Dec 23, 2013 10:37:11 AM IST
+-- Changed the Manufacturing Light BOM Viewer to Libero BOM Viewer.
+UPDATE AD_Menu SET Description='Maintain Bill of Materials', IsActive='N', Name='Parts and BOMs',Updated=TO_DATE('2013-12-23 10:37:11','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Menu_ID=53298
+;
+
