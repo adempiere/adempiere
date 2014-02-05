e98e788d7e2565b8c4fcd3d85aabac64e2fdf9c3
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2014-02-04 22:25:51 -0600

    New release ADempiere 380

--- base/src/org/compiere/Adempiere.java
+++ base/src/org/compiere/Adempiere.java
@@ -62,9 +62,9 @@ public final class Adempiere
 	// the releases will have a suffix (a) for alpha - (b) for beta - (t) for trunk - (s) for stable - and (LTS) for long term support
 	static public String	MAIN_VERSION	= "Release 3.8.0RC";
 	/** Detail Version as date      Used for Client/Server		*/
-	static public String	DATE_VERSION	= "2013-07-31";
+	static public String	DATE_VERSION	= "2014-04-30";
 	/** Database Version as date    Compared with AD_System		*/
-	static public String	DB_VERSION		= "2013-07-31";
+	static public String	DB_VERSION		= "2014-04-30";
 
 	/** Product Name            */
 	static public final String	NAME 			= "ADempiere\u00AE";
@@ -86,7 +86,7 @@ public final class Adempiere
 	/** Subtitle                */
 	static public final String	SUB_TITLE		= "Smart Suite ERP,CRM and SCM";
 	static public final String	ADEMPIERE_R		= "ADempiere\u00AE";
-	static public final String	COPYRIGHT		= "\u00A9 1999-2013 ADempiere\u00AE";
+	static public final String	COPYRIGHT		= "\u00A9 1999-2014 ADempiere\u00AE";
 
 	static private String		s_ImplementationVersion = null;
 	static private String		s_ImplementationVendor = null;
