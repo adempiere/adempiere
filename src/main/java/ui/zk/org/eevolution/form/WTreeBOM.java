c14f84b3eb4f58502859d39f08d242b3a0cdadaa
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2014-08-08 00:12:47 -0500

    revert Mike and merge cause by ZK incomplete work

--- src/main/java/ui/zk/org/eevolution/form/WTreeBOM.java
+++ src/main/java/ui/zk/org/eevolution/form/WTreeBOM.java
@@ -50,12 +50,12 @@ import org.eevolution.model.MPPProductBOMLine;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.EventListener;
 import org.zkoss.zk.ui.event.Events;
-import org.zkoss.zul.Borderlayout;
-import org.zkoss.zul.Center;
-import org.zkoss.zul.North;
-import org.zkoss.zul.South;
-import org.zkoss.zul.West;
-import org.zkoss.zul.DefaultTreeNode;
+import org.zkoss.zkex.zul.Borderlayout;
+import org.zkoss.zkex.zul.Center;
+import org.zkoss.zkex.zul.North;
+import org.zkoss.zkex.zul.South;
+import org.zkoss.zkex.zul.West;
+import org.zkoss.zul.SimpleTreeNode;
 import org.zkoss.zul.Space;
 import org.zkoss.zul.Tree;
 import org.zkoss.zul.Treecol;
@@ -282,7 +282,7 @@ public class WTreeBOM extends TreeBOM implements IFormController, EventListener,
 		if (M_Product_ID == 0)
 			return;
 		MProduct product = MProduct.get(getCtx(), M_Product_ID);
-		DefaultTreeNode parent = new DefaultTreeNode(productSummary(product, false), new ArrayList());
+        SimpleTreeNode parent = new SimpleTreeNode(productSummary(product, false), new ArrayList());
 
 		dataBOM.clear();
 
@@ -348,14 +348,14 @@ public class WTreeBOM extends TreeBOM implements IFormController, EventListener,
 		m_tree.addEventListener(Events.ON_SELECTION, this);
 		loadTableBOM();
 	}
-	
-	public DefaultTreeNode parent(MPPProductBOMLine bomline) 
+
+    public SimpleTreeNode parent(MPPProductBOMLine bomline)
 	{
 
 		
 		MProduct M_Product = MProduct.get(getCtx(), bomline.getM_Product_ID());
 		MPPProductBOM bomproduct = new MPPProductBOM(getCtx(), bomline.getPP_Product_BOM_ID(), null);
-		DefaultTreeNode parent = new DefaultTreeNode(productSummary(M_Product, false), new ArrayList());
+        SimpleTreeNode parent = new SimpleTreeNode(productSummary(M_Product, false), new ArrayList());
 
 		Vector<Object> line = new Vector<Object>(17);
 		line.add( new Boolean(false));  //  0 Select
@@ -386,10 +386,10 @@ public class WTreeBOM extends TreeBOM implements IFormController, EventListener,
 		}
 		return parent;
 	}
-	
-	public DefaultTreeNode parent(MPPProductBOM bom) 
-	{
-		DefaultTreeNode parent = new DefaultTreeNode(productSummary(bom), new ArrayList()); 
+
+	public SimpleTreeNode parent(MPPProductBOM bom)
+    {
+		SimpleTreeNode parent = new SimpleTreeNode(productSummary(bom), new ArrayList());
 
 		for (MPPProductBOMLine bomline : bom.getLines())
 		{
@@ -420,13 +420,13 @@ public class WTreeBOM extends TreeBOM implements IFormController, EventListener,
 		}
 		return parent;
 	}
-	
-	public DefaultTreeNode component(MProduct product) 
-	{   
+
+    public SimpleTreeNode component(MProduct product)
+    {
 
 		if (isImplosion())
 		{
-			DefaultTreeNode parent = new DefaultTreeNode(productSummary(product, false), new ArrayList());
+            SimpleTreeNode parent = new SimpleTreeNode(productSummary(product, false), new ArrayList());
 			for (MPPProductBOMLine bomline : MPPProductBOMLine.getByProduct(product))
 			{
 				parent.getChildren().add(parent(bomline));
@@ -438,9 +438,9 @@ public class WTreeBOM extends TreeBOM implements IFormController, EventListener,
 			for (MPPProductBOM bom : MPPProductBOM.getProductBOMs(product))
 			{
 				return parent(bom);
-			}  
-			return new DefaultTreeNode(productSummary(product, true), new ArrayList());
-		}
+			}
+            return new SimpleTreeNode(productSummary(product, true), new ArrayList());
+        }
 	}
 
 	private int getM_Product_ID() {
