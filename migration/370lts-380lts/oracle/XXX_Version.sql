10fa80bd6e548199434014e543ea0396d7b9eacd
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2014-11-25 22:51:00 -0600

    Update version date

--- migration/370lts-380lts/oracle/XXX_Version.sql
+++ migration/370lts-380lts/oracle/XXX_Version.sql
@@ -3,5 +3,5 @@ UPDATE AD_Column SET FieldLength=10,Updated=TO_DATE('2014-04-30 00:00:00','YYYY-
 ;
 
 UPDATE AD_SYSTEM
- SET releaseno = '3.8.0LTS', VERSION = '2014-04-30'
+ SET releaseno = '3.8.0LTS', VERSION = '2015-01-01'
   WHERE ad_system_id = 0 AND ad_client_id = 0;
