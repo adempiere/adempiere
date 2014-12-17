c3a089490851ad77437ad467b1128720f14847a5
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2014-12-16 22:59:56 -0600

    Solve issue when try create column view for a smart browser

--- base/src/org/adempiere/model/MViewDefinition.java
+++ base/src/org/adempiere/model/MViewDefinition.java
@@ -163,10 +163,11 @@ public class MViewDefinition extends X_AD_View_Definition {
 	 * @return List<MColumn>
 	 */
 	public List<MColumn> getEntityAttributes() {
-		final String whereClause = MColumn.COLUMNNAME_AD_Table_ID + "=? AND NOT EXISTS(SELECT 1 FROM AD_View_Column WHERE AD_View_Column.AD_Column_ID=AD_Column.AD_Column_ID)";
+		final String whereClause = MColumn.COLUMNNAME_AD_Table_ID + "=? AND NOT EXISTS(SELECT 1 FROM AD_View_Column "
+				+ "WHERE AD_View_Column.AD_Column_ID=AD_Column.AD_Column_ID AND AD_View_Column.AD_View_Definition_ID=?)";
 
 		return new Query(getCtx(), MColumn.Table_Name, whereClause,
-				get_TrxName()).setParameters(getAD_Table_ID())
+				get_TrxName()).setParameters(getAD_Table_ID(),getAD_View_Definition_ID())
 				.setOnlyActiveRecords(true).list();
 	}
 
