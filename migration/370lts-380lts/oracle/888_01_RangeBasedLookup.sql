3e802e17a0f5923a85f9848f3df12f104010b558
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2015-01-02 00:58:13 -0600

    Solve issue with is range field and XML Script

--- migration/370lts-380lts/oracle/888_01_RangeBasedLookup.sql
+++ migration/370lts-380lts/oracle/888_01_RangeBasedLookup.sql
@@ -71,3 +71,6 @@ UPDATE AD_Field SET IsDisplayed='Y', SeqNo=400,Updated=TO_DATE('2013-09-25 12:26
 UPDATE AD_Field SET IsDisplayed='Y', SeqNo=410,Updated=TO_DATE('2013-09-25 12:26:26','YYYY-MM-DD HH24:MI:SS'),UpdatedBy=0 WHERE AD_Field_ID=5122
 ;
 
+UPDATE AD_Column SET IsRange='N';
+
+UPDATE AD_Column SET DefaultValue = 'N' WHERE AD_Column_ID=68024;
