a0ef952ebe510976f6ab59eccd282077d27932d1
mckayERP <michael.mckay@mckayERP.com>
2015-12-09 21:50:08 +0100

    Strip down the migration loader to the minimal code.  It now calls processes to to the work.  Corrected the reference to the RoleAccessUpdate process.
    The key processes for migrations are: MigrationFromXML, MigrationApply, MigrationStepApply.  MigrationToXML has not been changed.
    
    These processes have been simplified by moving as much of the model knowledge into the model classes.as was possible.

--- base/src/org/adempiere/process/MigrationLoader.java
+++ base/src/org/adempiere/process/MigrationLoader.java
@@ -17,6 +17,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.adempiere.exceptions.AdempiereException;
 import org.apache.commons.io.FileUtils;
 import org.compiere.Adempiere;
 import org.compiere.model.I_AD_Migration;
@@ -30,7 +31,6 @@ import org.compiere.util.CLogger;
 import org.compiere.util.DB;
 import org.compiere.util.Env;
 import org.compiere.util.Trx;
-import org.compiere.util.TrxRunnable;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -38,83 +38,72 @@ import org.xml.sax.SAXException;
 
 public class MigrationLoader {
 	
-	DocumentBuilder builder;
 	/**	Logger	*/
 	private static CLogger	log	= CLogger.getCLogger (MigrationLoader.class);
 	
-	public Comparator<File> fileComparator = new Comparator<File>() {
-		// Note - Not locale sensitive.
-	    public int compare(File f1, File f2) {
-	        return f1.getName().compareToIgnoreCase(f2.getName());
-	    }
-	};
-		
-	static String readFile(File file, Charset encoding) throws IOException 
-	{
-	  byte[] encoded = Files.readAllBytes(Paths.get(file.getPath()));
-	  return new String(encoded, encoding);
-	}
-		
-	@SuppressWarnings("unchecked")
-	/**
-	 * Load XML migration file or files.  
-	 * @param file A file or directory containing migration files.  If file 
-	 * points to a directory, a recursive search of all sub-directories will
-	 * be performed and all XML files found will be loaded. 
-	 * @param apply True if the loaded migrations are to be applied.
-	 */
-	public void loadXML(File file, Boolean apply)
-	{
-		Boolean success = false;
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		dbf.setNamespaceAware(true);
-		dbf.setIgnoringElementContentWhitespace(true);
 
-		try {
-			builder = dbf.newDocumentBuilder();
-			
-			List<File> migrationFiles = new ArrayList<File>();
-			
-			if ( !file.exists() )
-			{
-				log.log(Level.WARNING, "No file or directory found");
-				return;
-			}
-			else if ( file.isDirectory() )  // file exists
-			{
-				log.log(Level.CONFIG, "Processing migration files in directory: " + file.getAbsolutePath() );
-				// Recursively find files
-				migrationFiles = (List<File>) FileUtils.listFiles(file, new String[]{"xml"}, true);
-				Collections.sort(migrationFiles, fileComparator);
-			}
-			else {
-				log.log(Level.CONFIG, "Processing migration file: " + file.getAbsolutePath() );				
-				migrationFiles.add(file);
-			}
-			
-			
-			for (File migFile : migrationFiles )
-			{
-				loadFile(migFile, apply);
-			}
-			
-			success = true;
-
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static void main(String[] args) {
+		
+		boolean clean_arg = false;
+		// If called with the argument "clean", the loader will apply any
+		// outstanding migrations, mark ALL applied dictionary migrations as processed
+		// and delete all the steps and data to save space.
+		if (args.length > 0)
+		 clean_arg = args[0].equals("clean");
+		
+		Adempiere.startupEnvironment(false);
+		CLogMgt.setLevel(Level.CONFIG);
+		
+		if (! DB.isConnected()) {
+			log.info("No DB Connection");
+			System.exit(1);
 		}
 		
-		// If the loading and application of the migrations was successful,
-		// run the post processes.  Don't run these if the migrations are 
-		// just loaded but not applied.
-		if (apply && success) {
+		
+		// Parameter values - these need to be final or effectively final
+		// Load migrations from the default location
+		String fileName = Adempiere.getAdempiereHome() + File.separator + "migration";
+		boolean apply = true;
+		boolean failOnError = true;
+		boolean clean = clean_arg;
+
+		Properties ctx = Env.getCtx();
+
+		try {
+			Trx.run(trxName -> {
+				int processId = 53175; // Import Migration from XML
+				MPInstance instance = new MPInstance(ctx, processId, 0);
+				instance.saveEx();
+				// FailOnError
+				MPInstancePara parameter = new MPInstancePara(instance,10);
+				parameter.setParameter("FailOnError",failOnError);
+				parameter.saveEx();
+				// FileName
+				parameter = new MPInstancePara(instance,20);
+				parameter.setParameter("FileName", fileName);
+				parameter.saveEx();
+				// Apply
+				parameter = new MPInstancePara(instance,30);
+				parameter.setParameter("Apply", apply);
+				parameter.saveEx();
+				// Clean
+				parameter = new MPInstancePara(instance,40);
+				parameter.setParameter("Clean", clean);
+				parameter.saveEx();
+	
+				ProcessInfo pi = new ProcessInfo("Import Migration from XML", processId);
+				pi.setAD_PInstance_ID(instance.getAD_PInstance_ID());
+				pi.setRecord_ID(0);
+				ServerProcessCtl migrationProcess = new ServerProcessCtl(null, pi, Trx.get(trxName, false));
+				migrationProcess.run();
+				log.log(Level.CONFIG, "Process=" + pi.getTitle() + " Error="+pi.isError() + " Summary=" + pi.getSummary());
+				if (failOnError && pi.isError())
+					throw new AdempiereException(pi.getSummary());
+			});
+			
+			// The import process will throw an AdempiereException if unsuccessful.
+
+			// Run the post processes.
 			Trx.run(trxName -> {
 				// Run the post processes
 				int processId = 258; // Sequence Check"
@@ -140,7 +129,7 @@ public class MigrationLoader {
 			});
 
 			Trx.run(trxName -> {
-				int processId = 172; // Role Access Update
+				int processId = 295; // Role Access Update
 				MPInstance instance = new MPInstance(Env.getCtx(), processId, 0);
 				ProcessInfo pi = new ProcessInfo("Role Access Update", processId);
 				instance.saveEx();
@@ -153,7 +142,7 @@ public class MigrationLoader {
 			});
 
 			Trx.run(trxName -> {
-				int processId = 53733; // Role Access Update
+				int processId = 53733; // UpdateGW
 				MPInstance instance = new MPInstance(Env.getCtx(), processId, 0);
 				instance.saveEx();
 				ProcessInfo pi = new ProcessInfo("Updating Garden World", processId);
@@ -165,126 +154,8 @@ public class MigrationLoader {
 				updateGW.start();
 				log.log(Level.CONFIG, "Process=" + pi.getTitle() + " Error="+pi.isError() + " Summary=" + pi.getSummary());
 			});
+		} catch (AdempiereException e) {
+			e.printStackTrace();
 		}
 	}
-	
-	public void loadFile(File file, Boolean apply) throws SAXException, IOException {
-
-		if (!file.exists()) return;
-		
-		if (!file.getName().endsWith(".xml")) return;
-		
-		if (file.getName().equals("build.xml")) return; 
-		
-		log.log(Level.CONFIG, "Loading file: " + file);
-		
-		Document doc = builder.parse(file);
-
-		NodeList migrations = doc.getDocumentElement().getElementsByTagName("Migration");
-		for ( int i = 0; i < migrations.getLength(); i++ ) {
-
-			Trx.run(new TrxRunnable() {
-				private Properties ctx;
-				private Element element;
-				MMigration migration;
-
-				TrxRunnable setParameters(Properties ctx, Element element, MigrationLoader loader) {
-					this.ctx = ctx;
-					this.element = element;
-					return this;
-				}
-
-				public void run(String trxName) {
-					try {
-							migration = MMigration.fromXmlNode(ctx, element, trxName);
-							migration.setMigrationScriptBatch(true);
-                            if (MMigration.STATUSCODE_Applied.equals(migration.getStatusCode())) {
-                                log.log(Level.CONFIG, migration.toString() + " ---> Migration already applied - skipping.");
-                                return;
-                            }
-							if (migration == null) {
-								log.log(Level.CONFIG, "XML file not a Migration. Skipping.");
-								return;
-							}
-
-							if (apply) {
-								applyMigration(migration.getCtx(), migration.getAD_Migration_ID());
-								migration.updateStatus();
-							}
-					} catch (SQLException e) {
-						e.printStackTrace();
-					}
-				}
-			}.setParameters(Env.getCtx(), (Element) migrations.item(i), this));
-		}
-
-	}
-
-	private void applyMigration(Properties ctx  , int migrationId) {
-		Trx.run(trxName -> {
-			int processId = 53173; // Role Access Update
-			MPInstance instance = new MPInstance(ctx, processId, migrationId);
-			instance.saveEx();
-			MPInstancePara parameter = new MPInstancePara(instance,10);
-			parameter.setParameter("FailOnError",true);
-			parameter.saveEx();
-
-			parameter = new MPInstancePara(instance,20);
-			parameter.setParameter("MigrationScriptBatch",true);
-			parameter.saveEx();
-
-			ProcessInfo pi = new ProcessInfo("Apply migration", processId);
-			pi.setAD_PInstance_ID(instance.getAD_PInstance_ID());
-			pi.setRecord_ID(migrationId);
-			ServerProcessCtl migrationProcess = new ServerProcessCtl(null, pi, Trx.get(trxName, false));
-			migrationProcess.run();
-			log.log(Level.CONFIG, "Process=" + pi.getTitle() + " Error="+pi.isError() + " Summary=" + pi.getSummary());
-		});
-	}
-
-	public static void main(String[] args) {
-		
-		
-		Boolean clean_migrations = false;
-		// If called with the argument "clean", the loader will apply any
-		// outstanding migrations, mark ALL applied dictionary migrations as processed
-		// and delete all the steps and data to save space.
-		if (args.length > 0)
-		 clean_migrations = args[0].equals("clean");
-		
-		Adempiere.startupEnvironment(false);
-		CLogMgt.setLevel(Level.CONFIG);
-		
-		if (! DB.isConnected()) {
-			log.info("No DB Connection");
-			System.exit(1);
-		}
-		
-		// Load migrations from the default location
-		File home = new File (Adempiere.getAdempiereHome() + File.separator + "migration");
-		Boolean apply = true;
-
-		MigrationLoader loader = new MigrationLoader();		
-		loader.loadXML(home, apply);  // and apply - each migration has to be applied before the next is loaded.
-		loader.clean(Env.getCtx(), clean_migrations, null);		
-	}
-
-	private void clean(Properties ctx, Boolean clean_migrations, String trxName) {
-		
-		if (!clean_migrations)
-			return;
-		// For backward compatibility, check if the processed column has been added 
-		// to the AD_Migration table
-		// The processed column was added to AD_MigrationStep just prior to release 3.8.0.
-		MMigration migration = new MMigration(ctx,0,trxName);
-		if (migration.get_ColumnIndex(I_AD_Migration.COLUMNNAME_Processed) < 0)
-			return;
-
-		migration = null;
-		Boolean notProcessed = false;
-		for (MMigration mig : MMigration.getMigrations(ctx, notProcessed, trxName)) {
-			if (mig != null)
-				mig.clean();
-		}
-	}	
 }
