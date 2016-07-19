e0a6d23d6f5fe759e53975b10c65c33f398a78a8
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2016-07-19 01:44:59 -0500

    #457 Apply Adempiere best practice https://github.com/adempiere/adempiere/pull/457 1.- Rename variables name to a best debug process 2.- Remove variable declared with null reference would cause NPE 3.- Refactor to functional programing is a best approach for Java 8

--- base/src/org/compiere/process/InvoiceCreateFrom.java
+++ base/src/org/compiere/process/InvoiceCreateFrom.java
@@ -17,6 +17,9 @@
 package org.compiere.process;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MInOut;
@@ -33,11 +36,13 @@ import org.compiere.model.PO;
 import org.compiere.util.Env;
 
 /** Generated Process for (Invoice Create From)
- *  @author ADempiere (generated) 
+ *  @author ADempiere (generated)
+ * 	@author Yamel Senih, ysenih@erpcya.com, ERPCyA http://www.erpcya.com
+ *	@author Victor Perez , victor.perez@e-evolution.com, http://e-evolution.com
  *  @version Release 3.8.0
  */
 public class InvoiceCreateFrom extends InvoiceCreateFromAbstract {
-	
+
 	/**	Create From Type of RMA		*/
 	private static final String RMA = "A";
 	/**	Create From Type of Order	*/
@@ -46,120 +51,90 @@ public class InvoiceCreateFrom extends InvoiceCreateFromAbstract {
 	private static final String RECEIPT = "R";
 	/**	Create From Type of Invoice	*/
 	private static final String INVOICE = "I";
-	/**	Create From Type			*/
-	private String 		createFromType = null;
-	/**	Created						*/
-	private int 		created = 0;
-	/**	Invoice						*/
-	private MInvoice 	invoice = null;
-	/**	Reference Identifier		*/
-	private int 		reference_ID = 0;
-	
 	@Override
 	protected void prepare() {
 		super.prepare();
 	}
-	
+
 	@Override
 	protected String doIt() throws Exception {
 		// Valid Record Identifier
 		if(getRecord_ID() == 0)
 			return "";
 		//	Get Shipment
-		invoice = new MInvoice(getCtx(), getRecord_ID(), get_TrxName());
-		log.config(invoice.toString());
+		MInvoice invoice = new MInvoice(getCtx(), getRecord_ID(), get_TrxName());
+		AtomicInteger referenceId = new AtomicInteger(0);
+		AtomicInteger 	created = new AtomicInteger(0);
+		List<Integer> recordIds =  getSelectionKeys();
+		String createFromType = recordIds.size() > 0 ?  getSelectionAsString(recordIds.get(0), "CF_CreateFromType") : null;
+		log.fine("CreateFromType=" + createFromType);
+		if (createFromType == null || createFromType.length() == 0)
+			throw new AdempiereException("@CreateFromType@ @NotFound@");
+
 		//	Loop
-		for(Integer key : getSelectionKeys()) {
-			if(createFromType == null) {
-				createFromType = getSelectionAsString(key, "CF_CreateFromType");
-				//	Valid Mandatory Create From Type
-				if(createFromType == null
-						|| createFromType.length() == 0)
-					throw new AdempiereException("@CreateFromType@ @NotFound@");
-			}
+		recordIds.stream().forEach( key -> {
 			// variable values
-			
-			int m_M_Product_ID = getSelectionAsInt(key, "CF_M_Product_ID");
-			int m_C_Charge_ID = getSelectionAsInt(key, "CF_C_Charge_ID");
-			int m_C_UOM_ID = getSelectionAsInt(key, "CF_C_UOM_ID");
-			BigDecimal m_QtyEntered = getSelectionAsBigDecimal(key, "CF_QtyEntered"); // Qty
+			int productId = getSelectionAsInt(key, "CF_M_Product_ID");
+			int chargeId = getSelectionAsInt(key, "CF_C_Charge_ID");
+			int uomId = getSelectionAsInt(key, "CF_C_UOM_ID");
+			BigDecimal qtyEntered = getSelectionAsBigDecimal(key, "CF_QtyEntered"); // Qty
 			// If a locator is specified on the product, choose that otherwise default locator
-			//	Precision of Qty UOM
-			int precision = 2;
-			if (m_M_Product_ID != 0) {
-				MProduct product = MProduct.get(getCtx(), m_M_Product_ID);
-				precision = product.getUOMPrecision();
-			}
-			m_QtyEntered = m_QtyEntered.setScale(precision, BigDecimal.ROUND_HALF_DOWN);
-			//
-			log.fine("Line QtyEntered=" + m_QtyEntered
-					+ ", Product=" + m_M_Product_ID 
+			log.fine("Line QtyEntered=" + qtyEntered
+					+ ", Product=" + productId
 					+ ", CreateFromType=" + createFromType + ", Key=" + key);
 
 			//	Create new Invoice Line
 			MInvoiceLine invoiceLine = new MInvoiceLine(invoice);
-			BigDecimal QtyInvoiced = null;
-			MProduct product = null;
-			if (m_M_Product_ID != 0) {
-				product = MProduct.get(Env.getCtx(), m_M_Product_ID);
-				precision = product.getUOMPrecision();
-				invoiceLine.setM_Product_ID(m_M_Product_ID, m_C_UOM_ID);	//	Line UOM
-			} else if(m_C_Charge_ID != 0){
-				invoiceLine.setC_Charge_ID(m_C_Charge_ID);
+			BigDecimal qtyInvoiced = null;
+			//	Precision of Qty UOM
+			int precision = 2;
+			if (productId > 0) {
+				MProduct product = MProduct.get(Env.getCtx(), productId);
+				if (product != null) {
+					invoiceLine.setM_Product_ID(product.getM_Product_ID(), uomId);
+					precision = product.getUOMPrecision();
+					if (product.getC_UOM_ID() != uomId) {
+						qtyEntered = qtyEntered.setScale(precision, BigDecimal.ROUND_HALF_DOWN);
+						qtyInvoiced = MUOMConversion.convertProductFrom(Env.getCtx(), productId, uomId, qtyEntered);
+					}
+				}
+			} else if(chargeId != 0) {
+				invoiceLine.setC_Charge_ID(chargeId);
 			}
-			//	
-			if (product != null 
-					&& product.getC_UOM_ID() != m_C_UOM_ID) {
-				QtyInvoiced = MUOMConversion.convertProductFrom(Env.getCtx(), m_M_Product_ID, m_C_UOM_ID, m_QtyEntered);
-			}
-			//	
-			m_QtyEntered = m_QtyEntered.setScale(precision, BigDecimal.ROUND_HALF_DOWN);
-			if (QtyInvoiced == null)
-				QtyInvoiced = m_QtyEntered;
-			//	
-			invoiceLine.setQty(m_QtyEntered);							//	Movement/Entered
-			invoiceLine.setQtyInvoiced(QtyInvoiced);
-			//
+
+			qtyEntered = qtyEntered.setScale(precision, BigDecimal.ROUND_HALF_DOWN);
+			if (qtyInvoiced == null)
+				qtyInvoiced = qtyEntered;
+
+			invoiceLine.setQty(qtyEntered);							//	Movement/Entered
+			invoiceLine.setQtyInvoiced(qtyInvoiced);
 			if(createFromType.equals(ORDER)) {
 				MOrderLine orderLine = new MOrderLine (getCtx(), key, get_TrxName());
 				//	Set reference
-				if(reference_ID == 0)
-					reference_ID = orderLine.getC_Order_ID();
+				referenceId.set(orderLine.getC_Order_ID());
 				//	Set InOut
 				String whereClause = "EXISTS (SELECT 1 "
 						+ "FROM M_InOut io "
 						+ "WHERE io.M_InOut_ID = M_InOutLine.M_InOut_ID "
 						+ "AND io.DocStatus IN ('CO','CL'))";
-				MInOutLine[] lines = MInOutLine.getOfOrderLine(Env.getCtx(),
-					key, whereClause, get_TrxName());
-				log.fine ("Receipt Lines with OrderLine = #" + lines.length);
-				MInOutLine inoutLine = null;
-				if (lines.length > 0) {
-					for (int j = 0; j < lines.length; j++) {
-						MInOutLine line = lines[j];
-						if (line.getQtyEntered().compareTo(m_QtyEntered) == 0) {
-							inoutLine = line;
-							break;
-						}
-					}
-					if (inoutLine == null) {
-						inoutLine = lines[0];	//	first as default
-					}
-				}
+				MInOutLine[] inOutLines = MInOutLine.getOfOrderLine(Env.getCtx(), key, whereClause, get_TrxName());
+				log.fine ("Receipt Lines with OrderLine = #" + inOutLines.length);
+				final BigDecimal qty = qtyEntered;
+				MInOutLine inOutLine = Arrays.stream(inOutLines)
+						.filter(ioLine -> ioLine != null && ioLine.getQtyEntered().compareTo(qty) == 0)
+						.findFirst().orElse(inOutLines.length > 0 ? inOutLines[0] : null);
 				//	Set From
-				if(inoutLine != null)
-					invoiceLine.setShipLine(inoutLine);
+				if(inOutLine != null)
+					invoiceLine.setShipLine(inOutLine);
 				else
 					invoiceLine.setOrderLine(orderLine);
 			} else if(createFromType.equals(INVOICE)) {
 				MInvoiceLine fromLine = new MInvoiceLine(getCtx(), key, get_TrxName());
-				MInvoice invoice = invoiceLine.getParent();
 				//	Set reference
-				if(reference_ID == 0)
-					reference_ID = invoice.getC_Invoice_ID();
+				referenceId.set(invoiceLine.getParent().getC_Invoice_ID());
 				//	Copy Values
 				PO.copyValues(fromLine, invoiceLine);
-				invoiceLine.setC_Invoice_ID(invoice.getC_Invoice_ID());
+				invoiceLine.setC_Invoice_ID(invoiceLine.getParent().getC_Invoice_ID());
 				invoiceLine.setAD_Org_ID(fromLine.getAD_Org_ID());
 				//	Reset
 				invoiceLine.setC_OrderLine_ID(0);
@@ -169,22 +144,20 @@ public class InvoiceCreateFrom extends InvoiceCreateFromAbstract {
 				invoiceLine.setM_AttributeSetInstance_ID(0);
 				invoiceLine.setS_ResourceAssignment_ID(0);
 				//	New Tax
-				if (invoice.getC_BPartner_ID() != fromLine.getC_Invoice().getC_BPartner_ID())
+				if (invoiceLine.getParent().getC_BPartner_ID() != fromLine.getC_Invoice().getC_BPartner_ID())
 					invoiceLine.setTax();	//	recalculate
 				//
 				invoiceLine.setProcessed(false);
 			} else if(createFromType.equals(RMA)) {
 				MRMALine rmaLine = new MRMALine(getCtx(), key, get_TrxName());
 				//	Set reference
-				if(reference_ID == 0)
-					reference_ID = rmaLine.getM_RMA_ID();
-				//	
+				referenceId.set(rmaLine.getM_RMA_ID());
+				//
 				invoiceLine.setRMALine(rmaLine);
 			} else if(createFromType.equals(RECEIPT)) {
 				MInOutLine inOutLine = new MInOutLine(getCtx(), key, get_TrxName());
 				//	Set reference
-				if(reference_ID == 0)
-					reference_ID = inOutLine.getM_InOut_ID();
+				referenceId.set(inOutLine.getM_InOut_ID());
 				invoiceLine.setShipLine(inOutLine);
 			}
 			//	Save
@@ -198,26 +171,28 @@ public class InvoiceCreateFrom extends InvoiceCreateFromAbstract {
 				// end MZ
 			}
 			//	Add to created
-			created ++;
-		}
+			created.updateAndGet(createNo -> createNo + 1);
+		});
 		//	Add reference to Order / Invoice / RMA
-		addReference();
-		//	
-		return "@Created@ " + created;
+		addReference(invoice, createFromType , referenceId.get());
+		return "@Created@ " + created.get();
 	}
-	
+
 	/**
 	 * Add Reference to Order / Invoice / RMA
-	 */
-	private void addReference() {
+	 * @param invoice
+	 * @param createFromType
+	 * @param referenceId
+     */
+	private void addReference(MInvoice invoice, String createFromType , int referenceId ) {
 		//	Valid Reference
-		if(reference_ID == 0)
+		if(referenceId == 0)
 			return;
 		if(createFromType.equals(ORDER)) {
-			MOrder p_order = new MOrder(getCtx(), reference_ID, get_TrxName());
-			invoice.setOrder(p_order);
+			MOrder order = new MOrder(getCtx(), referenceId, get_TrxName());
+			invoice.setOrder(order);
 		} else if(createFromType.equals(INVOICE)) {
-			MInvoice fromInvoice = new MInvoice(getCtx(), reference_ID, get_TrxName());
+			MInvoice fromInvoice = new MInvoice(getCtx(), referenceId, get_TrxName());
 			invoice.setAD_OrgTrx_ID(fromInvoice.getAD_OrgTrx_ID());
 			invoice.setC_Project_ID(fromInvoice.getC_Project_ID());
 			invoice.setC_Campaign_ID(fromInvoice.getC_Campaign_ID());
@@ -225,10 +200,10 @@ public class InvoiceCreateFrom extends InvoiceCreateFromAbstract {
 			invoice.setUser1_ID(fromInvoice.getUser1_ID());
 			invoice.setUser2_ID(fromInvoice.getUser2_ID());
 		} else if(createFromType.equals(RMA)) {
-			MRMA rma = new MRMA(getCtx(), reference_ID, get_TrxName());
+			MRMA rma = new MRMA(getCtx(), referenceId, get_TrxName());
 			invoice.setRMA(rma);
 		} else if(createFromType.equals(RECEIPT)) {
-			MInOut inOut = new MInOut(getCtx(), reference_ID, get_TrxName());
+			MInOut inOut = new MInOut(getCtx(), referenceId, get_TrxName());
 			invoice.setShipment(inOut);
 		}
 		//	Save
