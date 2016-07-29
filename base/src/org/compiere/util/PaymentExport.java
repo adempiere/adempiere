089dcdea0cd8ffe9d61fef110e32020213b5a448
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2016-07-29 13:09:30 -0500

    #297  Payment Selection must be like ADempiere Document https://github.com/adempiere/adempiere/issues/297 1.- Change of the for sentence for functional approach using stream . 2.- Remove result set and JDB query for query object 3.- Rename variable nice debug and DSL approach , the code is documentation 4.- Remove array to List and collection

--- base/src/org/compiere/util/PaymentExport.java
+++ base/src/org/compiere/util/PaymentExport.java
@@ -17,6 +17,7 @@
 package org.compiere.util;
 
 import java.io.File;
+import java.util.List;
 
 import org.compiere.model.MPaySelectionCheck;
 
@@ -24,6 +25,9 @@ import org.compiere.model.MPaySelectionCheck;
  * 	Custom Payment Export Interface
  *	
  *  @author Carlos Ruiz - GlobalQSS
+ *	@author  victor.perez , victor.perez@e-evolution.com http://www.e-evolution.com
+ * 		<li> FR [ 297 ] Apply ADempiere best Pratice
+ *		@see https://github.com/adempiere/adempiere/issues/297
  *  @version PaymentExport.java
  */
 public interface PaymentExport
@@ -35,6 +39,6 @@ public interface PaymentExport
 	 *  @param file file to export checks
 	 *  @return number of lines
 	 */
-	public int exportToFile (MPaySelectionCheck[] checks, File file, StringBuffer err);
+	public int exportToFile (List<MPaySelectionCheck> checks, File file, StringBuffer error);
 
 }	//	PaymentExport
