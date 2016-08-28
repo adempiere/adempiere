1cd65aea6d3c1d5e134e31c8b4116bdac920acab
Yamel Senih (ERPCyA) <ysenih@erpcya.com>
2016-08-27 19:58:55 -0430

    #297 Add Smart Browser for generate payment selection from application menu add observation from issue https://github.com/adempiere/adempiere/issues/297 and https://github.com/adempiere/adempiere/pull/462

---
+++ base/src/org/compiere/process/PSCreateFromPaySelectionAbstract.java
@@ -0,0 +1,55 @@
+/******************************************************************************
+ * Product: ADempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 2006-2016 ADempiere Foundation, All Rights Reserved.         *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * or via info@adempiere.net or http://www.adempiere.net/license.html         *
+ *****************************************************************************/
+
+package org.compiere.process;
+
+
+/** Generated Process for (Create from Payment Selection)
+ *  @author ADempiere (generated) 
+ *  @version Release 3.8.0
+ */
+public abstract class PSCreateFromPaySelectionAbstract extends SvrProcess
+{
+	/** Process Value 	*/
+	private static final String VALUE = "SBP_PaySelectionCreateFromPayselection";
+	/** Process Name 	*/
+	private static final String NAME = "Create from Payment Selection";
+	/** Process Id 	*/
+	private static final int ID = 53841;
+ 
+
+ 
+
+	@Override
+	protected void prepare()
+	{
+	}
+
+	/**	 Getter Parameter Value for Process ID	*/
+	public static final int getProcessId() {
+		return ID;
+	}
+
+	/**	 Getter Parameter Value for Process Value	*/
+	public static final String getProcessValue() {
+		return VALUE;
+	}
+
+	/**	 Getter Parameter Value for Process Name	*/
+	public static final String getProcessName() {
+		return NAME;
+	}
+}
