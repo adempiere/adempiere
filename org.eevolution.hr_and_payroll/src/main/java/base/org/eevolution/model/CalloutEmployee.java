9e65dc361e31fd2a42292c207ce8dfcb8b7ad6b5
Yamel Senih (ERPCyA) <ysenih@erpcya.com>
2017-02-06 18:46:43 -0430

    #753, Add reference to Business Partner on HR_EmployeeDependent, It allow that a employee can have a dependent with data from Business Partner reference to issue: https://github.com/adempiere/adempiere/issues/753

--- org.eevolution.hr_and_payroll/src/main/java/base/org/eevolution/model/CalloutEmployee.java
+++ org.eevolution.hr_and_payroll/src/main/java/base/org/eevolution/model/CalloutEmployee.java
@@ -1,12 +1,26 @@
-/**
- *
- */
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2007 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
 package org.eevolution.model;
 
 import org.adempiere.model.GridTabWrapper;
 import org.compiere.model.CalloutEngine;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
+import org.compiere.model.MBPartner;
 import org.compiere.util.Msg;
 
 import java.math.BigDecimal;
@@ -18,6 +32,9 @@ import java.util.Properties;
  * @author Alok Ranjan
  * @author victor.perez@e-evolution.com, www.e-evolution.com
  * 			<li> Refactory and apply ADempiere Best Practice
+ * @author Yamel Senih, ysenih@erpcya.com, ERPCyA http://www.erpcya.com
+ *		<a href="https://github.com/adempiere/adempiere/issues/753">
+ * 		@see FR [ 753 ] Add Business Partner reference on Employee Dependents</a>
  */
 public class CalloutEmployee extends CalloutEngine {
 
@@ -60,6 +77,38 @@ public class CalloutEmployee extends CalloutEngine {
         I_HR_EmployeeInsurance employeeInsurance = GridTabWrapper.create(mTab, I_HR_EmployeeInsurance.class);
         BigDecimal balanceAmount = employeeInsurance.getCoverageAmount().subtract(employeeInsurance.getClaimedAmount());
         employeeInsurance.setBalanceAmount(balanceAmount);
+        return;
+    }
+    
+	/**
+	 * Get Name from Business Partner and set it
+	 * @param ctx
+	 * @param WindowNo
+	 * @param mTab
+	 * @param mField
+	 * @param value
+	 */
+    public void bpartnerRelation(Properties ctx, int WindowNo, GridTab mTab,
+                             GridField mField, Object value) {
+    	//	
+        if (value == null || isCalloutActive()) {
+            return;
+        }
+        //	Get BP
+        Integer bPartnerId = (Integer) value;
+        if(bPartnerId.intValue() <= 0)
+        	return;
+        //	Get Names
+        MBPartner bPartner = MBPartner.get(ctx, bPartnerId);
+        //	Set Name
+        String name = bPartner.getName();
+        if(bPartner.getName2() != null
+        		&& bPartner.getName2().trim().length() > 0) {
+        	name = name + " " + bPartner.getName2();
+        }
+        
+        mTab.setValue(I_HR_EmployeeDependent.COLUMNNAME_Name, name);
+        //	
         return;
     }
 }
