ae435c2211360db5c8bd805607c5f27333b9b575
Yamel Senih <ysenih@erpcya.com>
2017-07-01 17:47:12 -0400

    #1116, Change folder name for a standard, reference to issue: https://github.com/adempiere/adempiere/issues/1166

--- zkwebui/WEB-INF/src/org/adempiere/webui/component/CWindowToolbar.java
+++ zkwebui/WEB-INF/src/org/adempiere/webui/component/CWindowToolbar.java
@@ -238,7 +238,7 @@ public class CWindowToolbar extends FToolbar implements EventListener
     {
     	ToolBarButton btn = new ToolBarButton("");
         btn.setName("btn"+name);
-        btn.setImage("/images/toolbar/"+image + (embedded ? "16.png" : "24.png"));
+        btn.setImage("/images/dark/"+image + (embedded ? "16.png" : "24.png"));
         btn.setTooltiptext(Msg.getMsg(Env.getCtx(),tooltip));
         if (embedded)
         {
@@ -571,7 +571,7 @@ public class CWindowToolbar extends FToolbar implements EventListener
     {
     	this.btnLock.setPressed(locked);
 
-    	String imgURL = "/images/toolbar/"+ (this.btnLock.isPressed() ? "LockX" : "Lock") + (embedded ? "16.png" : "24.png");
+    	String imgURL = "/images/dark/"+ (this.btnLock.isPressed() ? "LockX" : "Lock") + (embedded ? "16.png" : "24.png");
 		this.btnLock.setImage(imgURL);
     }
 
