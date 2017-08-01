b9f68a8701940f8f8272efb7b178dd7c4b44c7d6
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2017-07-31 19:16:45 -0500

    #951 Generate Class Model for all tables of ADempiere release 3.9.0 https://github.com/adempiere/adempiere/issues/951

--- base/src/org/adempiere/model/X_AD_Browse_Access.java
+++ base/src/org/adempiere/model/X_AD_Browse_Access.java
@@ -149,4 +149,21 @@ public class X_AD_Browse_Access extends PO implements I_AD_Browse_Access, I_Pers
 		}
 		return false;
 	}
+
+	/** Set Immutable Universally Unique Identifier.
+		@param UUID 
+		Immutable Universally Unique Identifier
+	  */
+	public void setUUID (String UUID)
+	{
+		set_Value (COLUMNNAME_UUID, UUID);
+	}
+
+	/** Get Immutable Universally Unique Identifier.
+		@return Immutable Universally Unique Identifier
+	  */
+	public String getUUID () 
+	{
+		return (String)get_Value(COLUMNNAME_UUID);
+	}
 }
--- base/src/org/adempiere/model/X_AD_Browse_Field.java
+++ base/src/org/adempiere/model/X_AD_Browse_Field.java
@@ -819,6 +819,23 @@ public class X_AD_Browse_Field extends PO implements I_AD_Browse_Field, I_Persis
 		return ii.intValue();
 	}
 
+	/** Set Immutable Universally Unique Identifier.
+		@param UUID 
+		Immutable Universally Unique Identifier
+	  */
+	public void setUUID (String UUID)
+	{
+		set_Value (COLUMNNAME_UUID, UUID);
+	}
+
+	/** Get Immutable Universally Unique Identifier.
+		@return Immutable Universally Unique Identifier
+	  */
+	public String getUUID () 
+	{
+		return (String)get_Value(COLUMNNAME_UUID);
+	}
+
 	/** Set Value Format.
 		@param VFormat 
 		Format of the value; Can contain fixed format elements, Variables: "_lLoOaAcCa09"
