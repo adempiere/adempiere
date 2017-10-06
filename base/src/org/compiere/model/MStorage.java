a446619aea8e22763bcb8f61c8b7c84d4a359b00
Mario Calderon - Systemhaus Westfalia <mario.calderon@westfalia-it.com>
2017-10-06 16:29:56 -0500

    #1358 Exception when creating a MStorage object out of MStorage.getWarehouse() Reference to issue https://github.com/adempiere/adempiere/issues/1358 . Add column UUID to SQL.
    (cherry picked from commit fd566125daeede4f09ccbf7546037615a53c0fe1)

--- base/src/org/compiere/model/MStorage.java
+++ base/src/org/compiere/model/MStorage.java
@@ -326,7 +326,7 @@ public class MStorage extends X_M_Storage
 		//	Specific Attribute Set Instance
 		String sql = "SELECT s.M_Product_ID,s.M_Locator_ID,s.M_AttributeSetInstance_ID,"
 			+ "s.AD_Client_ID,s.AD_Org_ID,s.IsActive,s.Created,s.CreatedBy,s.Updated,s.UpdatedBy,"
-			+ "s.QtyOnHand,s.QtyReserved,s.QtyOrdered,s.DateLastInventory "
+			+ "s.QtyOnHand,s.QtyReserved,s.QtyOrdered,s.DateLastInventory,s.UUID "
 			+ "FROM M_Storage s"
 			+ " INNER JOIN M_Locator l ON (l.M_Locator_ID=s.M_Locator_ID) ";
 		if (M_Locator_ID > 0)
@@ -351,7 +351,7 @@ public class MStorage extends X_M_Storage
 		{
 			sql = "SELECT s.M_Product_ID,s.M_Locator_ID,s.M_AttributeSetInstance_ID,"
 				+ "s.AD_Client_ID,s.AD_Org_ID,s.IsActive,s.Created,s.CreatedBy,s.Updated,s.UpdatedBy,"
-				+ "s.QtyOnHand,s.QtyReserved,s.QtyOrdered,s.DateLastInventory "
+				+ "s.QtyOnHand,s.QtyReserved,s.QtyOrdered,s.DateLastInventory,s.UUID "
 				+ "FROM M_Storage s"
 				+ " INNER JOIN M_Locator l ON (l.M_Locator_ID=s.M_Locator_ID)"
 				+ " LEFT OUTER JOIN M_AttributeSetInstance asi ON (s.M_AttributeSetInstance_ID=asi.M_AttributeSetInstance_ID) ";
