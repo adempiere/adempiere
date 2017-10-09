38e8882d6fd59a82c24a93dd580a2e3ba75d4b2c
Yamel Senih <ysenih@erpcya.com>
2017-10-09 11:22:03 -0400

    #1357, Add new helper method for dependent and attributes, reference to issue: https://github.com/adempiere/adempiere/issues/1357

--- base/src/org/eevolution/model/I_HR_EmployeeDependent.java
+++ base/src/org/eevolution/model/I_HR_EmployeeDependent.java
@@ -76,21 +76,6 @@ public interface I_HR_EmployeeDependent
 	  */
 	public Timestamp getBirthday();
 
-    /** Column name C_BPartnerRelation_ID */
-    public static final String COLUMNNAME_C_BPartnerRelation_ID = "C_BPartnerRelation_ID";
-
-	/** Set Related Partner.
-	  * Related Business Partner
-	  */
-	public void setC_BPartnerRelation_ID (int C_BPartnerRelation_ID);
-
-	/** Get Related Partner.
-	  * Related Business Partner
-	  */
-	public int getC_BPartnerRelation_ID();
-
-	public org.compiere.model.I_C_BPartner getC_BPartnerRelation() throws RuntimeException;
-
     /** Column name C_BPartner_ID */
     public static final String COLUMNNAME_C_BPartner_ID = "C_BPartner_ID";
 
@@ -105,6 +90,21 @@ public interface I_HR_EmployeeDependent
 	public int getC_BPartner_ID();
 
 	public org.compiere.model.I_C_BPartner getC_BPartner() throws RuntimeException;
+
+    /** Column name C_BPartnerRelation_ID */
+    public static final String COLUMNNAME_C_BPartnerRelation_ID = "C_BPartnerRelation_ID";
+
+	/** Set Related Partner.
+	  * Related Business Partner
+	  */
+	public void setC_BPartnerRelation_ID (int C_BPartnerRelation_ID);
+
+	/** Get Related Partner.
+	  * Related Business Partner
+	  */
+	public int getC_BPartnerRelation_ID();
+
+	public org.compiere.model.I_C_BPartner getC_BPartnerRelation() throws RuntimeException;
 
     /** Column name Created */
     public static final String COLUMNNAME_Created = "Created";
@@ -121,6 +121,45 @@ public interface I_HR_EmployeeDependent
 	  * User who created this records
 	  */
 	public int getCreatedBy();
+
+    /** Column name Gender */
+    public static final String COLUMNNAME_Gender = "Gender";
+
+	/** Set Gender	  */
+	public void setGender (String Gender);
+
+	/** Get Gender	  */
+	public String getGender();
+
+    /** Column name HR_CareerLevel_ID */
+    public static final String COLUMNNAME_HR_CareerLevel_ID = "HR_CareerLevel_ID";
+
+	/** Set Career Level.
+	  * The Career Level for this position
+	  */
+	public void setHR_CareerLevel_ID (int HR_CareerLevel_ID);
+
+	/** Get Career Level.
+	  * The Career Level for this position
+	  */
+	public int getHR_CareerLevel_ID();
+
+	public org.eevolution.model.I_HR_CareerLevel getHR_CareerLevel() throws RuntimeException;
+
+    /** Column name HR_Degree_ID */
+    public static final String COLUMNNAME_HR_Degree_ID = "HR_Degree_ID";
+
+	/** Set Degree.
+	  * Degree for an Employee
+	  */
+	public void setHR_Degree_ID (int HR_Degree_ID);
+
+	/** Get Degree.
+	  * Degree for an Employee
+	  */
+	public int getHR_Degree_ID();
+
+	public org.eevolution.model.I_HR_Degree getHR_Degree() throws RuntimeException;
 
     /** Column name HR_EmployeeDependent_ID */
     public static final String COLUMNNAME_HR_EmployeeDependent_ID = "HR_EmployeeDependent_ID";
@@ -145,6 +184,36 @@ public interface I_HR_EmployeeDependent
 	public int getHR_Employee_ID();
 
 	public org.eevolution.model.I_HR_Employee getHR_Employee() throws RuntimeException;
+
+    /** Column name HR_Grade_ID */
+    public static final String COLUMNNAME_HR_Grade_ID = "HR_Grade_ID";
+
+	/** Set Grade.
+	  * Grade
+	  */
+	public void setHR_Grade_ID (int HR_Grade_ID);
+
+	/** Get Grade.
+	  * Grade
+	  */
+	public int getHR_Grade_ID();
+
+	public org.eevolution.model.I_HR_Grade getHR_Grade() throws RuntimeException;
+
+    /** Column name HR_Race_ID */
+    public static final String COLUMNNAME_HR_Race_ID = "HR_Race_ID";
+
+	/** Set Race.
+	  * Race
+	  */
+	public void setHR_Race_ID (int HR_Race_ID);
+
+	/** Get Race.
+	  * Race
+	  */
+	public int getHR_Race_ID();
+
+	public org.eevolution.model.I_HR_Race getHR_Race() throws RuntimeException;
 
     /** Column name HR_Relationship_ID */
     public static final String COLUMNNAME_HR_Relationship_ID = "HR_Relationship_ID";
@@ -174,6 +243,19 @@ public interface I_HR_EmployeeDependent
 	  */
 	public boolean isActive();
 
+    /** Column name IsActiveStudent */
+    public static final String COLUMNNAME_IsActiveStudent = "IsActiveStudent";
+
+	/** Set Active Student.
+	  * Is a Active Student
+	  */
+	public void setIsActiveStudent (boolean IsActiveStudent);
+
+	/** Get Active Student.
+	  * Is a Active Student
+	  */
+	public boolean isActiveStudent();
+
     /** Column name IsCollectCash */
     public static final String COLUMNNAME_IsCollectCash = "IsCollectCash";
 
@@ -187,6 +269,32 @@ public interface I_HR_EmployeeDependent
 	  */
 	public boolean isCollectCash();
 
+    /** Column name IsDepending */
+    public static final String COLUMNNAME_IsDepending = "IsDepending";
+
+	/** Set Depending.
+	  * Is Depending from Employee
+	  */
+	public void setIsDepending (boolean IsDepending);
+
+	/** Get Depending.
+	  * Is Depending from Employee
+	  */
+	public boolean isDepending();
+
+    /** Column name IsScholarship */
+    public static final String COLUMNNAME_IsScholarship = "IsScholarship";
+
+	/** Set Is Scholarship.
+	  * Is Dependent Scholarship
+	  */
+	public void setIsScholarship (boolean IsScholarship);
+
+	/** Get Is Scholarship.
+	  * Is Dependent Scholarship
+	  */
+	public boolean isScholarship();
+
     /** Column name Name */
     public static final String COLUMNNAME_Name = "Name";
 
@@ -199,19 +307,6 @@ public interface I_HR_EmployeeDependent
 	  * Alphanumeric identifier of the entity
 	  */
 	public String getName();
-
-    /** Column name UUID */
-    public static final String COLUMNNAME_UUID = "UUID";
-
-	/** Set Immutable Universally Unique Identifier.
-	  * Immutable Universally Unique Identifier
-	  */
-	public void setUUID (String UUID);
-
-	/** Get Immutable Universally Unique Identifier.
-	  * Immutable Universally Unique Identifier
-	  */
-	public String getUUID();
 
     /** Column name Updated */
     public static final String COLUMNNAME_Updated = "Updated";
@@ -228,4 +323,30 @@ public interface I_HR_EmployeeDependent
 	  * User who updated this records
 	  */
 	public int getUpdatedBy();
+
+    /** Column name UUID */
+    public static final String COLUMNNAME_UUID = "UUID";
+
+	/** Set Immutable Universally Unique Identifier.
+	  * Immutable Universally Unique Identifier
+	  */
+	public void setUUID (String UUID);
+
+	/** Get Immutable Universally Unique Identifier.
+	  * Immutable Universally Unique Identifier
+	  */
+	public String getUUID();
+
+    /** Column name Value */
+    public static final String COLUMNNAME_Value = "Value";
+
+	/** Set Search Key.
+	  * Search key for the record in the format required - must be unique
+	  */
+	public void setValue (String Value);
+
+	/** Get Search Key.
+	  * Search key for the record in the format required - must be unique
+	  */
+	public String getValue();
 }
