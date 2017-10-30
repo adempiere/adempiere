3abdff27724153c9d201f380a0d32faf28a722b3
Yamel Senih <ysenih@erpcya.com>
2017-10-30 04:12:10 -0400

    #1432, Add parameters for all reports in Invoice Inquiry folder, reference to issue: https://github.com/adempiere/adempiere/issues/1432

--- db/ddlutils/postgresql/views/RV_C_INVOICE_VENDORMONTH.sql
+++ db/ddlutils/postgresql/views/RV_C_INVOICE_VENDORMONTH.sql
@@ -1,7 +1,9 @@
 CREATE OR REPLACE VIEW RV_C_INVOICE_VENDORMONTH
 (AD_CLIENT_ID, AD_ORG_ID, C_BPARTNER_ID, M_PRODUCT_CATEGORY_ID, DATEINVOICED, 
  LINENETAMT, LINELISTAMT, LINELIMITAMT, LINEDISCOUNTAMT, LINEDISCOUNT, 
- LINEOVERLIMITAMT, LINEOVERLIMIT, QTYINVOICED)
+ LINEOVERLIMITAMT, LINEOVERLIMIT, QTYINVOICED, IsSOTrx,
+ C_BP_Group_ID, C_DocTypeTarget_ID, DocStatus, 
+ M_Product_Class_ID, M_Product_Group_ID, M_Product_Classification_ID)
 AS 
 SELECT il.AD_Client_ID, il.AD_Org_ID,
 	po.C_BPartner_ID, il.M_Product_Category_ID,
@@ -15,12 +17,12 @@ SELECT il.AD_Client_ID, il.AD_Org_ID,
 	SUM(LineOverLimitAmt) AS LineOverLimitAmt,
 	CASE WHEN SUM(LineNetAmt)=0 THEN 0 ELSE
 	  100-ROUND((SUM(LineNetAmt)-SUM(LineOverLimitAmt))/SUM(LineNetAmt)*100,2) END AS LineOverLimit,
-	SUM(QtyInvoiced) AS QtyInvoiced
+	SUM(QtyInvoiced) AS QtyInvoiced, IsSOTrx,
+ C_BP_Group_ID, C_DocTypeTarget_ID, DocStatus, 
+ M_Product_Class_ID, M_Product_Group_ID, M_Product_Classification_ID
 FROM RV_C_InvoiceLine il
     INNER JOIN M_Product_PO po ON (il.M_Product_ID=po.M_Product_ID)
-WHERE il.IsSOTrx='Y'
 GROUP BY il.AD_Client_ID, il.AD_Org_ID, po.C_BPartner_ID, il.M_Product_Category_ID,
-	firstOf(il.DateInvoiced, 'MM');
-
-
-
+	firstOf(il.DateInvoiced, 'MM'), IsSOTrx,
+ C_BP_Group_ID, C_DocTypeTarget_ID, DocStatus, 
+ M_Product_Class_ID, M_Product_Group_ID, M_Product_Classification_ID;
