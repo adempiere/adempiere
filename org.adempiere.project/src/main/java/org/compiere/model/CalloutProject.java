5dc3075ea400fe801788f2675d68e07542bb1ff7
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2017-11-13 13:25:59 -0600

    #1479 Add callout for Standard Phase and Standard Task and define default value https://github.com/adempiere/adempiere/issues/1479

---
+++ org.adempiere.project/src/main/java/org/compiere/model/CalloutProject.java
@@ -0,0 +1,110 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.compiere.model;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Properties;
+
+import org.adempiere.model.GridTabWrapper;
+import org.compiere.util.Env;
+
+
+/**
+ *	Project Callouts
+ *	
+ *  @author Jorg Janke
+ *  @version $Id: CalloutProject.java,v 1.3 2006/07/30 00:51:04 jjanke Exp $
+ */
+public class CalloutProject extends CalloutEngine
+{
+	/**
+	 *	Project Planned - Price + Qty.
+	 *		- called from PlannedPrice, PlannedQty
+	 *		- calculates PlannedAmt (same as Trigger)
+	 *  @param ctx context
+	 *  @param WindowNo current Window No
+	 *  @param mTab Grid Tab
+	 *  @param mField Grid Field
+	 *  @param value New Value
+	 *  @return null or error message
+	 */
+	public  String planned (Properties ctx, int WindowNo, GridTab mTab, GridField mField, Object value)
+	{
+		if (isCalloutActive() || value == null)
+			return "";
+
+		BigDecimal PlannedQty, PlannedPrice;
+		int StdPrecision = Env.getContextAsInt(ctx, WindowNo, "StdPrecision");
+
+
+		//	get values
+		PlannedQty = (BigDecimal)mTab.getValue("PlannedQty");
+		if (PlannedQty == null)
+			PlannedQty = Env.ONE;
+		PlannedPrice = ((BigDecimal)mTab.getValue("PlannedPrice"));
+		if (PlannedPrice == null)
+			PlannedPrice = Env.ZERO;
+		//
+		BigDecimal PlannedAmt = PlannedQty.multiply(PlannedPrice);
+		if (PlannedAmt.scale() > StdPrecision)
+			PlannedAmt = PlannedAmt.setScale(StdPrecision, BigDecimal.ROUND_HALF_UP);
+		//
+		log.fine("PlannedQty=" + PlannedQty + " * PlannedPrice=" + PlannedPrice + " -> PlannedAmt=" + PlannedAmt + " (Precision=" + StdPrecision+ ")");
+		mTab.setValue("PlannedAmt", PlannedAmt);
+		return "";
+	}	//	planned
+
+	public String projectPhase(Properties ctx, int windowNo, GridTab gridTab, GridField gridField, Object value)
+	{
+		Optional<I_C_ProjectPhase> standardProjectPhaseOptional = Optional.of(GridTabWrapper.create(gridTab, I_C_ProjectPhase.class));
+		standardProjectPhaseOptional.ifPresent(projectPhase -> {
+			MProjectTypePhase projectTypePhase = (MProjectTypePhase) projectPhase.getC_Phase();
+			if (projectPhase.getC_Phase_ID() > 0) {
+				projectPhase.setName(projectTypePhase.getName());
+				projectPhase.setDescription(projectTypePhase.getDescription());
+				projectPhase.setHelp(projectTypePhase.getHelp());
+				projectPhase.setPriorityRule(projectTypePhase.getPriorityRule());
+				projectPhase.setIsMilestone(projectTypePhase.isMilestone());
+				projectPhase.setDurationUnit(projectTypePhase.getDurationUnit());
+				projectPhase.setDurationEstimated(projectTypePhase.getDurationEstimated());
+				projectPhase.setM_Product_ID(projectTypePhase.getM_Product_ID());
+				projectPhase.setQty(projectTypePhase.getStandardQty());
+			}
+		});
+		return "";
+	}
+
+	public String projectTask(Properties ctx, int windowNo, GridTab gridTab, GridField gridField, Object value)
+	{
+		Optional<I_C_ProjectTask> standardProjectTaskOptional = Optional.of(GridTabWrapper.create(gridTab, I_C_ProjectTask.class));
+		standardProjectTaskOptional.ifPresent(projectTask -> {
+			MProjectTypeTask projectTypeTask = (MProjectTypeTask) projectTask.getC_Task();
+			if (projectTask.getC_Task_ID() > 0) {
+				projectTask.setName(projectTypeTask.getName());
+				projectTask.setDescription(projectTypeTask.getDescription());
+				projectTask.setHelp(projectTypeTask.getHelp());
+				projectTask.setPriorityRule(projectTypeTask.getPriorityRule());
+				projectTask.setIsMilestone(projectTypeTask.isMilestone());
+				projectTask.setDurationUnit(projectTypeTask.getDurationUnit());
+				projectTask.setDurationEstimated(projectTypeTask.getDurationEstimated());
+				projectTask.setM_Product_ID(projectTypeTask.getM_Product_ID());
+			}
+		});
+		return "";
+	}
+}	//	CalloutProject
