ef8b1b1e679aca6892354744cfb299cc60c2d63c
Yamel Senih <ysenih@erpcya.com>
2017-12-18 22:05:16 -0400

    #1529, Add new Helper method for payroll, reference to issue: https://github.com/adempiere/adempiere/issues/1529

--- org.eevolution.hr_and_payroll/src/main/java/base/org/eevolution/model/MHRProcess.java
+++ org.eevolution.hr_and_payroll/src/main/java/base/org/eevolution/model/MHRProcess.java
@@ -53,6 +53,7 @@ import org.compiere.util.DB;
 import org.compiere.util.Env;
 import org.compiere.util.Msg;
 import org.compiere.util.TimeUtil;
+import org.compiere.util.Util;
 import org.eevolution.service.HRProcessActionMsg;
 
 import javax.script.ScriptEngine;
@@ -2028,6 +2029,120 @@ public class MHRProcess extends X_HR_Process implements DocAction , DocumentReve
 		//	Get from Movement helper method
 		return MHRMovement.getLastConcept(getCtx(), conceptValue, payrollId, 
 				partnerId, breakDate);
+	} // getConcept
+	
+	/**
+	 * Get Last concept date for a employee
+	 * @param conceptValue
+	 * @param payrollValue
+	 * @param breakDate
+	 * @return
+	 */
+	public Timestamp getLastConceptDate(String conceptValue, String payrollValue, Timestamp breakDate) {
+		int payrollId;
+		if (payrollValue == null) {
+			payrollId = getHR_Payroll_ID();
+		} else {
+			MHRPayroll payroll = MHRPayroll.getByValue(getCtx(), payrollValue);
+			if(payroll == null)
+				return null;
+			//	
+			payrollId = payroll.get_ID();
+		}
+		//	Get from Movement helper method
+		return MHRMovement.getLastConceptDate(getCtx(), conceptValue, payrollId, 
+				partnerId, breakDate);
+	} // getConcept
+	
+	/**
+	 * Get Last concept String for a employee
+	 * @param conceptValue
+	 * @param payrollValue
+	 * @param breakDate
+	 * @return
+	 */
+	public String getLastConceptString(String conceptValue, String payrollValue, Timestamp breakDate) {
+		int payrollId;
+		if (payrollValue == null) {
+			payrollId = getHR_Payroll_ID();
+		} else {
+			MHRPayroll payroll = MHRPayroll.getByValue(getCtx(), payrollValue);
+			if(payroll == null)
+				return null;
+			//	
+			payrollId = payroll.get_ID();
+		}
+		//	Get from Movement helper method
+		MHRMovement lastMovement = MHRMovement.getLastMovement(getCtx(), conceptValue, payrollId, 
+				partnerId, breakDate);
+		//	
+		if(lastMovement == null) {
+			return null;
+		}
+		//	For all
+		if(!Util.isEmpty(lastMovement.getTextMsg())) {
+			return lastMovement.getTextMsg();
+		}
+		//	For Description (optional)
+		return lastMovement.getDescription();
+	} // getConcept
+	
+	/**
+	 * Get Last concept Valid From for a employee
+	 * @param conceptValue
+	 * @param payrollValue
+	 * @param breakDate
+	 * @return
+	 */
+	public Timestamp getLastConceptValidFrom(String conceptValue, String payrollValue, Timestamp breakDate) {
+		int payrollId;
+		if (payrollValue == null) {
+			payrollId = getHR_Payroll_ID();
+		} else {
+			MHRPayroll payroll = MHRPayroll.getByValue(getCtx(), payrollValue);
+			if(payroll == null)
+				return null;
+			//	
+			payrollId = payroll.get_ID();
+		}
+		//	Get from Movement helper method
+		MHRMovement lastMovement = MHRMovement.getLastMovement(getCtx(), conceptValue, payrollId, 
+				partnerId, breakDate);
+		//	
+		if(lastMovement == null) {
+			return null;
+		}
+		//	Default
+		return lastMovement.getValidFrom();
+	} // getConcept
+	
+	/**
+	 * Get Last concept Valid To for a employee
+	 * @param conceptValue
+	 * @param payrollValue
+	 * @param breakDate
+	 * @return
+	 */
+	public Timestamp getLastConceptValidTo(String conceptValue, String payrollValue, Timestamp breakDate) {
+		int payrollId;
+		if (payrollValue == null) {
+			payrollId = getHR_Payroll_ID();
+		} else {
+			MHRPayroll payroll = MHRPayroll.getByValue(getCtx(), payrollValue);
+			if(payroll == null)
+				return null;
+			//	
+			payrollId = payroll.get_ID();
+		}
+		//	Get from Movement helper method
+		MHRMovement lastMovement = MHRMovement.getLastMovement(getCtx(), conceptValue, payrollId, 
+				partnerId, breakDate);
+		//	
+		if(lastMovement == null) {
+			return null;
+		}
+		//	Default
+		return lastMovement.getValidTo();
 	} // getConcept
 	
 	/**
