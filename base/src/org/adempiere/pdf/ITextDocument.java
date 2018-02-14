d766360ee99772dc2f058d36abb9558bad933915
homebeaver <eugen.hanussek@klst.com>
2018-02-14 23:03:34 +0100

    issue #1567 use ITextDocument, not Document

--- base/src/org/adempiere/pdf/ITextDocument.java
+++ base/src/org/adempiere/pdf/ITextDocument.java
@@ -54,17 +54,17 @@ import io.konik.harness.FileExtractor;
  *   copy javax.inject-1.jar to tools/lib 
  *   copy kernel-7.1.1.jar , pdfa-7.1.1.jar , io-7.1.1.jar , layout-7.1.1.jar to tools/lib 
  *  
- * TODO:
  * Referenzen zu Document:
  *  base org.compiere.print.ArchiveEngine 2x
- *                          ReportEngine 
+ *                          ReportEngine + org.spin.util.ExportFormatPDF
  *  client org.compiere.apps.Attachment  
  *                          .form.ArchiveViewer 
  *         org.compiere.print.Viewer
- *  zkwebui org.adempiere.webui.windoe.ZkReportViwer
+ *  zkwebui org.adempiere.webui.window.ZkReportViwer
  *        
+ * TODO:
  * wo werden com.lowagie.* Packages verwendet?
- *  org.adempiere.pdf.Document
+ *  org.adempiere.pdf.Document - deprecated
  *  org.adempiere.pdf.SmjPdfReport
  *  org.compiere.grid.VPanel
  *  org.adempiere.webui.apps.AEnv
