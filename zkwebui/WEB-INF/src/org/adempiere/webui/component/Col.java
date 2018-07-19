9fed9beb0f7518c650e25712103b78126255a7e7
Raul Muñoz <rmunoz@erpcya.com>
2018-07-19 09:34:58 -0500

    Changed in license
    (cherry picked from commit f7f717b2923ea328a06b5508739e7bafb3619057)

--- zkwebui/WEB-INF/src/org/adempiere/webui/component/Col.java
+++ zkwebui/WEB-INF/src/org/adempiere/webui/component/Col.java
@@ -2,11 +2,11 @@
  * Product: Posterita Ajax UI 												  *
  * Copyright (C) 2007 Posterita Ltd.  All Rights Reserved.                    *
  * This program is free software; you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
- * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
- * See the GNU General Public License for more details.                       *
+ * under the terms version 2 or later of the GNU General Public License as    *
+ * published by the Free Software Foundation. This program is distributed in  *
+ * the hope that it will be useful, but WITHOUT ANY WARRANTY; without even    *
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR        *
+ * PURPOSE. See the GNU General Public License for more details.              *
  * You should have received a copy of the GNU General Public License along    *
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
@@ -80,6 +80,7 @@ public class Col extends Div {
     		return false;
     	}
     }
+
     public boolean setEditor(WEditor comp) {
     	if(setComponent(comp.getComponent())) {	
     		editor = comp;
@@ -88,11 +89,9 @@ public class Col extends Div {
     	else {
     		return false;
     	}
-    }
-    
+    }    
 
 	/**
-	 * @param component
 	 * @return
 	 */
 	public void createAnchorInput() {
