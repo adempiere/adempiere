a271a42b2bf02cf65a8a3f627f97f1cc7299e7d6
homebeaver <eugen.hanussek@klst.com>
2018-07-31 21:09:40 +0200

    adempiere licence header
    refs https://github.com/adempiere/adempiere/pull/1576
    
    (cherry picked from commit e5dcaba20c440315a6d59121f047ae07b54e5161)

--- base/src/org/adempiere/pdf/ITextDocument.java
+++ base/src/org/adempiere/pdf/ITextDocument.java
@@ -1,18 +1,17 @@
-/**
- * Copyright (C) 2003-2018, http://www.klst.com/
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * Created by eugen.hanussek@klst.com https://github.com/homebeaver
- */
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2006 Adempiere, Inc. All Rights Reserved.               *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+
 package org.adempiere.pdf;
 
 import java.awt.Graphics2D;
@@ -74,6 +73,8 @@ import io.konik.harness.FileExtractor;
  * create a pdf from this awt report representation, method #writePDF(...). 
  * This class is a singleton and provides access to invoiceAppender and invoiceExtractor (itext-5)
  * and itext7 - for details @see https://github.com/adempiere/adempiere/issues/1567
+ * 
+ * @author eugen.hanussek@klst.com https://github.com/homebeaver
  */
 @Named
 @Singleton
