5140baec40b89173c1d4ef6da9cbbf6115bb5422
Michael McKay <michael.mckay@mckayerp.com>
2018-10-04 11:40:52 -0400

    Fix for #2053. Add FileName parameter to MigrationLoader.
    (cherry picked from commit 4cb342379e4c0560dc9168ab64a721ceea160be6)

--- base/src/org/compiere/process/MigrationFromXMLAbstract.java
+++ base/src/org/compiere/process/MigrationFromXMLAbstract.java
@@ -32,6 +32,8 @@ public abstract class MigrationFromXMLAbstract extends SvrProcess {
 	private static final int ID_FOR_PROCESS = 53175;
 	/**	Parameter Name for File Path or Name	*/
 	public static final String FILEPATHORNAME = "FilePathOrName";
+	/**	Parameter Name for File Name	*/  // Parameter changed name in DB
+	public static final String FILENAME = "FileName";
 	/**	Parameter Name for Apply	*/
 	public static final String APPLY = "Apply";
 	/**	Parameter Name for Force	*/
@@ -47,7 +49,7 @@ public abstract class MigrationFromXMLAbstract extends SvrProcess {
 	protected void prepare() {
 		filePathOrName = getParameterAsString(FILEPATHORNAME);
 		if (filePathOrName == null)
-			filePathOrName = getParameterAsString("FileName");
+			filePathOrName = getParameterAsString(FILENAME);
 		isApply = getParameterAsBoolean(APPLY);
 		isForce = getParameterAsBoolean(ISFORCE);
 	}
