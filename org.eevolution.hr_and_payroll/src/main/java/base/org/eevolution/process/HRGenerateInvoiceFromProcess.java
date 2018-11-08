ae19d487808b9b40c28fe024ea61d85c284b2094
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2018-11-07 20:56:43 -0600

    #2124 Enhancement SB for Payroll Create Invoice Business Partner https://github.com/adempiere/adempiere/issues/2124 -Adding filter movement -Set movement where concept is Invoiced -Add Partner Location -Add Doc Action for complete the documents after that was generated -improve the description for invoice and lines -Using the account dimension from payroll movement -Enable generate AR and AP Invoice include AR and AP Credit Note
    (cherry picked from commit 10f26ade99463efaf10a617e3a71417e5202617c)

--- org.eevolution.hr_and_payroll/src/main/java/base/org/eevolution/process/HRGenerateInvoiceFromProcess.java
+++ org.eevolution.hr_and_payroll/src/main/java/base/org/eevolution/process/HRGenerateInvoiceFromProcess.java
@@ -18,14 +18,17 @@ package org.eevolution.process;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 
 import org.compiere.apps.AEnv;
 import org.compiere.apps.AWindow;
+import org.compiere.model.I_C_BP_Relation;
 import org.compiere.model.I_C_BPartner_Location;
 import org.compiere.model.MBPartner;
 import org.compiere.model.MBPartnerLocation;
+import org.compiere.model.MDocType;
 import org.compiere.model.MInvoice;
 import org.compiere.model.MInvoiceLine;
 import org.compiere.model.MPaymentTerm;
@@ -36,15 +39,15 @@ import org.compiere.model.Query;
 import org.compiere.process.DocAction;
 import org.compiere.util.Env;
 import org.compiere.util.Msg;
-import org.eevolution.model.MHREmployee;
-import org.eevolution.model.MHRProcess;
+import org.eevolution.model.I_HR_Employee;
+import org.eevolution.model.I_HR_Process;
 import org.eevolution.model.MHRMovement;
 
 /** Generated Process for (Create Invoice for Business Partner)
- *  @author ADempiere (generated) 
- *  @version Release 3.9.0
+ *  @author Susanne Calderon, Westfalia
+ *  @author victor.perez@e-evolution.com, www-e-evolution.com
  */
-public class HR_CreateInvoiceBPartner extends HR_CreateInvoiceBPartnerAbstract
+public class HRGenerateInvoiceFromProcess extends HRGenerateInvoiceFromProcessAbstract
 {
 	@Override
 	protected void prepare()
@@ -52,68 +55,82 @@ public class HR_CreateInvoiceBPartner extends HR_CreateInvoiceBPartnerAbstract
 		super.prepare();
 	}
 
-	private 	String whereClauseWindow = "c_invoice_ID in (";
 	private 	int count = 0;
+	private HashMap <Integer, MInvoice> invoices  = new HashMap<>();
 
 	@Override
 	protected String doIt() throws Exception
 	{
 		MBPartner partner = null;
-		MInvoice invoice = null;
-		int lastPartnerId = 0;
-		if (getBusinessPartnerId() != 0)
-			partner = new MBPartner(getCtx(), getBusinessPartnerId(), get_TrxName());
-		List<MHRMovement> hrMovements = (List<MHRMovement>) getInstancesForSelection(get_TrxName());
-		for (MHRMovement hrmovement : hrMovements) {
-			if (getBusinessPartnerId() ==0)        	
-				partner = new MBPartner(getCtx(), hrmovement.getC_BPartner_ID(),get_TrxName());
+		if (getBPartnerId() > 0)
+			partner = new MBPartner(getCtx(), getBPartnerId(), get_TrxName());
 
-			MHREmployee employee = MHREmployee.getActiveEmployee(getCtx(), partner.getC_BPartner_ID(), get_TrxName());
-			if (getBusinessPartnerId() == 0 && hrmovement.getC_BPartner_ID() != lastPartnerId) {
+		List<MHRMovement> payrollMovements = (List<MHRMovement>) getInstancesForSelection(get_TrxName());
+		for (MHRMovement payrollMovement : payrollMovements) {
+			if (getBPartnerId() <= 0)
+				partner = MBPartner.get(getCtx(), payrollMovement.getC_BPartner_ID());
 
-				lastPartnerId = hrmovement.getC_BPartner_ID();
-				invoice = createInvoice((MHRProcess)hrmovement.getHR_Process(), partner, employee, getDocumentTypeId(), getDateInvoiced());
-				if (invoice == null)
-					continue;
+			if (payrollMovement.getC_BP_Relation_ID() > 0) {
+				I_C_BP_Relation partnerRelation = payrollMovement.getC_BP_Relation();
+				partner = MBPartner.get(getCtx(), partnerRelation.getC_BPartnerRelation_ID());
 			}
-			else if (invoice == null)
+
+			I_HR_Employee employee = payrollMovement.getHR_Employee();
+			if (!invoices.containsKey(partner.get_ID()))
 			{
-				invoice = createInvoice((MHRProcess)hrmovement.getHR_Process(), partner, employee, 
-						getDocumentTypeId(), getDateInvoiced());
+				MInvoice invoice = createInvoice(payrollMovement.getHR_Process(), partner, employee, getDocTypeId(), getDateInvoiced());
+				invoices.put(invoice.getC_BPartner_ID(), invoice);
 			}
 
-			if (invoice != null)
-				createInvoiceLine(invoice, employee, hrmovement, getChargeID());
+			createInvoiceLine(invoices.get(partner.get_ID()), employee, payrollMovement, getChargeId());
+
 		}
-		whereClauseWindow = whereClauseWindow.substring(0, whereClauseWindow.length() -1) + ")";
-		MQuery query = new MQuery("");
-		MTable table = new MTable(getCtx(), MInvoice.Table_ID, get_TrxName());
-		query.addRestriction(whereClauseWindow);
-		query.setRecordCount(count);
-		int AD_WindowNo = 0;		
-		AD_WindowNo = table.getPO_Window_ID();
 
-		int ad_session_ID  = Env.getContextAsInt(getCtx(), "AD_Session_ID");
-		MSession session = new MSession(getCtx(), ad_session_ID, null);
+		if (DocAction.ACTION_Complete.equals(getDocAction()))
+		{
+			invoices.entrySet().forEach( entry -> {
+				MInvoice invoice = entry.getValue();
+				invoice.processIt(getDocAction());
+				invoice.saveEx();
+			});
+		}
 
+		int sessionId  = Env.getContextAsInt(getCtx(), "AD_Session_ID");
+		MSession session = new MSession(getCtx(), sessionId, null);
 		if (session.getWebSession() == null ||session.getWebSession().length() == 0)
 		{
-			commitEx();
-			zoom (AD_WindowNo, query);
-			return "";
+			if (invoices.size() > 0) {
+				commitEx();
+				StringBuilder whereClause = new StringBuilder();
+				invoices.entrySet().forEach(entry -> whereClause.append(entry.getKey()).append(","));
+				StringBuilder whereClauseWindow=new StringBuilder("C_Invoice_ID IN (");
+				whereClauseWindow.append(whereClause.substring(0,whereClause.length()-1)).append(")");
+				MTable table = new MTable(getCtx(), MInvoice.Table_ID, get_TrxName());
+				MQuery query = new MQuery();
+				query.addRestriction(whereClauseWindow.toString());
+				query.setRecordCount(count);
+				int windowId = table.getPO_Window_ID();
+				zoom (windowId, query);
+			}
 		}
-
 		return "@OK@";
 	}
 	
-	 private MInvoice createInvoice(MHRProcess process, MBPartner partner, MHREmployee employee, int docTypeId, Timestamp dateInvoice) {
-		 MBPartnerLocation partnerLocation = getLocationBill(partner);
-		 if (partnerLocation == null) {
-			 log.log(Level.SEVERE, " @C_BPartner_Location_ID@ @NotFound@ : " + partner.getName());
-			 addLog(0, process.getDateAcct(), null, "@Bill_Location_ID@ @NotFound@ "
-					 + process.getDocumentNo() + " "
-					 + partner.getName());
-			 return null;
+	 private MInvoice createInvoice(I_HR_Process process, MBPartner partner, I_HR_Employee employee, int docTypeId, Timestamp dateInvoice) {
+		int partnerLocationId = 0;
+		if (getBPartnerLocationId() > 0) {
+			partnerLocationId = getBPartnerLocationId();
+		}
+		else{
+			 MBPartnerLocation partnerLocation = getLocationBill(partner);
+			 if (partnerLocation == null) {
+				 log.log(Level.SEVERE, " @C_BPartner_Location_ID@ @NotFound@ : " + partner.getName());
+				 addLog(0, process.getDateAcct(), null, "@Bill_Location_ID@ @NotFound@ "
+						 + process.getDocumentNo() + " "
+						 + partner.getName());
+				 return null;
+			 }
+			 partnerLocationId = partnerLocation.get_ID();
 		 }
 		 String paymentRule = employee != null?  employee.getPaymentRule():null;
 		 if (paymentRule == null || paymentRule.isEmpty())
@@ -129,19 +146,23 @@ public class HR_CreateInvoiceBPartner extends HR_CreateInvoiceBPartnerAbstract
 			 return null;
 		 }
 
-
 		 log.info("New Invoice for ");
-		 MInvoice invoice = new MInvoice(process.getCtx(), 0, get_TrxName());
+		 MDocType docType = MDocType.get(getCtx(), docTypeId);
+		 StringBuilder invoiceDescription = new StringBuilder();
+		 invoiceDescription.append(Msg.parseTranslation(getCtx(), "@HR_Process_ID@ " + process.getName()
+				 + " @DocumentNo@ " + process.getDocumentNo() + " @HR_Employee_ID@ " + employee.getName()));
+		 MInvoice invoice = new MInvoice(getCtx(), 0, get_TrxName());
 		 invoice.setAD_Org_ID(process.getAD_Org_ID());
-		 invoice.setIsSOTrx(false);
+		 invoice.setIsSOTrx(docType.isSOTrx());
 		 invoice.setPaymentRule(paymentRule);
 		 invoice.setC_DocTypeTarget_ID(docTypeId);
 		 invoice.setC_DocType_ID(docTypeId);
-		 invoice.setDescription(Msg.parseTranslation(process.getCtx(), "@HR_Process_ID@ " + process.getName() + " @DocumentNo@ " + process.getDocumentNo()));
+		 invoice.setDescription(invoiceDescription.toString());
 		 invoice.setDateOrdered(dateInvoice);
 		 invoice.setDateInvoiced(dateInvoice);
 		 invoice.setDateAcct(dateInvoice);
 		 invoice.setBPartner(partner);
+		 invoice.setC_BPartner_Location_ID(partnerLocationId);
 
 		 MPaymentTerm paymentTerm = MPaymentTerm.getPaymentTermByDefault(getCtx(), partner.get_TrxName());
 		 if (paymentTerm != null)
@@ -152,9 +173,8 @@ public class HR_CreateInvoiceBPartner extends HR_CreateInvoiceBPartnerAbstract
 
 		 invoice.setDocStatus(DocAction.STATUS_Drafted);
 		 invoice.setDocAction(DocAction.ACTION_None);
-		 invoice.setSalesRep_ID(Env.getAD_User_ID(process.getCtx()));
-		 invoice.saveEx();			
-		 whereClauseWindow = whereClauseWindow + invoice.getC_Invoice_ID() + ",";
+		 invoice.setSalesRep_ID(getAD_User_ID());
+		 invoice.saveEx();
 		 count = count ++;
 		 addLog(0, invoice.getDateInvoiced(), invoice.getGrandTotal(), "@C_Invoice_ID@ " + invoice.getDocumentNo() + " @C_BPartner_ID@  @TaxId@ " + invoice.getC_BPartner().getValue() + " @Name@ " + invoice.getC_BPartner().getName());
 		 return invoice;
@@ -168,21 +188,28 @@ public class HR_CreateInvoiceBPartner extends HR_CreateInvoiceBPartnerAbstract
 	     * @param chargeId
 	     * @return
 	     */
-	    private MInvoiceLine createInvoiceLine(MInvoice invoice, MHREmployee employee, MHRMovement movement, int chargeId) {
+	    private MInvoiceLine createInvoiceLine(MInvoice invoice, I_HR_Employee employee, MHRMovement movement, int chargeId) {
+			StringBuilder invoiceLineDescription = new StringBuilder();
+			invoiceLineDescription.append(Msg.parseTranslation(invoice.getCtx() , "@HR_Employee_ID@ " + employee.getName()
+					+  " @HR_Concept_ID@ " + movement.getHR_Concept().getName()));
 	        MInvoiceLine invoiceLine = new MInvoiceLine(invoice);
 	        invoiceLine.setC_Charge_ID(chargeId);
 	        invoiceLine.setQty(BigDecimal.ONE); //
-	        invoiceLine.setDescription(movement.getHR_Concept().getName());
+	        invoiceLine.setDescription(invoiceLineDescription.toString());
 	        invoiceLine.setC_Activity_ID(movement.getC_Activity_ID());
+	        invoiceLine.setC_Campaign_ID(movement.getC_Campaign_ID());
+	        invoiceLine.setAD_OrgTrx_ID(movement.getAD_OrgTrx_ID());
+	        invoiceLine.setC_Project_ID(movement.getC_Project_ID());
+	        invoiceLine.setC_ProjectPhase_ID(movement.getC_ProjectPhase_ID());
+	        invoiceLine.setC_ProjectTask_ID(movement.getC_ProjectTask_ID());
 	        invoiceLine.setUser1_ID(movement.getUser1_ID());
 	        invoiceLine.setUser2_ID(movement.getUser2_ID());
 	        invoiceLine.setUser3_ID(movement.getUser3_ID());
 	        invoiceLine.setUser4_ID(movement.getUser4_ID());
 	        invoiceLine.setPrice(movement.getAmount());
 	        invoiceLine.setTax();
-	        
 	        invoiceLine.saveEx();
-	        addLog(invoiceLine.getLine(), invoice.getDateInvoiced(), invoiceLine.getLineNetAmt(), movement.getHR_Concept().getName());
+	        addLog(invoiceLine.getLine(), invoice.getDateInvoiced(), invoiceLine.getLineNetAmt(), invoiceLineDescription.toString());
 
 	        movement.setC_InvoiceLine_ID(invoiceLine.getC_InvoiceLine_ID());
 	        movement.saveEx();
