35c2d482dc839487c0fa3998112ffa9877568997
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2018-12-06 11:13:32 -0600

    #2161 Setup ADempiere 391 Release https://github.com/adempiere/adempiere/issues/2161
    (cherry picked from commit f3eac5edecc1ad16ab0bac7cfe19c947dcdb25e6)

--- base/src/org/compiere/Adempiere.java
+++ base/src/org/compiere/Adempiere.java
@@ -67,9 +67,9 @@ public final class Adempiere
 	// the releases will have a suffix (a) for alpha - (b) for beta - (t) for trunk - (s) for stable - and (LTS) for long term support
 	static public String	MAIN_VERSION	= "Release 3.9.1";
 	/** Detail Version as date      Used for Client/Server		*/
-	static public String	DATE_VERSION	= "2018-11-30";
+	static public String	DATE_VERSION	= "2019-01-01";
 	/** Database Version as date    Compared with AD_System		*/
-	static public String	DB_VERSION		= "2018-11-30";
+	static public String	DB_VERSION		= "2019-01-01";
 
 	/** Product Name            */
 	static public final String	NAME 			= "ADempiere\u00AE";
@@ -91,7 +91,7 @@ public final class Adempiere
 	/** Subtitle                */
 	static public final String	SUB_TITLE		= "Smart Suite ERP,CRM and SCM";
 	static public final String	ADEMPIERE_R		= "Powered by ADempiere ERP\u00AE";
-	static public final String	COPYRIGHT		= "\u00A9 1999-2018 ADempiere\u00AE";
+	static public final String	COPYRIGHT		= "\u00A9 1999-2019 ADempiere\u00AE";
 
 	static private String		s_ImplementationVersion = null;
 	static private String		s_ImplementationVendor = null;
