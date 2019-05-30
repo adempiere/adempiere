29058752d17808ca61697abff98fa5b05320482c
Yamel Senih <yamelsenih@gmail.com>
2019-05-29 23:19:25 -0430

    Add social media infor for user contact

---
+++ base/src/org/spin/model/MADUserSocialMedia.java
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * Copyright (C) 2003-2015 E.R.P. Consultores y Asociados, C.A.               *
+ * All Rights Reserved.                                                       *
+ * Contributor(s): Yamel Senih www.erpya.com                                  *
+ *****************************************************************************/
+package org.spin.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+
+/**
+ * @author Yamel Senih, ySenih@erpya.com, ERPCyA http://www.erpya.com
+ *		Add for handle social media for user
+ */
+public class MADUserSocialMedia extends X_AD_UserSocialMedia {
+
+	public MADUserSocialMedia(Properties ctx, int AD_UserSocialMedia_ID, String trxName) {
+		super(ctx, AD_UserSocialMedia_ID, trxName);
+	}
+
+	public MADUserSocialMedia(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7151089570474994623L;
+
+}
