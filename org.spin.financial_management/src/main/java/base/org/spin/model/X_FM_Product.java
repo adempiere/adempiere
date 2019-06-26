0a125337b1375809c49403fe0753cb767cf1cca2
Yamel Senih <yamelsenih@gmail.com>
2019-06-25 19:55:43 -0430

    Add Financial Management integration for ADempiere Core

---
+++ org.spin.financial_management/src/main/java/base/org/spin/model/X_FM_Product.java
@@ -0,0 +1,209 @@
+/******************************************************************************
+ * Product: ADempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 2006-2017 ADempiere Foundation, All Rights Reserved.         *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * or (at your option) any later version.										*
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * or via info@adempiere.net or http://www.adempiere.net/license.html         *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.spin.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+import org.compiere.model.*;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Model for FM_Product
+ *  @author Adempiere (generated) 
+ *  @version Release 3.9.0 - $Id$ */
+public class X_FM_Product extends PO implements I_FM_Product, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20180320L;
+
+    /** Standard Constructor */
+    public X_FM_Product (Properties ctx, int FM_Product_ID, String trxName)
+    {
+      super (ctx, FM_Product_ID, trxName);
+      /** if (FM_Product_ID == 0)
+        {
+			setFM_ProductCategory_ID (0);
+			setFM_Product_ID (0);
+			setM_Product_ID (0);
+			setName (null);
+			setValue (null);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_FM_Product (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 3 - Client - Org 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_FM_Product[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	public org.spin.model.I_FM_ProductCategory getFM_ProductCategory() throws RuntimeException
+    {
+		return (org.spin.model.I_FM_ProductCategory)MTable.get(getCtx(), org.spin.model.I_FM_ProductCategory.Table_Name)
+			.getPO(getFM_ProductCategory_ID(), get_TrxName());	}
+
+	/** Set Financial Product Category.
+		@param FM_ProductCategory_ID Financial Product Category	  */
+	public void setFM_ProductCategory_ID (int FM_ProductCategory_ID)
+	{
+		if (FM_ProductCategory_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_FM_ProductCategory_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_FM_ProductCategory_ID, Integer.valueOf(FM_ProductCategory_ID));
+	}
+
+	/** Get Financial Product Category.
+		@return Financial Product Category	  */
+	public int getFM_ProductCategory_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_FM_ProductCategory_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Financial Product.
+		@param FM_Product_ID Financial Product	  */
+	public void setFM_Product_ID (int FM_Product_ID)
+	{
+		if (FM_Product_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_FM_Product_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_FM_Product_ID, Integer.valueOf(FM_Product_ID));
+	}
+
+	/** Get Financial Product.
+		@return Financial Product	  */
+	public int getFM_Product_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_FM_Product_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public org.compiere.model.I_M_Product getM_Product() throws RuntimeException
+    {
+		return (org.compiere.model.I_M_Product)MTable.get(getCtx(), org.compiere.model.I_M_Product.Table_Name)
+			.getPO(getM_Product_ID(), get_TrxName());	}
+
+	/** Set Product.
+		@param M_Product_ID 
+		Product, Service, Item
+	  */
+	public void setM_Product_ID (int M_Product_ID)
+	{
+		if (M_Product_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_M_Product_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_M_Product_ID, Integer.valueOf(M_Product_ID));
+	}
+
+	/** Get Product.
+		@return Product, Service, Item
+	  */
+	public int getM_Product_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_M_Product_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Name.
+		@param Name 
+		Alphanumeric identifier of the entity
+	  */
+	public void setName (String Name)
+	{
+		set_Value (COLUMNNAME_Name, Name);
+	}
+
+	/** Get Name.
+		@return Alphanumeric identifier of the entity
+	  */
+	public String getName () 
+	{
+		return (String)get_Value(COLUMNNAME_Name);
+	}
+
+	/** Set Immutable Universally Unique Identifier.
+		@param UUID 
+		Immutable Universally Unique Identifier
+	  */
+	public void setUUID (String UUID)
+	{
+		set_Value (COLUMNNAME_UUID, UUID);
+	}
+
+	/** Get Immutable Universally Unique Identifier.
+		@return Immutable Universally Unique Identifier
+	  */
+	public String getUUID () 
+	{
+		return (String)get_Value(COLUMNNAME_UUID);
+	}
+
+	/** Set Search Key.
+		@param Value 
+		Search key for the record in the format required - must be unique
+	  */
+	public void setValue (String Value)
+	{
+		set_Value (COLUMNNAME_Value, Value);
+	}
+
+	/** Get Search Key.
+		@return Search key for the record in the format required - must be unique
+	  */
+	public String getValue () 
+	{
+		return (String)get_Value(COLUMNNAME_Value);
+	}
+
+    /** Get Record ID/ColumnName
+        @return ID/ColumnName pair
+      */
+    public KeyNamePair getKeyNamePair() 
+    {
+        return new KeyNamePair(get_ID(), getValue());
+    }
+}
