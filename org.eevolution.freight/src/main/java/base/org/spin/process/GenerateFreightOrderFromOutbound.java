16ea99f0a7dd24e3df1c29d0d3c2fb58c07f1903
Yamel Senih <ysenih@erpya.com>
2019-09-01 14:20:03 -0400

    Add Outbound reference

--- org.eevolution.freight/src/main/java/base/org/spin/process/GenerateFreightOrderFromOutbound.java
+++ org.eevolution.freight/src/main/java/base/org/spin/process/GenerateFreightOrderFromOutbound.java
@@ -19,6 +19,9 @@ package org.spin.process;
 
 import org.adempiere.exceptions.AdempiereException;
 import org.compiere.model.MClientInfo;
+import org.compiere.model.MProduct;
+import org.eevolution.model.I_DD_Freight;
+import org.eevolution.model.I_WM_InOutBoundLine;
 import org.eevolution.model.MDDFreight;
 import org.eevolution.model.MDDFreightLine;
 import org.eevolution.model.MWMInOutBound;
@@ -53,31 +56,44 @@ public class GenerateFreightOrderFromOutbound extends GenerateFreightOrderFromOu
 		freightOrder.saveEx();
 		//	Add lines from Outbound
 		MWMInOutBound outbound = new MWMInOutBound(getCtx(), getInOutBoundId(), get_TrxName());
-		int lineNo = 10;
-		MDDFreightLine line = new MDDFreightLine(getCtx(), 0, get_TrxName());
-		line.setDD_Freight_ID(freightOrder.getDD_Freight_ID());
-		line.setLine(lineNo);
-		line.setWeight(outbound.getWeight());
-		line.setVolume(outbound.getVolume());
-		//	Set from client
-		MClientInfo clientInfo = MClientInfo.get(getCtx());
-		//	Weight
-		if(clientInfo.getC_UOM_Weight_ID() <= 0) {
-			throw new AdempiereException("@C_UOM_Weight_ID@ @NotFound@ @SeeClientInfoConfig@");
-		}
-		//	Volume
-		if(clientInfo.getC_UOM_Volume_ID() <= 0) {
-			throw new AdempiereException("@C_UOM_Volume_ID@ @NotFound@ @SeeClientInfoConfig@");
-		}
-		//	Set values
-		line.setWeight_UOM_ID(clientInfo.getC_UOM_Weight_ID());
-		line.setVolume_UOM_ID(clientInfo.getC_UOM_Volume_ID());
-		line.setM_Freight_ID(getFreightId());
-		line.setFreightAmt(getFreightAmt());
-		line.saveEx();
+		outbound.getLines(true, I_WM_InOutBoundLine.COLUMNNAME_Line)
+			.stream()
+			.forEach(outboundLine -> {
+				MDDFreightLine line = new MDDFreightLine(getCtx(), 0, get_TrxName());
+				line.setDD_Freight_ID(freightOrder.getDD_Freight_ID());
+				line.setLine(outboundLine.getLine());
+				MProduct product = MProduct.get(getCtx(), outboundLine.getM_Product_ID());
+				if(product.getWeight() != null) {
+					line.setWeight(product.getWeight());
+				}
+				if(product.getVolume() != null) {
+					line.setVolume(product.getVolume());
+				}
+				//	Set from client
+				MClientInfo clientInfo = MClientInfo.get(getCtx());
+				//	Weight
+				if(clientInfo.getC_UOM_Weight_ID() <= 0) {
+					throw new AdempiereException("@C_UOM_Weight_ID@ @NotFound@ @SeeClientInfoConfig@");
+				}
+				//	Volume
+				if(clientInfo.getC_UOM_Volume_ID() <= 0) {
+					throw new AdempiereException("@C_UOM_Volume_ID@ @NotFound@ @SeeClientInfoConfig@");
+				}
+				//	Set values
+				line.setWeight_UOM_ID(clientInfo.getC_UOM_Weight_ID());
+				line.setVolume_UOM_ID(clientInfo.getC_UOM_Volume_ID());
+				line.setM_Freight_ID(getFreightId());
+				line.setFreightAmt(getFreightAmt());
+				line.setM_Product_ID(outboundLine.getM_Product_ID());
+				line.setWM_InOutBoundLine_ID(outboundLine.getWM_InOutBoundLine_ID());
+				line.saveEx();
+			});
 		//	Complete
 		freightOrder.processIt(getDocAction());
 		freightOrder.saveEx();
+		//	Add to log
+		addLog(freightOrder.getDD_Freight_ID(), freightOrder.getDateDoc(), null, "@Created@");
+		openResult(I_DD_Freight.Table_Name);
 		//	
 		return "@DD_Freight_ID@ @Created@: " + freightOrder.getDocumentInfo();
 	}
