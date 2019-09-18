565b835e8fd51b79b37a5931a2aa73522018676e
Yamel Senih <ysenih@erpya.com>
2019-09-18 19:44:35 -0400

    Set document no for payment from payment selection (#2803)
    * Set document no for payment from payment selection
    
    * Add old compatibility with overwrite documentNo for check payment rule

--- base/src/org/compiere/model/MPaySelectionCheck.java
+++ base/src/org/compiere/model/MPaySelectionCheck.java
@@ -303,14 +303,14 @@ public final class MPaySelectionCheck extends X_C_PaySelectionCheck
 			//	Existing Payment
 			if (paySelectionCheck.getC_Payment_ID() != 0
 					&& (payment.getDocStatus().equals(DocAction.STATUS_Completed)
-								|| payment.getDocStatus().equals(DocAction.STATUS_Closed)))
-			{
+								|| payment.getDocStatus().equals(DocAction.STATUS_Closed))) {
 				//	Update check number
-				if (paySelectionCheck.getPaymentRule().equals(PAYMENTRULE_Check))
-				{
+				if (paySelectionCheck.getPaymentRule().equals(PAYMENTRULE_Check)) {
 					payment.setCheckNo(paySelectionCheck.getDocumentNo());
-					payment.saveEx();
+				} else {
+					payment.setDocumentNo(paySelectionCheck.getDocumentNo());
 				}
+				payment.saveEx();
 			} else {	//	New Payment
 				I_C_PaySelection paySelection =  paySelectionCheck.getC_PaySelection();
 				MDocType documentType = MDocType.get(paySelectionCheck.getCtx(), paySelection.getC_DocType_ID());
@@ -339,6 +339,9 @@ public final class MPaySelectionCheck extends X_C_PaySelectionCheck
 				payment.setDateTrx(paySelectionCheck.getParent().getPayDate());
 				payment.setDateAcct(payment.getDateTrx()); // globalqss [ 2030685 ]
 				payment.setC_BPartner_ID(paySelectionCheck.getC_BPartner_ID());
+				if (!paySelectionCheck.getPaymentRule().equals(PAYMENTRULE_Check)) {
+					payment.setDocumentNo(paySelectionCheck.getDocumentNo());
+				}
 				//	Link to Batch
 				if (batch != null) {
 					if (batch.getC_PaymentBatch_ID() == 0)
