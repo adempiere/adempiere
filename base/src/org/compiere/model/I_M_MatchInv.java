fa149b984853567874f5b233b496ff1c0a8ffe95
Carlos Parada <carlosaparadam@gmail.com>
2019-10-07 16:24:22 -0400

    Add Document Type on Match Invoice

--- base/src/org/compiere/model/I_M_MatchInv.java
+++ base/src/org/compiere/model/I_M_MatchInv.java
@@ -62,6 +62,21 @@ public interface I_M_MatchInv
 	  */
 	public int getAD_Org_ID();
 
+    /** Column name C_DocType_ID */
+    public static final String COLUMNNAME_C_DocType_ID = "C_DocType_ID";
+
+	/** Set Document Type.
+	  * Document type or rules
+	  */
+	public void setC_DocType_ID (int C_DocType_ID);
+
+	/** Get Document Type.
+	  * Document type or rules
+	  */
+	public int getC_DocType_ID();
+
+	public org.compiere.model.I_C_DocType getC_DocType() throws RuntimeException;
+
     /** Column name C_InvoiceLine_ID */
     public static final String COLUMNNAME_C_InvoiceLine_ID = "C_InvoiceLine_ID";
 
