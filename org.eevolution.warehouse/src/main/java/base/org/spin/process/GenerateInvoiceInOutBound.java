f2e7e3c65a79bd4384fd8e0d5d385e8022148bc3
Yamel Senih <ysenih@erpya.com>
2019-11-12 21:43:11 -0400

    Add consolidate document flag for smart browser "Generate Invoice from Outbound Orders

--- org.eevolution.warehouse/src/main/java/base/org/spin/process/GenerateInvoiceInOutBound.java
+++ org.eevolution.warehouse/src/main/java/base/org/spin/process/GenerateInvoiceInOutBound.java
@@ -22,14 +22,12 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
-import org.compiere.model.MDocType;
 import org.compiere.model.MInvoice;
 import org.compiere.model.MInvoiceLine;
 import org.compiere.model.MOrder;
 import org.compiere.model.MOrderLine;
 import org.compiere.model.PO;
 import org.compiere.model.Query;
-import org.compiere.print.MPrintFormat;
 import org.eevolution.model.MWMInOutBound;
 import org.eevolution.model.MWMInOutBoundLine;
 
@@ -105,7 +103,12 @@ public class GenerateInvoiceInOutBound extends GenerateInvoiceInOutBoundAbstract
 	 * @return MInOut return the Shipment header
 	 */
 	private MInvoice getInvoice(MOrderLine orderLine, MWMInOutBound outbound) {
-		MInvoice invoice = invoices.get(orderLine.getC_Order_ID());
+		int key = orderLine.getC_Order_ID();
+		if(isConsolidateDocument()) {
+			key = orderLine.getC_BPartner_ID();
+		}
+		//	
+		MInvoice invoice = invoices.get(key);
 		if(invoice != null)
 			return invoice;
 
@@ -117,7 +120,7 @@ public class GenerateInvoiceInOutBound extends GenerateInvoiceInOutBoundAbstract
 		invoice.setIsSOTrx(true);
 		invoice.saveEx();
 
-		invoices.put(order.getC_Order_ID(), invoice);
+		invoices.put(key, invoice);
 		return invoice;
 	}
 	
