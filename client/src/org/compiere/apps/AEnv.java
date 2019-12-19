567254f63b663f015556251656e87faf353139b3
Michael McKay <michael.mckay@mckayerp.com>
2019-12-19 07:07:59 -0500

    Change how AEnv calculates the center of the screen to compensate for multiple displays.  If ADempiere is occupying the secondary screen, popups and dialogs will appear on the main display rather than overlayed on the application as expected.

--- client/src/org/compiere/apps/AEnv.java
+++ client/src/org/compiere/apps/AEnv.java
@@ -161,22 +161,26 @@ public final class AEnv
 		window.pack();
 		// take into account task bar and other adornments
 		GraphicsConfiguration config = window.getGraphicsConfiguration();
-		Rectangle bounds = config.getBounds();
-		Dimension sSize = bounds.getSize();
+		Rectangle displayBounds = config.getBounds();
 		Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(config);
-		sSize.width -= (insets.left + insets.right);
-		sSize.height -= (insets.top + insets.bottom);
+		displayBounds.x += insets.left;
+		displayBounds.y += insets.top;
+		displayBounds.width -= (insets.left + insets.right);
+		displayBounds.height -= (insets.top + insets.bottom);
 		
 		Dimension wSize = window.getSize();
 		//	fit on window
-		if (wSize.height > sSize.height)
-			wSize.height = sSize.height;
-		if (wSize.width > sSize.width)
-			wSize.width = sSize.width;
+		if (wSize.height > displayBounds.height)
+			wSize.height = displayBounds.height;
+		if (wSize.width > displayBounds.width)
+			wSize.width = displayBounds.width;
 		window.setSize(wSize);
-		//	Center
-		int x = (sSize.width - wSize.width) / 2;
-		int y = (sSize.height - wSize.height) / 2;
+		
+		//  Display device offset is set below. Here
+		//  x and y are relative to the screen.
+		//	Center 
+		int x = (displayBounds.width - wSize.width) / 2;
+		int y = (displayBounds.height - wSize.height) / 2;
 		if (position == SwingConstants.CENTER)
 			;
 		else if (position == SwingConstants.NORTH_WEST)
@@ -190,7 +194,7 @@ public final class AEnv
 		}
 		else if (position == SwingConstants.NORTH_EAST)
 		{
-			x = (sSize.width - wSize.width);
+			x = (displayBounds.width - wSize.width);
 			y = 0;
 		}
 		else if (position == SwingConstants.WEST)
@@ -199,24 +203,25 @@ public final class AEnv
 		}
 		else if (position == SwingConstants.EAST)
 		{
-			x = (sSize.width - wSize.width);
+			x = (displayBounds.width - wSize.width);
 		}
 		else if (position == SwingConstants.SOUTH)
 		{
-			y = (sSize.height - wSize.height);
+			y = (displayBounds.height - wSize.height);
 		}
 		else if (position == SwingConstants.SOUTH_WEST)
 		{
 			x = 0;
-			y = (sSize.height - wSize.height);
+			y = (displayBounds.height - wSize.height);
 		}
 		else if (position == SwingConstants.SOUTH_EAST)
 		{
-			x = (sSize.width - wSize.width);
-			y = (sSize.height - wSize.height);
+			x = (displayBounds.width - wSize.width);
+			y = (displayBounds.height - wSize.height);
 		}
-		//
-		window.setLocation(bounds.x + x + insets.left, bounds.y + y + insets.top);
+		
+		//  Now add the display device offsets
+		window.setLocation(displayBounds.x + x + insets.left, displayBounds.y + y + insets.top);
 	}	//	positionScreen
 
 	/**
@@ -246,41 +251,47 @@ public final class AEnv
 		}
 		window.pack();
 		//
-		Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize();
 		// take into account task bar and other adornments
 		GraphicsConfiguration config = window.getGraphicsConfiguration();
+		Rectangle displayBounds = config.getBounds(); // Mulit-screen aware
 		Insets insets = Toolkit.getDefaultToolkit().getScreenInsets(config);
-		sSize.width -= (insets.left + insets.right);
-		sSize.height -= (insets.top + insets.bottom);
+		displayBounds.x += insets.left;
+		displayBounds.y += insets.top;
+		displayBounds.width -= (insets.left + insets.right);
+		displayBounds.height -= (insets.top + insets.bottom);
 		
 		Dimension wSize = window.getSize();
 		//	fit on window
-		if (wSize.height > sSize.height)
-			wSize.height = sSize.height;
-		if (wSize.width > sSize.width)
-			wSize.width = sSize.width;
+		if (wSize.height > displayBounds.height)
+			wSize.height = displayBounds.height;
+		if (wSize.width > displayBounds.width)
+			wSize.width = displayBounds.width;
 		window.setSize(wSize);
 		//	center in parent
 		Rectangle pBounds = parent.getBounds();
-		//	Parent is in upper left corner
-		if (pBounds.x == pBounds.y && pBounds.x == 0)
+		//	Parent is in upper left corner and smaller than
+		//  the window. 
+		if (pBounds.x == displayBounds.x 
+			&& pBounds.y == displayBounds.y
+			&& pBounds.width < wSize.width
+			&& pBounds.height < wSize.height)
 		{
 			positionCenterScreen(window);
 			return;
 		}
 		//  Find middle
 		int x = pBounds.x + ((pBounds.width-wSize.width)/2);
-		if (x < 0)
-			x = 0;
+		if (x < pBounds.x)
+			x = pBounds.x;
 		int y = pBounds.y + ((pBounds.height-wSize.height)/2);
-		if (y < 0)
-			y = 0;
+		if (y < pBounds.y)
+			y = pBounds.y;
 
 		//	Is it on Screen?
-		if (x + wSize.width > sSize.width)
-			x = sSize.width - wSize.width;
-		if (y + wSize.height > sSize.height)
-			y = sSize.height - wSize.height;
+		if (x + wSize.width > displayBounds.x + displayBounds.width)
+			x = displayBounds.x + displayBounds.width - wSize.width;
+		if (y + wSize.height > displayBounds.y + displayBounds.height)
+			y = displayBounds.y + displayBounds.height - wSize.height;
 		//
 	//	System.out.println("Position: x=" + x + " y=" + y + " w=" + wSize.getWidth() + " h=" + wSize.getHeight()
 	//		+ " - Parent loc x=" + pLoc.x + " y=" + y + " w=" + pSize.getWidth() + " h=" + pSize.getHeight());
