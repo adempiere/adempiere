569be4fd9628521cfae9eb7f732cdcc9d9a2d951
Michael McKay <michael.mckay@mckayerp.com>
2019-12-20 10:28:43 -0500

    Change the VCreateFromPanel to use base classes for selection listener and row selection events.

--- client/src/org/compiere/apps/form/VCreateFromPanel.java
+++ client/src/org/compiere/apps/form/VCreateFromPanel.java
@@ -29,8 +29,8 @@ import org.compiere.apps.AppsAction;
 import org.compiere.apps.ConfirmPanel;
 import org.compiere.apps.StatusBar;
 import org.compiere.minigrid.MiniTable;
-import org.compiere.minigrid.MiniTable.MiniTableSelectionListener;
-import org.compiere.minigrid.MiniTable.RowSelectionEvent;
+import org.compiere.minigrid.SelectionListener;
+import org.compiere.minigrid.RowSelectionEvent;
 import org.compiere.swing.CButton;
 import org.compiere.swing.CPanel;
 import org.compiere.util.Env;
@@ -48,7 +48,7 @@ import org.compiere.util.TrxRunnable;
  *		@see https://github.com/adempiere/adempiere/issues/114
  *
  */
-public class VCreateFromPanel extends CPanel implements ActionListener, MiniTableSelectionListener
+public class VCreateFromPanel extends CPanel implements ActionListener, SelectionListener
 {
 
 	/**
