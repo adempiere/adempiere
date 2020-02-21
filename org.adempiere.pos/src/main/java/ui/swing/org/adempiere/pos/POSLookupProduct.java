2ac12ad59eb03aae01c7481a1f0001784fa452ab
Yamel Senih <ysenih@erpya.com>
2020-02-21 12:34:14 -0400

    Remove unnecessary imports import

--- org.adempiere.pos/src/main/java/ui/swing/org/adempiere/pos/POSLookupProduct.java
+++ org.adempiere.pos/src/main/java/ui/swing/org/adempiere/pos/POSLookupProduct.java
@@ -26,7 +26,6 @@ import org.compiere.util.KeyNamePair;
 import org.compiere.util.Msg;
 
 import javax.swing.JComboBox;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
