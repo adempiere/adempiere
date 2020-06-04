33cde5a98298ad23adb61a8ac22d8ca08c811288
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2020-06-03 21:00:29 -0500

    #3131 Error calculating parent tax on sales order with warehouse document type https://github.com/adempiere/adempiere/issues/3131 - Remove duplicated code with same funcionality

--- base/src/org/compiere/model/MOrderLine.java
+++ base/src/org/compiere/model/MOrderLine.java
@@ -974,14 +974,14 @@ public class MOrderLine extends X_C_OrderLine implements IDocumentLine
 	{
 		if (!success)
 			return success;
-		if (!newRecord && is_ValueChanged("C_Tax_ID"))
-		{
-			//	Recalculate Tax for old Tax
-			if (!getParent().isProcessed())
-				if (!updateOrderTax(true))
-					return false;
-		}
-		return updateHeaderTax();
+		if (newRecord
+				|| (!newRecord && is_ValueChanged(MOrderLine.COLUMNNAME_C_Tax_ID) && !getParent().isProcessed())
+				|| (!newRecord && is_ValueChanged(MOrderLine.COLUMNNAME_QtyEntered) && !getParent().isProcessed())
+				|| (!newRecord && is_ValueChanged(MOrderLine.COLUMNNAME_PriceActual) && !getParent().isProcessed())
+		)
+			return getParent().calculateTaxTotal();
+
+		return true;
 	}	//	afterSave
 
 	/**
@@ -1009,7 +1009,7 @@ public class MOrderLine extends X_C_OrderLine implements IDocumentLine
 	 * 
 	 * @author teo_sarca [ 1583825 ]
 	 */
-	private boolean updateOrderTax(boolean oldTax) {
+	/*private boolean updateOrderTax(boolean oldTax) {
 		MOrderTax tax = MOrderTax.get (this, getPrecision(), oldTax, get_TrxName());
 		if (tax != null) {
 			if (!tax.calculateTaxFromLines())
@@ -1024,7 +1024,7 @@ public class MOrderLine extends X_C_OrderLine implements IDocumentLine
 			}
 		}
 		return true;
-	}
+	}*/
 	
 	/**
 	 *	Update Tax & Header
@@ -1034,8 +1034,7 @@ public class MOrderLine extends X_C_OrderLine implements IDocumentLine
 	{
 		//	Recalculate Tax for this Tax
 		if (!getParent().isProcessed())
-			if (!updateOrderTax(false))
-				return false;
+				getParent().calculateTaxTotal();
 		
 		//	Update Order Header
 		String sql = "UPDATE C_Order i"
