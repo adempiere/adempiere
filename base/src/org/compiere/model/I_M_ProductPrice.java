6c792f8f8bdc28e1bf2b1082d72d73eecbd89a7d
Yamel Senih <ysenih@erpya.com>
2020-11-23 09:29:50 -0400

    Add model class

--- base/src/org/compiere/model/I_M_ProductPrice.java
+++ base/src/org/compiere/model/I_M_ProductPrice.java
@@ -22,7 +22,7 @@ import org.compiere.util.KeyNamePair;
 
 /** Generated Interface for M_ProductPrice
  *  @author Adempiere (generated) 
- *  @version Release 3.9.2
+ *  @version Release 3.9.3
  */
 public interface I_M_ProductPrice 
 {
@@ -121,6 +121,15 @@ public interface I_M_ProductPrice
 
 	public org.compiere.model.I_M_Product getM_Product() throws RuntimeException;
 
+    /** Column name M_ProductPrice_ID */
+    public static final String COLUMNNAME_M_ProductPrice_ID = "M_ProductPrice_ID";
+
+	/** Set Product Price	  */
+	public void setM_ProductPrice_ID (int M_ProductPrice_ID);
+
+	/** Get Product Price	  */
+	public int getM_ProductPrice_ID();
+
     /** Column name PriceLimit */
     public static final String COLUMNNAME_PriceLimit = "PriceLimit";
 
@@ -160,19 +169,6 @@ public interface I_M_ProductPrice
 	  */
 	public BigDecimal getPriceStd();
 
-    /** Column name UUID */
-    public static final String COLUMNNAME_UUID = "UUID";
-
-	/** Set Immutable Universally Unique Identifier.
-	  * Immutable Universally Unique Identifier
-	  */
-	public void setUUID (String UUID);
-
-	/** Get Immutable Universally Unique Identifier.
-	  * Immutable Universally Unique Identifier
-	  */
-	public String getUUID();
-
     /** Column name Updated */
     public static final String COLUMNNAME_Updated = "Updated";
 
@@ -188,4 +184,17 @@ public interface I_M_ProductPrice
 	  * User who updated this records
 	  */
 	public int getUpdatedBy();
+
+    /** Column name UUID */
+    public static final String COLUMNNAME_UUID = "UUID";
+
+	/** Set Immutable Universally Unique Identifier.
+	  * Immutable Universally Unique Identifier
+	  */
+	public void setUUID (String UUID);
+
+	/** Get Immutable Universally Unique Identifier.
+	  * Immutable Universally Unique Identifier
+	  */
+	public String getUUID();
 }
