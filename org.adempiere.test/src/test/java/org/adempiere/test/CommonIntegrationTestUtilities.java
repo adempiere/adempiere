3a193c2dc0e4b8483e7be7c22ca694e766915d95
mckayERP <michael.mckay@mckayerp.com>
2020-12-10 20:34:12 -0500

    Add junit jupiter test support (#3280) This commit adds the necessarylibraries and updates the class path. The base build.xml is modified to execute unit and integration tests according to flags set in the test.properties file added to the util_dev directory. The build.xml files in the various modules can access this file to determine which tests should be run.

---
+++ org.adempiere.test/src/test/java/org/adempiere/test/CommonIntegrationTestUtilities.java
@@ -0,0 +1,83 @@
+/******************************************************************************
+ * Product: ADempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 2006-2020 ADempiere Foundation, All Rights Reserved.         *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * or via info@adempiere.net or http://www.adempiere.net/license.html         *
+ *****************************************************************************/
+package org.adempiere.test;
+
+import static org.compiere.model.X_C_PeriodControl.PERIODSTATUS_Closed;
+import static org.compiere.model.X_C_PeriodControl.PERIODSTATUS_Open;
+
+import java.sql.Timestamp;
+import java.util.Properties;
+
+import org.compiere.model.MAcctSchema;
+import org.compiere.model.MClient;
+import org.compiere.model.MPeriod;
+import org.compiere.model.MPeriodControl;
+import org.compiere.util.Env;
+import org.compiere.util.TimeUtil;
+
+public class CommonIntegrationTestUtilities {
+
+	private boolean initialAutoPeriodControl;
+	private Timestamp currentLoginDate = TimeUtil.getDay(System.currentTimeMillis());
+	private static final String DATE_PROPERTY = "#Date";
+	
+	public void turnOffAutoPeriodControl(Properties ctx, int ad_client_id) {
+				
+		MAcctSchema as = MClient.get(ctx, ad_client_id).getAcctSchema();
+		initialAutoPeriodControl = as.isAutoPeriodControl();
+		as.setAutoPeriodControl(false);
+		as.saveEx();
+		
+	}
+
+	public void closePeriod(Properties ctx, int ad_org_id, Timestamp dateInPeriod, 
+			String docBaseType, String trxName) {
+		
+		MPeriod period = MPeriod.get(ctx, dateInPeriod, ad_org_id, trxName);
+		MPeriodControl periodControl = period.getPeriodControl(docBaseType);
+		periodControl.setPeriodStatus(PERIODSTATUS_Closed);
+		periodControl.saveEx();
+		
+	}
+
+	public void openPeriod(Properties ctx, int ad_org_id, Timestamp dateInPeriod, 
+			String docBaseType, String trxName) {
+
+		MPeriod period = MPeriod.get(ctx, dateInPeriod, ad_org_id, trxName);
+		MPeriodControl periodControl = period.getPeriodControl(docBaseType);
+		periodControl.setPeriodStatus(PERIODSTATUS_Open);
+		periodControl.saveEx();
+
+	}
+	
+	public void resetAutoPeriodControl(Properties ctx, int ad_client_id) {
+		
+		MAcctSchema as = MClient.get(ctx, ad_client_id).getAcctSchema();
+		as.setAutoPeriodControl(initialAutoPeriodControl);
+		as.saveEx();
+		
+	}
+	
+	public void resetLoginDate(Properties ctx) {
+		ctx.setProperty(DATE_PROPERTY, currentLoginDate.toString());
+	}
+
+	public void setLoginDate(Properties ctx, Timestamp newLoginDate) {
+		currentLoginDate  = Env.getContextAsDate(ctx, DATE_PROPERTY);
+		ctx.setProperty(DATE_PROPERTY, newLoginDate.toString());
+	}
+
+}
