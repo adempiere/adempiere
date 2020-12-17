225677872c492e8018dd180545cb921cb39d2da0
mckayERP <michael.mckay@mckayerp.com>
2020-12-17 06:50:22 -0500

    Convert MAttributTest to jupiter and change the model to org.compiere.model.

--- base/test/src/org/compiere/model/MAttributeTest.java
+++ base/test/src/org/compiere/model/MAttributeTest.java
@@ -1,48 +1,43 @@
 /******************************************************************************
  * Product: Adempiere ERP & CRM Smart Business Solution                       *
  * Copyright (C) 2008 SC ARHIPAC SERVICE SRL. All Rights Reserved.            *
- * This program is free software; you can redistribute it and/or modify it    *
+ * Copyright (C) 2006-2020 ADempiere Foundation, All Rights Reserved.         *
+ * This program is free software, you can redistribute it and/or modify it    *
  * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
  * See the GNU General Public License for more details.                       *
  * You should have received a copy of the GNU General Public License along    *
- * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * or via info@adempiere.net or http://www.adempiere.net/license.html         *
  *****************************************************************************/
-package test.functional;
 
-import org.compiere.model.MAttribute;
-import org.compiere.model.MAttributeInstance;
-import org.compiere.model.MAttributeValue;
-import org.compiere.model.MInvoice;
-import org.compiere.util.Env;
+package org.compiere.model;
 
-import test.AdempiereTestCase;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * @author Teo Sarca, www.arhipac.ro //red1 borrows from MInvoiceTest
- */
-public class MAttributeTest extends AdempiereTestCase
-{
-	@Override
-	protected void setUp() throws Exception
-	{
-		super.setUp();
-		assertEquals("Client is not GardenWorld", 11, Env.getAD_Client_ID(getCtx()));
-	}
-	
-	public void testQuery() throws Exception
-	{
-		MAttribute attrib = new MAttribute(getCtx(), 100, getTrxName());
-		MAttribute[] attbClient = MAttribute.getOfClient(getCtx(), true, true);
-		assertTrue("atrributes must be true", attbClient.length > 0);
-		MAttributeValue[] av = attrib.getMAttributeValues();
-		assertTrue("atrributes must have values", av.length > 0);		
-		MAttributeInstance ai = attrib.getMAttributeInstance(100);
-		assertTrue("atrributes must have values", ai.isActive());		
+import org.adempiere.test.CommonGWSetup;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
-	}
+@Tag("Model")
+@Tag("MAttribute")
+class MAttributeTest extends CommonGWSetup {
+
+    @Test
+    void testQuery() throws Exception {
+
+        MAttribute attrib = new MAttribute(ctx, 100, trxName);
+        MAttribute[] attbClient = MAttribute.getOfClient(ctx, true, true);
+        assertTrue(attbClient.length > 0, "atrributes must be true");
+        MAttributeValue[] av = attrib.getMAttributeValues();
+        assertTrue(av.length > 0, "atrributes must have values");
+        MAttributeInstance ai = attrib.getMAttributeInstance(100);
+        assertTrue(ai.isActive(), "atrributes must have values");
+
+    }
 
 }
