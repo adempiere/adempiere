28a5c3a4109a34dd0fc75e110dc8617c0f3cd804
mckayERP <michael.mckay@mckayerp.com>
2020-12-19 13:27:42 -0500

    Update IT_MCostDetail to junit5 jupiter

--- base/test/src/org/compiere/model/IT_MCostElement.java
+++ base/test/src/org/compiere/model/IT_MCostElement.java
@@ -1,45 +1,36 @@
 /******************************************************************************
- * Product: Adempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 2008 SC ARHIPAC SERVICE SRL. All Rights Reserved.            *
- * This program is free software; you can redistribute it and/or modify it    *
+ * Product: ADempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 2006-2020 ADempiere Foundation, All Rights Reserved.         *
+ * This program is free software, you can redistribute it and/or modify it    *
  * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
  * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
  * See the GNU General Public License for more details.                       *
  * You should have received a copy of the GNU General Public License along    *
- * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * or via info@adempiere.net or http://www.adempiere.net/license.html         *
  *****************************************************************************/
-package test.functional;
 
-import org.compiere.model.MClient;
-import org.compiere.model.MCostElement;
-import org.compiere.model.X_M_CostElement;
-import org.compiere.util.Env;
-import test.AdempiereTestCase;
+package org.compiere.model;
 
-/**
- * @author Teo Sarca, www.arhipac.ro // copied by red1 from MInvoiceTest
- */
-public class MCostElementTest extends AdempiereTestCase
-{
-	public static final int BPARTNER_Standard = 112;
-	
-	@Override
-	protected void setUp() throws Exception
-	{
-		super.setUp();
-		assertEquals("Client is not GardenWorld", 11, Env.getAD_Client_ID(getCtx()));
-	}
-	
-	public void testQuery() throws Exception
-	{
-		X_M_CostElement ce =new X_M_CostElement(getCtx(), 102, null);
-		MClient client = MClient.get(ce.getCtx());
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-		MCostElement[] retValue5 = MCostElement.getElements(client.getCtx(), null); 
-		assertTrue("getElements returns 8", retValue5.length == 8);
-	}
+import java.util.List;
+
+import org.adempiere.test.CommonGWSetup;
+import org.junit.jupiter.api.Test;
+
+class IT_MCostElement extends CommonGWSetup {
+
+    @Test
+    void checkThatCostElementsExist() {
+
+        List<MCostElement> list = MCostElement.getCostElement(ctx, trxName);
+        assertTrue(list.size() > 0, "Expected cost elements to exist");
+
+    }
 
 }
