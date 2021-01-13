1462404aadc48cc7b4ea499958224d689c10d459
Yamel Senih <ysenih@erpya.com>
2021-01-13 15:07:11 -0400

    Prevent NPE for Create From on Payment Selection ## Step by Step - Create a new Payment Selection header - Go to process Create from Invoice - Select a Invoice - Press Ok

--- base/src/org/compiere/model/MPaySelectionLine.java
+++ base/src/org/compiere/model/MPaySelectionLine.java
@@ -132,7 +132,7 @@ public class MPaySelectionLine extends X_C_PaySelectionLine
 		setPayAmt (payAmt);
 		MInvoice invoice = new MInvoice(getCtx(), invoiceId, get_TrxName());
 		if(invoice.getC_ConversionType_ID() > 0) {
-			setC_ConversionType_ID(order.getC_ConversionType_ID());
+			setC_ConversionType_ID(invoice.getC_ConversionType_ID());
 		}
 		setDiscountAmt(discountAmt);
 		setDifferenceAmt(openAmt.subtract(payAmt).subtract(discountAmt));
