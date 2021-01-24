3e468f14201063a5efb10a09aeace2cbfdd1cb88
mckayERP <michael.mckay@mckayerp.com>
2021-01-23 23:57:10 -0500

    Move the query getting the time from the database to the DB class.  Move the utility to get the timestamp two seconds in the past to TimeUtils class.

--- base/src/org/compiere/util/TimeUtil.java
+++ base/src/org/compiere/util/TimeUtil.java
@@ -127,6 +127,7 @@ public class TimeUtil
 		return new Timestamp (cal.getTimeInMillis());
 	}	//	getNextDay
 
+
 	/**
 	 * 	Get last date in month
 	 *  @param day day
@@ -1354,4 +1355,21 @@ public class TimeUtil
         //	Value
         return weeks;
 	}
+	
+	/**
+	 * Return a BigDecimal representing the time in milliseconds 2 seconds 
+	 * prior to the current database time (not the system time). Useful for
+	 * limiting processes that may conflict with current operations.
+	 * @return BigDecimal representing time in milliseconds
+	 */
+    public static BigDecimal getTwoSecondsPriorToCurrentTimeInMillis() {
+
+        Timestamp ts = DB.getCurrentTimeFromDatabase();
+        long ms = ts.getTime() - (2 * 1000);
+        ts = new Timestamp(ms);
+        long mili = ts.getTime();
+        return new BigDecimal(Long.toString(mili));
+
+    }
+
 }	//	TimeUtil
