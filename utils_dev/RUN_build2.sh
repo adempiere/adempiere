b9b001de030c0ef9d800a467eee4fa07595ee597
Michael McKay <michael.mckay@mckayerp.com>
2021-02-24 20:11:57 -0500

    Update RUN_build2.sh
    Correct Ant path

--- utils_dev/RUN_build2.sh
+++ utils_dev/RUN_build2.sh
@@ -30,7 +30,7 @@ if [ $# -eq 0 ]
 fi
 
 #classpath
-export ANT_HOME="../tools/lib/ant/ant/apache-ant-1.10.9"
+export ANT_HOME="../tools/lib/ant/apache-ant-1.10.9"
 export JAVA_CLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant-launcher.jar:../tools/lib/commons-net.jar:$JAVA_HOME/lib/tools.jar
 export JAVA_OPTS="-Xms128m -Xmx512m"
 
