ff43939b06076c9c34ac6f14284f0a8923fc6b56
mckayERP <michael.mckay@mckayerp.com>
2021-04-16 12:53:40 -0400

    Update IT_MPeriod.java to test the modified methods.
    (cherry picked from commit d4fa7bd1b41a69bf41e783a5755bdb624775d819)

--- base/test/src/org/compiere/model/IT_MPeriod.java
+++ base/test/src/org/compiere/model/IT_MPeriod.java
@@ -1,10 +1,11 @@
 package org.compiere.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.sql.Timestamp;
 
-import org.adempiere.test.CommonGWSetup;
+import org.adempiere.test.CommonGWData;
+import org.adempiere.test.CommonSystemSetup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -12,12 +13,13 @@ import org.junit.jupiter.api.Test;
 @Tag("Model")
 @Tag("MPeriod")
 @DisplayName("Given the MPeriod class and current database")
-class IT_MPeriod extends CommonGWSetup {
+class IT_MPeriod extends CommonSystemSetup {
 
     @Test
     final void getMinPeriodStartDate_shouldReturnNonNullResult() {
 
-        Timestamp min = MPeriod.getMinPeriodStartDate(ctx, trxName);
+        Timestamp min = MPeriod.getMinPeriodStartDate(ctx,
+                CommonGWData.AD_CLIENT_ID, 0, trxName);
         assertNotNull(min);
 
     }
@@ -25,7 +27,8 @@ class IT_MPeriod extends CommonGWSetup {
     @Test
     final void getMaxPeriodEndDate_shouldReturnNotNullResult() {
 
-        Timestamp max = MPeriod.getMaxPeriodEndDate(ctx, trxName);
+        Timestamp max = MPeriod.getMaxPeriodEndDate(ctx,
+                CommonGWData.AD_CLIENT_ID, 0, trxName);
         assertNotNull(max);
 
     }
