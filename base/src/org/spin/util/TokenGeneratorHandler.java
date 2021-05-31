50f0572fd5a215a120a6aa4aee22c4dc68ccf4ad
Yamel Senih <ysenih@erpya.com>
2021-05-31 17:08:40 -0400

    Minor change imports Just remove unnecessary import for class

--- base/src/org/spin/util/TokenGeneratorHandler.java
+++ base/src/org/spin/util/TokenGeneratorHandler.java
@@ -27,7 +27,6 @@ import org.spin.model.MADToken;
 import org.spin.model.MADTokenDefinition;
 import org.compiere.util.Env;
 
-import com.sun.enterprise.admin.util.Logger;
 /**
  * @author Raul Muñoz, rMunoz@erpya.com, ERPCyA http://www.erpcya.com
  * @author Yamel Senih, ySenih@erpya.com, ERPCyA http://www.erpya.com
@@ -151,7 +150,7 @@ public class TokenGeneratorHandler {
                 superClazz = superClazz.getSuperclass();
             }
         } catch (Exception e) {
-        	Logger.logError("Loading class Error"+ e.getMessage());
+        	logger.log(Level.SEVERE, "Loading class Error"+ e.getMessage());
         }
         //
         logger.log(Level.SEVERE,"Not found Class: " + className);
