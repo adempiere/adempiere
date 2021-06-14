752c3e8816769e2fca35d6b4cff22d1f900521d5
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2021-06-13 23:29:28 -0400

    fix long value.

--- base/src/org/spin/model/MADContextInfo.java
+++ base/src/org/spin/model/MADContextInfo.java
@@ -41,6 +41,9 @@ import org.compiere.util.Util;
  * @author yamel, ysenih@erpya.com , http://www.erpya.com
  * <li> FR [ 1784 ]
  * @see https://github.com/adempiere/adempiere/issues/1784
+ * @author Edwin Betancourt, EdwinBetanc0urt@outlook.com , http://www.erpya.com
+ * 		<li> BR [ 3489 ] Window footer shows number of lines in null (ContextInfo)
+ * 		@see https://github.com/adempiere/adempiere/issues/3489
  */
 public class MADContextInfo extends X_AD_ContextInfo {
 
@@ -247,6 +250,7 @@ public class MADContextInfo extends X_AD_ContextInfo {
 							&& !(value instanceof Double)
 							&& !(value instanceof Float)
 							&& !(value instanceof Integer)
+							&& !(value instanceof Long)
 							&& !(value instanceof String)
 							&& !(value instanceof Timestamp)) {
 						continue;
