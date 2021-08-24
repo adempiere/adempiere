bf01699b869c97f4b1dc0a240f9532d5ef82cccf
Carlos Parada <cParada@erpya.com>
2021-08-24 16:59:42 -0400

    Fixed selection on Smart Browse

--- zkwebui/WEB-INF/src/org/eevolution/grid/WBrowserListItemRenderer.java
+++ zkwebui/WEB-INF/src/org/eevolution/grid/WBrowserListItemRenderer.java
@@ -482,6 +482,13 @@ public class WBrowserListItemRenderer implements ListitemRenderer, EventListener
 						table.setCheckmark(true);
 						table.removeEventListener(Events.ON_SELECT, this);
 						table.addEventListener(Events.ON_SELECT, this);
+					}else
+					{
+						ListItem listItem = table.getItemAtIndex(rowIndex);
+						boolean selected = id.isSelected();
+						if (listItem != null && !listItem.isSelected() && selected) {
+							listItem.setSelected(true);
+						}
 					}
 				}
 			}
