fbc2a24357bc0cfcbda8a41508fa683d4d7aaa21
Yamel Senih <ysenih@erpya.com>
2021-08-29 22:10:07 +0000

    Add Waiting Time for Queue Type (#3602)
    Pull request: https://github.com/adempiere/adempiere/pull/3602
    Tested and accepted by Mario Calderon (marcalwestf, mariocalderon007@gmail.com)

--- base/src/org/spin/queue/process/FlushSystemQueue.java
+++ base/src/org/spin/queue/process/FlushSystemQueue.java
@@ -17,15 +17,22 @@
 
 package org.spin.queue.process;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.IntStream;
 
 import org.compiere.model.Query;
+import org.compiere.util.TimeUtil;
 import org.compiere.util.Trx;
+import org.compiere.util.Util;
 import org.spin.queue.model.I_AD_Queue;
 import org.spin.queue.model.MADQueue;
+import org.spin.queue.model.MADQueueType;
 import org.spin.queue.util.QueueLoader;
 import org.spin.queue.util.QueueManager;
 
@@ -45,11 +52,16 @@ public class FlushSystemQueue extends FlushSystemQueueAbstract {
 		if(getBatchsToProcess() > 0
 				&& getRecordsByBatch() > 0) {
 			StringBuffer whereClause = new StringBuffer(I_AD_Queue.COLUMNNAME_Processed).append(" = 'N'");
-			List<Object> parameters = new ArrayList<>();
+			List<Object> parameters = new ArrayList<Object>();
 			if(getQueueTypeId() > 0) {
 				whereClause.append(" AND ").append(I_AD_Queue.COLUMNNAME_AD_QueueType_ID).append(" = ?");
 				parameters.add(getQueueTypeId());
 			}
+			Timestamp now = new Timestamp(System.currentTimeMillis());
+			now = TimeUtil.getDayTime(now, now);
+			whereClause.append(" AND ").append(I_AD_Queue.COLUMNNAME_Updated).append(" < ?");
+			parameters.add(now);
+			AtomicReference<Timestamp> referenceTime = new AtomicReference<Timestamp>(now);
 			//	For batch
 			Trx.run(transactionName -> {
 				IntStream.range(0, getBatchsToProcess()).forEach(page -> {
@@ -61,22 +73,63 @@ public class FlushSystemQueue extends FlushSystemQueueAbstract {
 						.getIDsAsList()
 						.forEach(queueId -> {
 							MADQueue queueToProcess = new MADQueue(getCtx(), queueId, transactionName);
-							QueueManager queueManager = QueueLoader.getInstance()
-									.getQueueManager(queueToProcess.getAD_QueueType_ID())
-									.withContext(getCtx())
-									.withTransactionName(transactionName);
-							try {
-								queueManager.process(queueToProcess, isDeleteAfterProcess());
-								counter.incrementAndGet();
-							} catch (Exception e) {
-								errors.incrementAndGet();
-								addLog("@AD_Queue_ID@: [" + queueToProcess + "]: " + e.getLocalizedMessage());
-								log.severe(queueToProcess + ": " + e.getLocalizedMessage());
+							if(isValidToProcess(queueToProcess, referenceTime.get())) {
+								QueueManager queueManager = QueueLoader.getInstance()
+										.getQueueManager(queueToProcess.getAD_QueueType_ID())
+										.withContext(getCtx())
+										.withTransactionName(transactionName);
+								try {
+									queueManager.process(queueToProcess, isDeleteAfterProcess());
+									counter.incrementAndGet();
+								} catch (Exception e) {
+									errors.incrementAndGet();
+									addLog("@AD_Queue_ID@: [" + queueToProcess + "]: " + e.getLocalizedMessage());
+									log.severe(queueToProcess + ": " + e.getLocalizedMessage());
+								}
 							}
 						});
 				});
 			});
 		}
 		return "@Processed@: " + counter + " @Errors@: " + errors;
+	}
+	
+	/**
+	 * Is valid to process a queue
+	 * @param queueToProcess
+	 * @param now
+	 * @return
+	 */
+	private boolean isValidToProcess(MADQueue queueToProcess, Timestamp now) {
+		return addWaitingTime(queueToProcess).before(now);
+	}
+	
+	/**
+	 * Add Waiting Time
+	 * @param queueToProcess
+	 * @return
+	 */
+	private Timestamp addWaitingTime(MADQueue queueToProcess) {
+		MADQueueType queueType = MADQueueType.getById(getCtx(), queueToProcess.getAD_QueueType_ID(), queueToProcess.get_TrxName());
+		if(queueType.getWaitingTime() > 0
+				&& !Util.isEmpty(queueType.getTimeUnit())) {
+			GregorianCalendar calendar = new GregorianCalendar();
+			calendar.setTime(queueToProcess.getUpdated());
+			if(queueType.getTimeUnit().equals(MADQueueType.TIMEUNIT_Year)) {
+				calendar.add(Calendar.YEAR, queueType.getWaitingTime());
+			} else if(queueType.getTimeUnit().equals(MADQueueType.TIMEUNIT_Month)) {
+				calendar.add(Calendar.MONTH, queueType.getWaitingTime());
+			} else if(queueType.getTimeUnit().equals(MADQueueType.TIMEUNIT_Week)) {
+				calendar.add(Calendar.WEEK_OF_MONTH, queueType.getWaitingTime());
+			} else if(queueType.getTimeUnit().equals(MADQueueType.TIMEUNIT_Day)) {
+				calendar.add(Calendar.DAY_OF_YEAR, queueType.getWaitingTime());
+			} else if(queueType.getTimeUnit().equals(MADQueueType.TIMEUNIT_Hour)) {
+				calendar.add(Calendar.HOUR_OF_DAY, queueType.getWaitingTime());
+			} else if(queueType.getTimeUnit().equals(MADQueueType.TIMEUNIT_Minute)) {
+				calendar.add(Calendar.MINUTE, queueType.getWaitingTime());
+			}
+			return new Timestamp(calendar.getTimeInMillis());
+		}
+		return queueToProcess.getUpdated();
 	}
 }
