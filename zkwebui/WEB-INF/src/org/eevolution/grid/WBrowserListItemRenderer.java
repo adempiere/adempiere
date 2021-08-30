ea003e056aff088cbeba48513421ac81f31878da
Carlos Parada <carlosaparadam@gmail.com>
2021-08-30 12:05:15 -0400

    Fixed process selection when summary row is the first row

--- zkwebui/WEB-INF/src/org/eevolution/grid/WBrowserListItemRenderer.java
+++ zkwebui/WEB-INF/src/org/eevolution/grid/WBrowserListItemRenderer.java
@@ -815,7 +815,8 @@ public class WBrowserListItemRenderer implements ListitemRenderer, EventListener
 			WBrowserTable table = (WBrowserTable) event.getTarget();
 			if (table.isCheckmark()) {
 				int cnt = table.getRowCount();
-				if (cnt == 0 || !(table.getValueAt(0, 0) instanceof IDColumn))
+				if (table.getValueAt(0, 0) !=null && 
+						(cnt == 0 || !(table.getValueAt(0, 0) instanceof IDColumn)))
 					return;
 
 				//update IDColumn
