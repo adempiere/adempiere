8631015c8fab67b5f6be41d829193f51c28df202
Raúl Capecce <rcapecce@gmail.com>
2022-02-04 10:16:50 -0300

    New way to iterate ResultSets

--- base/src/org/compiere/util/ResultSetRunnable.java
+++ base/src/org/compiere/util/ResultSetRunnable.java
@@ -3,6 +3,12 @@ package org.compiere.util;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+/**
+ * Interface to run ResultSets in anonymous function
+ * @author Raul Capecce, raul.capecce@openupsolutions.com
+ * 		<li>FR [ 3725 ] New way to iterate ResultSets
+ * 		@see https://github.com/adempiere/adempiere/issues/3725
+ */
 @FunctionalInterface
 public interface ResultSetRunnable {
     void run(ResultSet resultSet) throws SQLException;
