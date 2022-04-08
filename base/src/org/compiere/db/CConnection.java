347d7162e51483b1b5667264c8818edadea3c959
Yamel Senih <ysenih@erpya.com>
2022-04-08 15:32:59 -0400

    instance of connection

--- base/src/org/compiere/db/CConnection.java
+++ base/src/org/compiere/db/CConnection.java
@@ -152,6 +152,11 @@ public class CConnection implements Serializable, Cloneable
 				s_cc = new CConnection (null);
 				s_cc.setAttributes (attributes);
 				log.fine(s_cc.toString());
+			} else {
+				s_cc = new CConnection(apps_host);
+				//  set also in ALogin and Ctrl
+				Ini.setProperty (Ini.P_CONNECTION, s_cc.toStringLong ());
+				Ini.saveProperties (Ini.isClient ());
 			}
 		}
 
