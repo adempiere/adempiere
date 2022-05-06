6b29276546df5a36cb545dbd122e23e1cd357226
Yamel Senih <ysenih@erpya.com>
2022-05-06 15:19:34 -0400

    Add P_ADEMPIERE_APPS_TYPE support to Ini loader Currently exists a enviroment variable added to ADempiere Pool connection, the problem is that it jus is setted as OS variable instead of Ini loader like is all ini enviroment of adempiere. This pull request add support to Ini loader and keep the OS enviroment as second option

--- base/src/org/compiere/db/DB_PostgreSQL.java
+++ base/src/org/compiere/db/DB_PostgreSQL.java
@@ -596,7 +596,7 @@ public class DB_PostgreSQL implements AdempiereDatabase
 				datasourceLongRunning = new HikariDataSource(config);;
 				log.warning("Starting Client Hikari Connection Pool");
 			} else {
-				Optional<String> maybeApplicationType = Optional.ofNullable(System.getenv("ADEMPIERE_APPS_TYPE"));
+				Optional<String> maybeApplicationType = Optional.ofNullable(Optional.ofNullable(Ini.getApplicationType()).orElse(System.getenv("ADEMPIERE_APPS_TYPE")));
 				datasourceLongRunning = maybeApplicationType
 						.map(applicationType -> {
 							if ("wildfly".equals(applicationType)) {
