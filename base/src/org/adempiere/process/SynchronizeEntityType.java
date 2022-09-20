21b79b75bf5e736d6b6ae65f6441994e29b9bc7c
eEvolution author Victor Perez <victor.perez@e-evolution.com>
2022-09-20 17:18:52 -0500

    #3987 [Feature Request] Synchronize Entity Type https://github.com/adempiere/adempiere/issues/3987

--- base/src/org/adempiere/process/SynchronizeEntityType.java
+++ base/src/org/adempiere/process/SynchronizeEntityType.java
@@ -1,33 +1,30 @@
-/******************************************************************************
- * Product: ADempiere ERP & CRM Smart Business Solution                       *
- * Copyright (C) 2006-2017 ADempiere Foundation, All Rights Reserved.         *
- * This program is free software, you can redistribute it and/or modify it    *
- * under the terms version 2 of the GNU General Public License as published   *
- * or (at your option) any later version.                                     *
- * by the Free Software Foundation. This program is distributed in the hope   *
- * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
- * See the GNU General Public License for more details.                       *
- * You should have received a copy of the GNU General Public License along    *
- * with this program, if not, write to the Free Software Foundation, Inc.,    *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- * For the text or an alternative of this public license, you may reach us    *
- * or via info@adempiere.net                                                  *
- * or https://github.com/adempiere/adempiere/blob/develop/license.html        *
- *****************************************************************************/
+/**
+ * Copyright (C) 2003-2022, e-Evolution Consultants S.A. , http://www.e-evolution.com
+ * This program is free software, you can redistribute it and/or modify it
+ * under the terms version 2 of the GNU General Public License as published
+ * or (at your option) any later version.
+ * by the Free Software Foundation. This program is distributed in the hope
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with this program, if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * For the text or an alternative of this public license, you may reach us
+ * or via info@adempiere.net or http://www.adempiere.net/license.html
+ * Email: victor.perez@e-evolution.com, http://www.e-evolution.com , http://github.com/e-Evolution
+ * Created by victor.perez@e-evolution.com , www.e-evolution.com
+ */
 
 package org.adempiere.process;
 
 import org.adempiere.model.MBrowse;
-import org.compiere.model.MClientInfo;
 import org.compiere.model.MEntityType;
 import org.compiere.model.MForm;
 import org.compiere.model.MMenu;
 import org.compiere.model.MProcess;
-import org.compiere.model.MProcessPara;
 import org.compiere.model.MTree;
 import org.compiere.model.MTreeNode;
-import org.compiere.model.MTree_NodeMM;
 import org.compiere.model.MWindow;
 import org.compiere.wf.MWorkflow;
 
