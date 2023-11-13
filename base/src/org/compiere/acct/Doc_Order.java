3e25fb5759b6690d13800beeac319c8c5cfa43de
Raúl Capecce <rcapecce@gmail.com>
2023-11-13 18:34:26 -0300

    Fixes #4188

--- base/src/org/compiere/acct/Doc_Order.java
+++ base/src/org/compiere/acct/Doc_Order.java
@@ -47,6 +47,9 @@ import org.compiere.util.Env;
  *  </pre>
  *  @author Jorg Janke
  *  @version  $Id: Doc_Order.java,v 1.3 2006/07/30 00:53:33 jjanke Exp $
+ * @author Raul Capecce, raul.capecce@solopsoftware.com, Solop https://solopsoftware.com/
+ *		<a href="https://github.com/adempiere/adempiere/issues/4188">
+ * 		@see BF [ 4188 ] Badly formatted end of line in files</a>
  */
 public class Doc_Order extends Doc
 {
